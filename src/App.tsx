a9b06845ddd049ee0e4850cc0ee9424a13852223
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-04-24 14:44:35 -0400

    initial version

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MealCard } from "./components/MealCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MealCard mealTime="10am" food={{name: "breakfast", serving: {vegetable: 1, fruit: 1, carbohydrate: 1, protein: 1, fat: 1, sweeet: 1}}} />
     </div>
   );
 }
