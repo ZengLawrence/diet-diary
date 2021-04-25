82913dd534c757f9e51ab926d01dc7e3c11dad97
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-04-25 14:01:34 -0400

    tool bar

--- src/container/DayPage.tsx
+++ src/container/DayPage.tsx
@@ -1,13 +1,23 @@
+import { faPlusSquare } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import _ from "lodash";
+import { Fragment } from "react";
 import { MealCard } from "../components/MealCard";
 import { Meal } from "../model/Food";
 
-export const DayPage = (props: {meals: Meal[]}) => {
-  const {meals} = props;
-  const mealCards = _.map(meals, (meal, index) => <MealCard key={index} meal={meal}/>);
+const ToolBar = () => (
+  <div className="d-flex justify-content-end" >
+    <FontAwesomeIcon icon={faPlusSquare} size="3x" />
+  </div>
+);
+
+export const DayPage = (props: { meals: Meal[] }) => {
+  const { meals } = props;
+  const mealCards = _.map(meals, (meal, index) => <MealCard key={index} meal={meal} />);
   return (
-    <div>
+    <Fragment>
+      <ToolBar />
       {mealCards}
-    </div>
+    </Fragment>
   )
 }
