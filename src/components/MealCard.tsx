9023498b8ec537788d37f7e2710e8840ec2b159a
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-04-25 17:02:35 -0400

    extract Serving Summary

--- src/components/MealCard.tsx
+++ src/components/MealCard.tsx
@@ -1,38 +1,8 @@
 import _ from "lodash";
 import { Card, ListGroup } from "react-bootstrap";
-import { Meal, Serving } from "../model/Food";
-import { FoodGroupServingBadge } from "./FoodGroupBadge";
+import { Meal } from "../model/Food";
+import { ServingSummary } from "./ServingSummary";
 import { FoodItem } from "./FoodItem";
-
-function add(n1: number | undefined, n2: number | undefined) {
-  return _.defaultTo(n1, 0) + _.defaultTo(n2, 0);
-}
-
-function sumServings(s1: Serving, s2: Serving): Serving {
-  return {
-    vegetable: add(s1.vegetable, s2.vegetable),
-    fruit: add(s1.fruit, s2.fruit),
-    carbohydrate: add(s1.carbohydrate, s2.carbohydrate),
-    protein: add(s1.protein, s2.protein),
-    fat: add(s1.fat, s2.fat),
-    sweet: add(s1.sweet, s2.sweet),
-  };
-}
-
-const ServingSummary = (props: { servings: Serving[] }) => {
-  const { servings } = props;
-  const servingSummary = _.reduce(servings, sumServings, {});
-  return (
-    <Card.Footer className="d-flex justify-content-end" >
-      <FoodGroupServingBadge foodGroup="vegetable" serving={servingSummary} />
-      <FoodGroupServingBadge foodGroup="fruit" serving={servingSummary} />
-      <FoodGroupServingBadge foodGroup="carbohydrate" serving={servingSummary} />
-      <FoodGroupServingBadge foodGroup="protein" serving={servingSummary} />
-      <FoodGroupServingBadge foodGroup="fat" serving={servingSummary} />
-      <FoodGroupServingBadge foodGroup="sweet" serving={servingSummary} />
-    </Card.Footer>
-  );
-}
 
 export const MealCard = (props: { meal: Meal }) => {
   const { mealTime, foods } = props.meal;
@@ -44,7 +14,9 @@ export const MealCard = (props: { meal: Meal }) => {
       <ListGroup>
         {foodItems}
       </ListGroup>
-      <ServingSummary servings={servings} />
+      <Card.Footer className="d-flex justify-content-end" >
+        <ServingSummary servings={servings} />
+      </Card.Footer>
     </Card>
   );
 }
