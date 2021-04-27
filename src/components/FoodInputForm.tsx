2e36e8e77db5b8835ca432d3c5090f09171ddcef
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-04-26 20:32:54 -0400

    refactor

--- src/components/FoodInputForm.tsx
+++ src/components/FoodInputForm.tsx
@@ -4,13 +4,27 @@ import { Button, Form, Row } from "react-bootstrap";
 import { ServingInputControl } from "./ServingInputControl";
 import { Food, FoodGroup, totalCalories } from "../model/Food";
 
-function reducer(state: Food, action: { type: string; foodGroup: FoodGroup; serving: number | undefined }) {
+interface Action {
+  type: string;
+}
+
+interface SetServingAction extends Action {
+  type: "set-serving";
+  foodGroup: FoodGroup; 
+  serving: number | undefined;
+}
+
+function setServing(food: Food, action: SetServingAction) {
+  return {
+    ...food,
+    serving: _.set(food.serving, action.foodGroup, action.serving),
+  };
+}
+
+function reducer(state: Food, action: Action | SetServingAction) {
   switch (action.type) {
     case 'set-serving':
-      return {
-        ...state,
-        serving: _.set(state.serving, action.foodGroup, action.serving)
-      };
+      return setServing(state, action as SetServingAction);
     default:
       throw new Error();
   }
