82cd542348676b9996a330f54fa78c256d9dfeb3
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-04-28 20:30:29 -0400

    handle empty string

--- src/components/ServingInputControl.tsx
+++ src/components/ServingInputControl.tsx
@@ -1,12 +1,17 @@
 import _ from "lodash";
 import { Col, Form, Row } from "react-bootstrap";
-import { FoodGroupBadge } from "./FoodGroupBadge";
 import { FoodGroup, getCalories } from "../model/Food";
+import { FoodGroupBadge } from "./FoodGroupBadge";
 
 export const ServingInputControl = (props: { foodGroup: FoodGroup; onChange: (foodGroup: FoodGroup, serving: number) => void; }) => {
   const { foodGroup, onChange } = props;
   const controlId = "formServing" + foodGroup;
   const calories = _.toString(getCalories(foodGroup)) + " Cal.";
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const val = _.toNumber(e.target.value);
+    onChange(foodGroup, val);
+  }
 
   return (
     <Form.Group as={Row} controlId={controlId}>
@@ -14,7 +19,7 @@ export const ServingInputControl = (props: { foodGroup: FoodGroup; onChange: (fo
         {_.capitalize(foodGroup)}{' '}<FoodGroupBadge foodGroup={foodGroup} value={calories} />
       </Form.Label>
       <Col sm={2}>
-        <Form.Control type="text" onChange={e => onChange(foodGroup, parseFloat(e.target.value))} />
+        <Form.Control type="text" onChange={handleChange} />
       </Col>
     </Form.Group>
   );
