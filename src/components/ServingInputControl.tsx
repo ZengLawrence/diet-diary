961f71c244965be332f11308392960c9b7b31cec
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-05-01 08:52:11 -0400

    handle decimal

--- src/components/ServingInputControl.tsx
+++ src/components/ServingInputControl.tsx
@@ -1,12 +1,13 @@
 import _ from "lodash";
+import { useEffect, useState } from "react";
 import { Col, Form, Row } from "react-bootstrap";
 import { FoodGroup, getCalories, Serving } from "../model/Food";
 import { FoodGroupBadge } from "./FoodGroupBadge";
 
 interface Props {
-  foodGroup: FoodGroup; 
+  foodGroup: FoodGroup;
   serving: Serving;
-  onChange: (foodGroup: FoodGroup, serving: number) => void; 
+  onChange: (foodGroup: FoodGroup, serving: number) => void;
 }
 
 export const ServingInputControl = (props: Props) => {
@@ -14,10 +15,20 @@ export const ServingInputControl = (props: Props) => {
   const controlId = "formServing" + foodGroup;
   const calories = _.toString(getCalories(foodGroup)) + " Cal.";
 
+  const [servingStr, setServingStr] = useState(_.get(serving, foodGroup) || '');
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const val = _.toNumber(e.target.value);
-    props.onChange(foodGroup, val);
+    const val = e.target.value;
+    setServingStr(val);
+    props.onChange(foodGroup, _.toNumber(val));
   }
+
+  useEffect(() => {
+    if (_.get(serving, foodGroup) != _.toNumber(servingStr)) {
+      // reset happens
+      setServingStr(_.get(serving, foodGroup) || '');
+    }
+  }, [foodGroup, serving]);
 
   return (
     <Form.Group as={Row} controlId={controlId}>
@@ -25,11 +36,11 @@ export const ServingInputControl = (props: Props) => {
         {_.capitalize(foodGroup)}{' '}<FoodGroupBadge foodGroup={foodGroup} value={calories} />
       </Form.Label>
       <Col sm={2}>
-        <Form.Control 
-          type="text" 
-          value={_.get(serving, foodGroup) || ''}
-          onChange={handleChange} 
-          />
+        <Form.Control
+          type="text"
+          value={servingStr}
+          onChange={handleChange}
+        />
       </Col>
     </Form.Group>
   );
