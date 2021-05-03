4c93b433726cf591821e0d7bed9fe63cd028b822
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-05-03 17:32:10 -0400

    new day

--- src/container/DayPage.tsx
+++ src/container/DayPage.tsx
@@ -1,17 +1,24 @@
+import { faCalendarPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import _ from "lodash";
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
+import { Action, newDayAction } from "../actions";
 import { AddMealToolBar } from "../components/AddMealToolBar";
 import { CalorieServingSummary } from "../components/CalorieServingSummary";
 import { MealCard } from "../components/MealCard";
+import { MealDispatch } from "../components/MealDispatch";
 import { AppState } from "../model/AppState";
 
 export const DayPage = (props: { state: AppState }) => {
   const { date, mealStates } = props.state;
   const mealCards = _.map(mealStates, (mealState, index) => <MealCard key={index} mealIndex={index} state={mealState} />);
+  const dispatch: React.Dispatch<Action> = useContext(MealDispatch);
 
   return (
     <Fragment>
-      <h1 className="text-center">{date}</h1>
+      <h1 className="text-center">
+        {date}{' '}<FontAwesomeIcon icon={faCalendarPlus} className="text-primary" onClick={() => dispatch(newDayAction())} />
+      </h1>
       <CalorieServingSummary meals={_.map(mealStates, 'meal')} />
       {mealCards}
       <AddMealToolBar />
