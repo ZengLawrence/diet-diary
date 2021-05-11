f00aebb6c6e793d4759013267d488c1d4191821a
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-05-10 20:35:38 -0400

    remove goal summary

--- src/components/summary/CalorieServingSummary.tsx
+++ src/components/summary/CalorieServingSummary.tsx
@@ -3,7 +3,6 @@ import { calcMealCalories } from "../../model/calorieFunction";
 import { Meal } from "../../model/Food";
 import { calcMealsServingSummary } from "../../model/servingFunction";
 import { CalorieSummary } from "./CalorieSummary";
-import { GoalSummary } from "./GoalSummary";
 import { ServingSummary } from "./ServingSummary";
 
 function calcCaloriesSummary(meals: Meal[]) {
@@ -12,10 +11,7 @@ function calcCaloriesSummary(meals: Meal[]) {
 
 export const CalorieServingSummary = (props: { meals: Meal[]; goalCalorie: number }) => (
   <div className="d-flex">
-    <div className="d-flex flex-column" style={{minWidth: "80px"}}>
-      <CalorieSummary calories={calcCaloriesSummary(props.meals)} />
-      <GoalSummary calories={props.goalCalorie} />
-    </div>
+    <CalorieSummary calories={calcCaloriesSummary(props.meals)} />
     <ServingSummary serving={calcMealsServingSummary(props.meals)} />
   </div>
 )
--- src/components/summary/CalorieSummary.tsx
+++ src/components/summary/CalorieSummary.tsx
@@ -1,5 +1,5 @@
 export const CalorieSummary = (props: { calories: number; }) => (
-  <div className="d-flex flex-column flex-fill justify-content-center border rounded bg-info text-white text-center">
+  <div className="d-flex flex-column flex-fill justify-content-center border rounded bg-info text-white text-center p-1">
     <div style={{ fontSize: '40px' }}>{props.calories}</div>
     <div>calories</div>
   </div>
