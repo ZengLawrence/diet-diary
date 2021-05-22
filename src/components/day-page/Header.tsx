4cf5e6d67d58d6a61376a2891dfe3940f0f681cc
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-05-22 17:13:51 -0400

    ux

--- src/components/day-page/Header.tsx
+++ src/components/day-page/Header.tsx
@@ -7,12 +7,12 @@ export const Header = (props: { state: AppState; }) => {
   const { date, editMode, target, editTarget } = props.state;
 
   return (
-    <div className="d-flex flex-column flex-sm-row justify-content-between align-items-center">
-      <h1 className="order-sm-1">{date}</h1>
-      <div className="order-sm-0">
+    <div className="d-flex flex-column flex-md-row justify-content-between align-items-center">
+      <h1 className="order-md-1">{date}</h1>
+      <div className="order-md-0">
         <TargetPanel target={target} editMode={editMode} editTarget={editTarget} />
       </div>
-      <div className="order-sm-2">
+      <div className="order-md-2">
         {editMode && <NewDayButton />}{' '}
         <DayEditModeButton editMode={editMode} />
       </div>
