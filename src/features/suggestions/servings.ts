ba9f8581a6197989b5170f3ed699bb7a4425e6b7
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-05-30 12:05:20 -0400

    add content

--- src/features/suggestions/servings.ts
+++ src/features/suggestions/servings.ts
@@ -1,11 +1,23 @@
 import _ from "lodash";
 import { servings as fruitServings } from "./fruit-serving.json";
 import { servings as vegetableServings } from "./vegetable-serving.json";
+import { servings as carbohydrateServings } from "./carbohydrate-serving.json";
+import { servings as proteinDiaryServings } from "./protein-diary-serving.json";
+import { servings as fatServings } from "./fat-serving.json";
+import { servings as sweetServings } from "./sweet-serving.json";
 
 const vegetableServing = (serving: any) => ({ ...serving, foodGroup: "vegetable" });
 const fruitServing = (serving: any) => ({ ...serving, foodGroup: "fruit" });
+const carbohydrateServing = (serving: any) => ({ ...serving, foodGroup: "carbohydrate" });
+const proteinDiaryServing = (serving: any) => ({ ...serving, foodGroup: "proteinDiary" });
+const fatServing = (serving: any) => ({ ...serving, foodGroup: "fat" });
+const sweetServing = (serving: any) => ({ ...serving, foodGroup: "sweet" });
 
 export default _.concat(
   _.map(vegetableServings, vegetableServing),
   _.map(fruitServings, fruitServing),
+  _.map(carbohydrateServings, carbohydrateServing),
+  _.map(proteinDiaryServings, proteinDiaryServing),
+  _.map(fatServings, fatServing),
+  _.map(sweetServings, sweetServing),
 );
