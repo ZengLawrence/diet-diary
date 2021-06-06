1cb88f1a00d76ed2022bfb8b7fc5046b5238d678
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-06-06 11:12:30 -0400

    change to recursion

--- src/features/suggestions/search.ts
+++ src/features/suggestions/search.ts
@@ -28,11 +28,11 @@ const searchExpression = (words: string[]) => ({
 
 const match = (words: string[]) => fuse.search(searchExpression(words));
 
-const search = (words: string[], scorePredicate: (res: { score: number; }) => boolean) => {
+const search = (words: string[], scorePredicate: (res: { score: number; }) => boolean) : any[] => {
   const res = _.filter(match(words), scorePredicate);
   if (_.size(res) === 0 && _.size(words) > 1) {
     const dropLastWord = () => _.take(words, _.size(words) - 1);
-    return _.filter(match(dropLastWord()), scorePredicate);
+    return search(dropLastWord(), scorePredicate);
   }
   return res;
 };
