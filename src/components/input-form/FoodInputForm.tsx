0d7029170e64acc6dcd0f5dea0f6597db338bee8
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-06-12 09:42:05 -0400

    refactor

--- src/components/input-form/FoodInputForm.tsx
+++ src/components/input-form/FoodInputForm.tsx
@@ -1,29 +1,9 @@
-import _ from "lodash";
-import { Fragment } from "react";
 import { Button, Form } from "react-bootstrap";
-import { ServingSuggestion } from "../../features/suggestions/ServingSuggestion";
 import { calcFoodCalories, displayCalorieValue } from "../../model/calorieFunction";
 import { Food } from "../../model/Food";
-import { FoodGroupLabelBadge } from "../badge";
-import { BestChoiceLegend } from "../BestChoiceLegend";
-import { BlueStar } from "../BlueStar";
 import { ServingInputControl } from "./ServingInputControl";
+import { ServingSuggestionFormText } from "./ServingSuggestionFormText";
 import { useFoodInputFormStateReducer } from "./useFoodInputFormStateReducer";
-
-const hasBestChoice = (suggestions: ServingSuggestion[]) => _.findIndex(suggestions, { 'bestChoice': true }) >= 0;
-
-const ServingHintsText = (props: { suggestions: ServingSuggestion[] }) => {
-  const appendComma = (i: number) => i < (_.size(props.suggestions) - 1);
-  return (
-    <Fragment>
-      {props.suggestions.map(({ foodName, foodGroup, servingSize, bestChoice }, index) => (
-        <div key={index}>
-          {bestChoice && <BlueStar />}<span className="font-weight-bolder">{foodName}</span><FoodGroupLabelBadge foodGroup={foodGroup} /><span>{servingSize}{appendComma(index) && ","}&nbsp;</span>
-        </div>
-      ))}
-    </Fragment>
-  )
-}
 
 interface Props {
   food: Food;
@@ -56,15 +36,7 @@ export const FoodInputForm = (props: Props) => {
         <Form.Control.Feedback type="invalid">
           Please enter food name.
         </Form.Control.Feedback>
-        <Form.Text className="d-flex flex-column">
-          {_.size(suggestions) > 0 && <div>One Serving is</div>}
-          <div className="d-flex flex-column flex-sm-row flex-wrap w-100">
-            <ServingHintsText suggestions={suggestions} />
-          </div>
-          <div style={{ maxWidth: "100px" }}>
-            {hasBestChoice(suggestions) && <BestChoiceLegend />}
-          </div>
-        </Form.Text>
+        <ServingSuggestionFormText suggestions={suggestions} />
       </Form.Group>
 
       <Form.Group>
