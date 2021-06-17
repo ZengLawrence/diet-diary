a026cd767e64a43f8800168593dfa6d58b343889
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-06-17 17:24:34 -0400

    refactor

--- src/components/meal-card/MealCard.tsx
+++ src/components/meal-card/MealCard.tsx
@@ -5,8 +5,9 @@ import DeleteButton from "../../features/meal-card/DeleteButton";
 import DoneButton from "../../features/meal-card/DoneButton";
 import EditButton from "../../features/meal-card/EditButton";
 import NewFoodButton from "../../features/meal-card/NewFoodButton";
-import { MealState } from "../../model/AppState";
+import { MealEditState, MealState } from "../../model/AppState";
 import { calcMealCalories, displayCalorieValue } from "../../model/calorieFunction";
+import { Meal } from "../../model/Food";
 import { calcServingSummary } from "../../model/servingFunction";
 import { FoodGroupServingBadgePanel } from "../badge/FoodGroupServingBadgePanel";
 import { FoodGroupItems } from "./FoodGroupItems";
@@ -17,10 +18,9 @@ interface Props {
   editMode: boolean;
 }
 
-export const MealCard = (props: Props) => {
-  const { state, mealIndex, editMode } = props;
-  const { meal, editState, foodEditIndex } = state;
-  const { mealTime, foods } = meal;
+const MealCardHeader = (props: { meal: Meal; editState: MealEditState; mealIndex: number; editMode: boolean; }) => {
+  const { meal, editState, mealIndex, editMode } = props;
+  const { mealTime } = meal;
   const totalCalories = calcMealCalories(meal);
 
   const deleteButton = editMode &&
@@ -30,19 +30,30 @@ export const MealCard = (props: Props) => {
   const editModeButton = editMode &&
     (editState === "edit" ? <DoneButton mealIndex={mealIndex} /> : <EditButton mealIndex={mealIndex} />);
 
+
+  return (
+    <Card.Header className="d-flex flex-wrap align-items-center">
+      <div className="flex-fill order-sm-0">{mealTime}</div>
+      <div className="order-sm-2">
+        {deleteButton}&nbsp;
+        {editModeButton}
+      </div>
+      <div className="d-flex justify-content-between align-items-center order-sm-1 flex-grow-1 flex-md-grow-0">
+        <div className="mr-1">{displayCalorieValue(totalCalories)}{' '}Cal.</div>
+        <FoodGroupServingBadgePanel serving={calcServingSummary(meal)} />
+      </div>
+    </Card.Header>
+  );
+}
+
+export const MealCard = (props: Props) => {
+  const { state, mealIndex, editMode } = props;
+  const { meal, editState, foodEditIndex } = state;
+  const { foods } = meal;
+
   return (
     <Card className="mt-1">
-      <Card.Header className="d-flex flex-wrap align-items-center">
-        <div className="flex-fill order-sm-0">{mealTime}</div>
-        <div className="order-sm-2">
-          {deleteButton}&nbsp;
-          {editModeButton}
-        </div>
-        <div className="d-flex justify-content-between align-items-center order-sm-1 flex-grow-1 flex-md-grow-0">
-          <div className="mr-1">{displayCalorieValue(totalCalories)}{' '}Cal.</div>
-          <FoodGroupServingBadgePanel serving={calcServingSummary(meal)} />
-        </div>
-      </Card.Header>
+      <MealCardHeader meal={meal} editState={editState} mealIndex={mealIndex} editMode={editMode} />
 
       <ListGroup>
         <FoodGroupItems foods={foods} mealIndex={mealIndex} foodEditIndex={foodEditIndex} editState={editState} />
