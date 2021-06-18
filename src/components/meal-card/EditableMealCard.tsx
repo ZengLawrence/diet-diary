8ed4a65ddab1fc4a35e54b3f7da1eed52365c836
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-06-17 20:02:03 -0400

    refactor

---
+++ src/components/meal-card/EditableMealCard.tsx
@@ -0,0 +1,40 @@
+import _ from "lodash";
+import { Card, ListGroup } from "react-bootstrap";
+import AddFoodInputForm from "../../features/input-form/AddFoodInputForm";
+import NewFoodButton from "../../features/meal-card/NewFoodButton";
+import { MealState } from "../../model/AppState";
+import { calcMealCalories } from "../../model/calorieFunction";
+import { calcServingSummary } from "../../model/servingFunction";
+import { FoodGroupItems } from "./FoodGroupItems";
+import { MealCardHeader } from "./MealCardHeader";
+
+export const EditableMealCard = (props: { state: MealState; mealIndex: number; }) => {
+  const editMode = true;
+  const { state, mealIndex } = props;
+  const { meal, editState, foodEditIndex } = state;
+  const { mealTime, foods } = meal;
+
+  return (
+    <Card className="mt-1">
+      <MealCardHeader
+        mealTime={mealTime}
+        calories={calcMealCalories(meal)}
+        serving={calcServingSummary(meal)}
+        editState={editState}
+        mealIndex={mealIndex}
+        editMode={editMode} />
+
+      <ListGroup>
+        <FoodGroupItems foods={foods} mealIndex={mealIndex} foodEditIndex={foodEditIndex} editState={editState} />
+        {editState === 'edit' &&
+          <ListGroup.Item>
+            <NewFoodButton mealIndex={mealIndex} />
+          </ListGroup.Item>}
+        {editState === 'add' &&
+          <ListGroup.Item key={_.size(foods)}>
+            <AddFoodInputForm mealIndex={mealIndex} />
+          </ListGroup.Item>}
+      </ListGroup>
+    </Card>
+  );
+};
--- src/components/meal-card/MealCard.tsx
+++ src/components/meal-card/MealCard.tsx
@@ -1,12 +1,5 @@
-import _ from "lodash";
-import { Card, ListGroup } from "react-bootstrap";
-import AddFoodInputForm from "../../features/input-form/AddFoodInputForm";
-import NewFoodButton from "../../features/meal-card/NewFoodButton";
 import { MealState } from "../../model/AppState";
-import { calcMealCalories } from "../../model/calorieFunction";
-import { calcServingSummary } from "../../model/servingFunction";
-import { FoodGroupItems } from "./FoodGroupItems";
-import { MealCardHeader } from "./MealCardHeader";
+import { EditableMealCard } from "./EditableMealCard";
 import { UneditableMealCard } from "./UneditableMealCard";
 
 interface Props {
@@ -15,38 +8,6 @@ interface Props {
   editMode: boolean;
 }
 
-const EditableMealCard = (props: Props) => {
-  const { state, mealIndex, editMode } = props;
-  const { meal, editState, foodEditIndex } = state;
-  const { mealTime, foods } = meal;
-
-  return (
-    <Card className="mt-1">
-      <MealCardHeader
-        mealTime={mealTime}
-        calories={calcMealCalories(meal)}
-        serving={calcServingSummary(meal)}
-        editState={editState}
-        mealIndex={mealIndex}
-        editMode={editMode} />
-
-      <ListGroup>
-        <FoodGroupItems foods={foods} mealIndex={mealIndex} foodEditIndex={foodEditIndex} editState={editState} />
-        {editState === 'edit' &&
-          <ListGroup.Item>
-            <NewFoodButton mealIndex={mealIndex} />
-          </ListGroup.Item>
-        }
-        {editState === 'add' &&
-          <ListGroup.Item key={_.size(foods)}>
-            <AddFoodInputForm mealIndex={mealIndex} />
-          </ListGroup.Item>
-        }
-      </ListGroup>
-    </Card >
-  );
-}
-
 export const MealCard = (props: Props) => (
-  props.editMode ? <EditableMealCard state={props.state} mealIndex={props.mealIndex} editMode={props.editMode} /> : <UneditableMealCard meal={props.state.meal}/>
+  props.editMode ? <EditableMealCard state={props.state} mealIndex={props.mealIndex} /> : <UneditableMealCard meal={props.state.meal} />
 )
