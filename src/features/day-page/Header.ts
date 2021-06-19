fe962dc77a66ecbb39a2509dbbe9b84c1980559f
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-06-18 21:21:10 -0400

    use selector

--- src/features/day-page/Header.ts
+++ src/features/day-page/Header.ts
@@ -1,10 +1,11 @@
 import { connect } from "react-redux";
+import { dateSelector, editModeSelector } from "../../app/selectors";
 import { RootState } from "../../app/store";
 import { Header } from "../../components/day-page/Header";
 
 const mapStateToProps = (state: RootState) => ({
-  date: state.date,
-  editMode: state.editMode,
+  date: dateSelector(state),
+  editMode: editModeSelector(state),
 })
 
 export default connect(mapStateToProps)(Header);
