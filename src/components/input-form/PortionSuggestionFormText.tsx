02c598804a96e2b4b3f1909d02d4500062b64c61
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-06-19 17:13:07 -0400

    refactor

--- src/components/input-form/PortionSuggestionFormText.tsx
+++ src/components/input-form/PortionSuggestionFormText.tsx
@@ -1,29 +1,30 @@
 import _ from "lodash";
-import { Fragment } from "react";
 import { Form } from "react-bootstrap";
 import { PortionSuggestion } from "../../features/suggestions";
 import { calcServingCalories } from "../../model/calorieFunction";
 import { FoodGroupServingBadgePanel } from "../badge/FoodGroupServingBadgePanel";
 import { CalorieSpan } from "../CalorieSpan";
 
-const ServingHintsText = (props: { suggestions: PortionSuggestion[]; }) => (
-  <Fragment>
-    {props.suggestions.map(({ foodName, portionSize, serving }, index) => (
-      <div key={index}>
-        <span className="font-weight-bolder">{foodName}</span>&nbsp;
-        <span>{portionSize}</span>
-        <FoodGroupServingBadgePanel serving={serving} />
-          (<CalorieSpan value={calcServingCalories(serving)} />)
-      </div>
-    ))}
-  </Fragment>
-)
+const PortionServingHint = (props: { suggestion: PortionSuggestion }) => {
+  const { foodName, portionSize, serving } = props.suggestion;
+
+  return (
+    <span>
+      <span className="font-weight-bolder">{foodName}</span>&nbsp;
+      <span>{portionSize}</span>
+      <FoodGroupServingBadgePanel serving={serving} />
+      (<CalorieSpan value={calcServingCalories(serving)} />)
+    </span>
+  )
+}
 
 export const PortionSuggestionFormText = (props: { suggestions: PortionSuggestion[]; }) => (
   <Form.Text className="d-flex flex-column">
     {_.size(props.suggestions) > 0 && <div>One portion is</div>}
     <div className="d-flex flex-column flex-wrap w-100">
-      <ServingHintsText suggestions={props.suggestions} />
+      {props.suggestions.map((suggestion, index) => (
+        <PortionServingHint key={index} suggestion={suggestion} />
+      ))}
     </div>
   </Form.Text>
 )
