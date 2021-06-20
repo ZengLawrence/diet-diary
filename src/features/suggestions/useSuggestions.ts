5211dc5402206f0dc18b827c5e3f9c5db6dd0be1
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-06-20 10:49:17 -0400

    use reducer

--- src/features/suggestions/useSuggestions.ts
+++ src/features/suggestions/useSuggestions.ts
@@ -1,16 +1,48 @@
+import { createAction } from "@reduxjs/toolkit";
 import _ from "lodash";
-import { useEffect, useRef, useState } from "react";
-import { ServingSuggestion } from "./ServingSuggestion";
+import { useEffect, useReducer, useRef } from "react";
 import { generatePortionSuggestions, generateServingSuggestions } from "./generateServingSuggestions";
 import { PortionSuggestion } from "./PortionSuggestion";
+import { ServingSuggestion } from "./ServingSuggestion";
 
 const debouncedGenerateServingSuggestions = _.debounce(generateServingSuggestions, 500, { maxWait: 2000 });
 
 const debouncedGeneratePortionSuggestions = _.debounce(generatePortionSuggestions, 500, { maxWait: 2000 });
 
+interface State {
+  servingSuggestions: ServingSuggestion[],
+  portionSuggestions: PortionSuggestion[],
+}
+
+const setServingSuggestionsAction = createAction<ServingSuggestion[]>("setServingSuggestions");
+type SetServingSuggestionsAction = ReturnType<typeof setServingSuggestionsAction>;
+
+const setPortionSuggestionsAction = createAction<PortionSuggestion[]>("portionSuggestions");
+type SetPortionSuggestionsAction = ReturnType<typeof setPortionSuggestionsAction>;
+
+type Action = SetServingSuggestionsAction | SetPortionSuggestionsAction;
+
+const initialState = (): State => ({
+  servingSuggestions: [],
+  portionSuggestions: [],
+})
+
+function reducer(state: State, action: Action) {
+  switch (action.type) {
+    case _.toString(setServingSuggestionsAction):
+      return { ...state, servingSuggestions: (action as SetServingSuggestionsAction).payload };
+    case _.toString(setPortionSuggestionsAction):
+      return { ...state, portionSuggestions: (action as SetPortionSuggestionsAction).payload };
+    default:
+      throw new Error();
+  }
+}
+
 export const useSuggestions = (initialDescription: string) => {
-  const [servingSuggestions, setServingSuggestions] = useState([] as ServingSuggestion[]);
-  const [portionSuggestions, setPortionSuggestions] = useState([] as PortionSuggestion[]);
+  const [state, dispatch] = useReducer(reducer, initialState());
+  const setServingSuggestions = (servingSuggestions: ServingSuggestion[]) => dispatch(setServingSuggestionsAction(servingSuggestions));
+  const setPortionSuggestions = (portionSuggestions: PortionSuggestion[]) => dispatch(setPortionSuggestionsAction(portionSuggestions));
+
   const descRef = useRef(initialDescription);
 
   const generateSuggestions = (desc: string) => {
@@ -23,11 +55,10 @@ export const useSuggestions = (initialDescription: string) => {
   useEffect(() => {
     generateServingSuggestions(descRef, setServingSuggestions);
     generatePortionSuggestions(descRef, setPortionSuggestions);
-  }, [descRef, setServingSuggestions])
-  
+  }, [descRef, dispatch])
+
   return {
-    servingSuggestions,
-    portionSuggestions,
+    ...state,
     generateSuggestions
   };
 }
