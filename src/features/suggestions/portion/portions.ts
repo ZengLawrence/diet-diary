204c6537fdd492731b5a531640d7e19b50ddf3c4
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-06-26 18:57:47 -0400

    split portion data file

--- src/features/suggestions/portion/portions.ts
+++ src/features/suggestions/portion/portions.ts
@@ -1,3 +1,17 @@
-import { portions } from "./portion-serving.json";
+import _ from "lodash";
+import { PortionSuggestion } from "../PortionSuggestion";
+import { portions as breakfastPortions } from "./breakfast-portion.json";
+import { portions as sandwichPortions } from "./sandwich-portion.json";
+import { portions as saladPortions } from "./salad-portion.json";
+import { portions as soupPortions } from "./soup-portion.json";
+import { portions as mainCoursePortions } from "./main-course-portion.json";
+import { portions as sideDishPortions } from "./side-dish-portion.json";
 
-export default portions;
+export default _.concat(
+  breakfastPortions as PortionSuggestion[],
+  sandwichPortions as PortionSuggestion[],
+  saladPortions as PortionSuggestion[],
+  soupPortions as PortionSuggestion[],
+  mainCoursePortions as PortionSuggestion[],
+  sideDishPortions as PortionSuggestion[]
+);
