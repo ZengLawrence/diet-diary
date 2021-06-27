d0f6481db2c6d7fd0fab6a608125aa03f61cb871
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-06-27 15:08:46 -0400

    1% milk

---
+++ src/features/suggestions/foodName.test.ts
@@ -0,0 +1,18 @@
+import _ from "lodash";
+import { foodName } from "./foodName";
+
+test("search for proper formatted food description '<food name> <serving size>' e.g. 'banana 1 small'", () => {
+  expect(foodName("banana 1 small")).toBe("banana");
+})
+
+test("search for food description with serving size but no unit e.g. 'banana 1'", () => {
+  expect(foodName("banana 1")).toBe("banana");
+})
+
+test("search for food description with no serving size e.g. 'banana'", () => {
+  expect(foodName("banana")).toBe("banana");
+})
+
+test("search for food description with number in its name e.g. '1% milk 1 cup'", () => {
+  expect(foodName("1% milk 1 cup")).toBe("1% milk");
+})
--- src/features/suggestions/foodName.ts
+++ src/features/suggestions/foodName.ts
@@ -1,3 +1,3 @@
 import _ from "lodash";
 
-export const foodName = (phrase: string) => _.trim(_.head(_.split(phrase, /\d/, 1)));
+export const foodName = (phrase: string) => _.trim(_.head(_.split(phrase, /\s{1}\d/, 1)));
