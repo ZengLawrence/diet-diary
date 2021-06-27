edec9efdff6ef0f4044af12953e3aad2900afccb
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-06-27 16:38:39 -0400

    initial version

---
+++ src/components/name-food-form/NameFoodForm.tsx
@@ -0,0 +1,36 @@
+import { Form } from "react-bootstrap";
+import { Food } from "../../model/Food";
+import { SecondaryButton } from "../buttons/SecondaryButton";
+import { SubmitButton } from "../buttons/SubmitButton";
+
+interface Props {
+  food: Food;
+}
+
+export const NameFoodFrom = (props: Props) => (
+  <Form
+    noValidate
+    onSubmit={() => { }}
+    className="border p-1"
+  >
+
+    <Form.Group as={Form.Row} className="ml-1 mr-1">
+      <Form.Label htmlFor="inputFoodName" srOnly>Food name</Form.Label>
+      <Form.Control
+        id="inputFoodName"
+        type="text"
+        value={props.food.name}
+        required
+      />
+    </Form.Group>
+
+    <div className="d-flex justify-content-end">
+      <div className="mr-1 order-sm-1">
+        <SecondaryButton label="Cancel" onClick={() => { }} />
+      </div>
+      <div className="mr-1 order-sm-0" >
+        <SubmitButton label="Add" />
+      </div>
+    </div>
+  </Form>
+)
