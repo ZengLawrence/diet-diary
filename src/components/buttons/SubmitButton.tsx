0daec2db35b7c03d4a02a70607d1d413ca295f8f
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-06-27 16:21:22 -0400

    not needed

--- src/components/buttons/SubmitButton.tsx
+++ src/components/buttons/SubmitButton.tsx
@@ -1,10 +1,9 @@
 import { Button } from "react-bootstrap";
 
-export const SubmitButton = (props: { label: string; onClick: (e: React.MouseEvent<HTMLButtonElement>) => void; }) => (
+export const SubmitButton = (props: { label: string; }) => (
   <Button
     variant="outline-primary"
     type="submit"
-    onClick={props.onClick}
   >
     {props.label}
   </Button>
