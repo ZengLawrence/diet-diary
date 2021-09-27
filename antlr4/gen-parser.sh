d6557fe0efde6be102bd839f3fd653f2a759f3f7
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-09-27 17:17:24 -0400

    unneeded

--- antlr4/gen-parser.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-echo $DIR
-shopt -s expand_aliases
-source $DIR/bash.profile
-antlr4 -Dlanguage=JavaScript $DIR/food-description/FoodDescription.g4 -o .
