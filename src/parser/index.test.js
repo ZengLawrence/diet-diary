42359ee337a8d518dcb3a85a0ca658f2914d68b7
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-09-27 20:18:03 -0400

    add unit

--- src/parser/index.test.js
+++ src/parser/index.test.js
@@ -4,7 +4,10 @@ test("brocoli 1 cup", () => {
   expect(parseFoodDescription("brocoli 1 cup")).toMatchObject(
     {
       foodName: "brocoli",
-      measurement: { number: "1" }
+      measurement: { 
+        number: "1",
+        unit: "cup"
+      }
     }
   );
 })
