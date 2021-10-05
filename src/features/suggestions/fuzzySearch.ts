da53c40e5a2ac6f0afe2ec701b0ccc5084c05099
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-10-05 09:22:51 -0400

    remove search by words

--- src/features/suggestions/fuzzySearch.ts
+++ src/features/suggestions/fuzzySearch.ts
@@ -1,44 +1,19 @@
 import Fuse from "fuse.js";
 import _ from "lodash";
 
-const searchExpression = (words: string[]) => ({
-  $and: _.map(words, w => ({ "foodName": w }))
-})
-
-const dropLastWord = (phrase: string) => {
-  const lastWord = _.last(_.words(phrase)) || "";
-  const lastWordIndex = phrase.lastIndexOf(lastWord);
-  return _.trim(phrase.substring(0, lastWordIndex));
-}
-
 type ScorePredicate = (res: { score: number; }) => boolean;
 
 function searchByPhrase<T>(fuse: Fuse<T>, phrase: string, scorePredicate: ScorePredicate) {
   const match = (phrase: string) => fuse.search(phrase);
-  return _.filter(match(phrase), scorePredicate);
-}
-
-function searchByWords<T>(fuse: Fuse<T>, words: string[], scorePredicate: ScorePredicate) {
-  const match = (words: string[]) => fuse.search(searchExpression(words));
-  return _.filter(match(words), scorePredicate);
+  return _.filter(match(phrase), scorePredicate) as { item: T; score?: number; }[];
 }
 
 export function search<T>(
   fuse: Fuse<T>,
   foodName: string,
   scorePredicate: ScorePredicate = () => true
-): { item: T; score?: number; }[] {
-
-  const resByPhrase = searchByPhrase(fuse, foodName, scorePredicate) as { item: T; score?: number; }[];
-  if (_.size(resByPhrase) > 0) {
-    return resByPhrase;
-  }
-  const words = _.words(foodName);
-  const resByWords = searchByWords(fuse, words, scorePredicate) as { item: T; score?: number; }[];
-  if (_.size(resByWords) === 0 && _.size(words) > 1) {
-    return search(fuse, dropLastWord(foodName), scorePredicate);
-  }
-  return resByWords;
+) {
+  return searchByPhrase(fuse, foodName, scorePredicate);
 }
 
 export const scoreLessThan = (limit: number) => {
