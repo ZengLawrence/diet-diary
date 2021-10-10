56e9014e041c43f6651aecfd0656a73c7624bc58
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-10-10 19:19:12 -0400

    simplify

--- src/components/input-form/separateSuggestions.ts
+++ src/components/input-form/separateSuggestions.ts
@@ -2,25 +2,17 @@ import _ from "lodash";
 import { PortionSuggestion, ServingSuggestion } from "../../features/suggestions";
 import { Selectable } from "../../model/Selectable";
 
-function separate(
-  result: { 
-    servingSuggestions: (ServingSuggestion & Selectable)[]; 
-    portionSuggestions: (PortionSuggestion & Selectable)[]; 
-  }, 
-  suggestion: ((ServingSuggestion | PortionSuggestion) & Selectable)) {
-  if ("servingSize" in suggestion) {
-    return {
-      ...result,
-      servingSuggestions: [...result.servingSuggestions, suggestion as (ServingSuggestion & Selectable)]
-    };
-  } else {
-    return {
-      ...result,
-      portionSuggestions: [...result.portionSuggestions, suggestion as (PortionSuggestion & Selectable)]
-    };
-  }
+function isServingSuggestion(suggestion: (ServingSuggestion | PortionSuggestion)) : suggestion is ServingSuggestion {
+  return "servingSize" in suggestion;
+}
+
+function isPortionSuggestion(suggestion: (ServingSuggestion | PortionSuggestion)) : suggestion is PortionSuggestion {
+  return "portionSize" in suggestion;
 }
 
 export function separateSuggestions(suggestions: ((ServingSuggestion | PortionSuggestion) & Selectable)[]) {
-  return _.reduce(suggestions, separate, { servingSuggestions: [], portionSuggestions: [] });
+  return { 
+    servingSuggestions: _.filter(suggestions, isServingSuggestion) as (ServingSuggestion & Selectable)[],
+    portionSuggestions: _.filter(suggestions, isPortionSuggestion) as (PortionSuggestion & Selectable)[],
+  };
 }
