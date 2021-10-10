00197c1751a88b19b512bc50cc75d7c18f601dcc
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-10-10 10:22:52 -0400

    combine search for serving and portion

---
+++ src/features/suggestions/search/search.ts
@@ -0,0 +1,11 @@
+import _ from "lodash";
+import portions from "../portion/portions";
+import { PortionSuggestion } from "../portion/PortionSuggestion";
+import { buildDocuments, search } from "../search/miniSearchEngine";
+import servings from "../serving/servings";
+import { ServingSuggestion } from "../serving/ServingSuggestion";
+
+const suggestions = buildDocuments<ServingSuggestion | PortionSuggestion>(_.concat(servings, portions));
+
+export const searchFoodServingPortionSize = (foodName: string) =>
+  _.slice(search(suggestions, foodName), 0, 2);
