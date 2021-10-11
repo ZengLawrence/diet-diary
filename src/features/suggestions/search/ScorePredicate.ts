937b3472d8d1a598a55cede5e4b87d8141073825
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-10-11 17:04:20 -0400

    unused

--- src/features/suggestions/search/ScorePredicate.ts
+++
@@ -1,7 +0,0 @@
-export type ScorePredicate = (res: { score: number; }) => boolean;
-
-export const scoreLessThan = (limit: number) => {
-  return function ({ score }: { score: number; }) {
-    return score < limit;
-  };
-};
