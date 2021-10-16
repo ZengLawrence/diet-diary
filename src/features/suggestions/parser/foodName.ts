613f8b487d67efbcf773a44605db2f390d7bd1f7
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-10-16 15:52:48 -0400

    unused

--- src/features/suggestions/parser/foodName.ts
+++
@@ -1,6 +0,0 @@
-import {parseFoodDescription} from "./foodDescription";
-
-export const foodName = (phrase: string) => {
-  const desc = parseFoodDescription(phrase) as {foodName: string};
-  return desc.foodName;
-}
