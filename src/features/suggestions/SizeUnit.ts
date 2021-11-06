77e90744ec35b8834d1b7347922e6bbb314793f8
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-11-06 11:48:06 -0400

    Add size units

---
+++ src/features/suggestions/SizeUnit.ts
@@ -0,0 +1,40 @@
+import { Measure, Unit } from "convert-units";
+
+export type SizeSystems = "size";
+
+export type SizeUnits =
+  | "small"
+  | "medium"
+  | "large";
+
+const size: Record<SizeUnits, Unit> = {
+  small: {
+    name: {
+      singular: 'Small',
+      plural: 'Small',
+    },
+    to_anchor: 0.75,
+  },
+  medium: {
+    name: {
+      singular: 'Medium',
+      plural: 'Medium',
+    },
+    to_anchor: 1,
+  },
+  large: {
+    name: {
+      singular: 'Large',
+      plural: 'Large',
+    },
+    to_anchor: 1.5,
+  },
+}
+
+const measure: Measure<SizeSystems, SizeUnits> = {
+  systems: {
+    size,
+  },
+};
+
+export default measure;
