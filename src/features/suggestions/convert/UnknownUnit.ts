df032c2de93d973c69f5656043b411cf9d5a2b88
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-11-11 11:26:28 -0500

    Add unknow unit for explicit intent

---
+++ src/features/suggestions/convert/UnknownUnit.ts
@@ -0,0 +1,23 @@
+import { Measure, Unit } from "convert-units";
+
+export type UnknownSystems = "unknown";
+
+export type UnknownUnits = "unknown";
+
+const unknown: Record<UnknownUnits, Unit> = {
+  unknown: {
+    name: {
+      singular: 'Unknown',
+      plural: 'Unknown',
+    },
+    to_anchor: 1,
+  },
+}
+
+const measure: Measure<UnknownSystems, UnknownUnits> = {
+  systems: {
+    unknown,
+  },
+};
+
+export default measure;
