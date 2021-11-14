4264490b7527ea3cb77de13fbb68997f5c81e835
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-11-14 17:53:22 -0500

    Update documentation

--- src/features/suggestions/convert/ConvertFunctions.ts
+++ src/features/suggestions/convert/ConvertFunctions.ts
@@ -1,5 +1,16 @@
 export interface ConvertFunctions<T> {
+  /**
+   * Type guard function to check the type before calling areUnitsConvertible and convert functions.
+   */
   isSupportedUnitType: (unit: any) => boolean;
+
+  /**
+   * Check if units are convertible.  It is used safe guard calling convert function.
+   */
   areUnitsConvertible: (fromUnit: T, toUnit: T) => boolean;
+
+  /**
+   * Convert quantity from one unit to another.  Should call areUnitsConvertible to check if units are convertible before calling this function.
+   */
   convert: (quantity: number, unit: T, toUnit: T) => number;
 }
