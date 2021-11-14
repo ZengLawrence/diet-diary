7e09bf18234f7f1def40b1005f79f3a287532ed8
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-11-14 14:41:31 -0500

    Make composite parser generic for reuse

--- src/features/suggestions/convert/CompositeParserFunctions.ts
+++ src/features/suggestions/convert/CompositeParserFunctions.ts
@@ -1,21 +1,29 @@
+import _ from "lodash";
 import { ParserFunctions } from "./ParserFunctions";
-import { StandardUnitParserFunctions } from "./standard-unit";
-import { VariableUnitParseFunctions } from "./variable-unit";
 
-function canParse(unitText: string | undefined) {
-  return VariableUnitParseFunctions.canParse(unitText)
-    || StandardUnitParserFunctions.canParse(unitText);
+function selectFunction<T>(funcs: ParserFunctions<T>[], unitText: string | undefined) {
+  return _.head(_.filter(funcs, function({canParse}) {
+    return canParse(unitText);
+  }));
 }
 
-function parse(unitText: string | undefined) {
-  if (VariableUnitParseFunctions.canParse(unitText)) {
-    return VariableUnitParseFunctions.parse(unitText);
+function canParse<T>(funcs: ParserFunctions<T>[], unitText: string | undefined) {
+  const selectedFunc = selectFunction(funcs, unitText);
+  return selectedFunc ? true : false;
+}
+
+function parse<T>(funcs: ParserFunctions<T>[], unitText: string | undefined) {
+  const selectedFunc = selectFunction(funcs, unitText);
+  if (selectedFunc) {
+    return selectedFunc.parse(unitText);
+  } else {
+    return _.last(funcs)!.parse(unitText);
   }
-  return StandardUnitParserFunctions.parse(unitText);
 }
-type Unit = ReturnType<typeof parse>;
 
-export const CompositeParserFunctions: ParserFunctions<Unit> = {
-  canParse,
-  parse,
-};
+export default function compose<T>(...funcs: ParserFunctions<T>[]): ParserFunctions<T> {
+  return {
+    canParse: _.partial(canParse, funcs),
+    parse: _.partial(parse, funcs),
+  };
+}
--- src/features/suggestions/convert/index.ts
+++ src/features/suggestions/convert/index.ts
@@ -1,9 +1,11 @@
 import { CompositeConvertFunctions } from "./CompositeConvertFunctions";
-import { CompositeParserFunctions } from "./CompositeParserFunctions";
+import composeParser from "./CompositeParserFunctions";
+import { StandardUnit, StandardUnitParserFunctions } from "./standard-unit";
+import { VariableUnit, VariableUnitParseFunctions } from "./variable-unit";
 
 export const isMeasurementConvertible = CompositeConvertFunctions.isMeasurementConvertible;
-export const parseUnit = CompositeParserFunctions.parse;
-export type Unit = ReturnType<typeof parseUnit>;
+export type Unit = StandardUnit | VariableUnit;
+export const parseUnit = composeParser<Unit>(VariableUnitParseFunctions, StandardUnitParserFunctions).parse;
 
 const convert = CompositeConvertFunctions.convert;
 export default convert;
