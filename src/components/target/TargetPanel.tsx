41fc144b3b6df55076da32b19ed21cc24c86c076
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-11-20 12:39:45 -0500

    Show no target label on UI

--- src/components/target/TargetPanel.tsx
+++ src/components/target/TargetPanel.tsx
@@ -1,22 +1,36 @@
+import { Fragment } from "react";
 import ChangeTargetButton from "../../features/target/ChangeTargetButton";
 import TargetDropDown from "../../features/target/TargetDropDown";
 import TargetFoodGroupServingGoalBadgePanel from "../../features/target/TargetFoodGroupServingGoalBadgePanel";
 import TargetLabel from "../../features/target/TargetLabel";
 
-export const TargetPanel = (props: { editMode: boolean; editTarget: boolean }) => {
-  const { editMode, editTarget } = props;
-  const showChangeTargetButton = (editMode && !editTarget);
-
+const TargetCaloriePanel = (props: { editTarget: boolean }) => {
+  const { editTarget } = props;
   const caloriePanel = editTarget ? <TargetDropDown /> : <TargetLabel />;
+  return (
+    <div>
+      <div className="d-flex flex-nowrap  align-items-end">
+        Target:&nbsp; {caloriePanel} &nbsp;Cal.
+      </div>
+      <TargetFoodGroupServingGoalBadgePanel />
+    </div>
+  )
+}
+
+const NoTargetPanel = (props: { editTarget: boolean }) => {
+  const { editTarget } = props;
+  return (
+    (editTarget ? <TargetDropDown /> : <Fragment>No Target</Fragment>)
+  )
+}
+
+export const TargetPanel = (props: { editMode: boolean; editTarget: boolean; noTarget: boolean; }) => {
+  const { editMode, editTarget, noTarget } = props;
+  const showChangeTargetButton = (editMode && !editTarget);
 
   return (
     <div className="d-flex align-items-center">
-      <div>
-        <div className="d-flex flex-nowrap  align-items-end">
-          Target:&nbsp; {caloriePanel} &nbsp;Cal.
-        </div>
-        <TargetFoodGroupServingGoalBadgePanel />
-      </div>&nbsp;
+      {noTarget ? <NoTargetPanel editTarget={editTarget} /> : <TargetCaloriePanel editTarget={editTarget} />}&nbsp;
       {showChangeTargetButton && <ChangeTargetButton />}
     </div>
   );
