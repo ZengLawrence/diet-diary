b82e0a74b8c9f87278a8059f998b484eef53389f
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-11-21 11:00:30 -0500

    Use composition for better reuse

--- src/features/download/DownloadButton.tsx
+++ src/features/download/DownloadButton.tsx
@@ -1,14 +1,19 @@
+import { Button } from "react-bootstrap";
 import { useAppSelector } from "../../app/hooks";
 import { diarySelector } from "../../app/selectors";
-import { SecondaryButton } from "../../components/buttons/SecondaryButton";
+import { VariantSecondary } from "../../components/buttons/ButtonVariant";
 import downloadAsCsv from "./exportCsv";
 
+/**
+ * Special implementation because it needs access to the app state directly.
+ * 
+ * @returns Download button
+ */
 export const DownloadButton = () => {
   const {date, meals} = useAppSelector(diarySelector);
-
   const handleClicked = () => downloadAsCsv(date, meals)
 
   return (
-    <SecondaryButton label="Download" onClick={handleClicked} />
+    <Button variant={VariantSecondary} onClick={handleClicked}>Download</Button>
   )
 }
