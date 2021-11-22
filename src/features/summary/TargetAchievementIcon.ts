60c57cd3f2e043070d26a950690564a69d88921b
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-11-22 17:23:49 -0500

    Refactor functions for reuse and simplify

--- src/features/summary/TargetAchievementIcon.ts
+++ src/features/summary/TargetAchievementIcon.ts
@@ -4,13 +4,30 @@ import { targetSelector, totalServingSelector } from "../../app/selectors";
 import { RootState } from "../../app/store";
 import { TargetAchievementIcon, TargetAction } from "../../components/summary/TargetAchievementIcon";
 import { FoodGroup, Serving } from "../../model/Food";
-import { isMinLimit, Target } from "../../model/Target";
+import { isMinLimit } from "../../model/Target";
 
-function getAction(totalServing: Serving, foodGroup: FoodGroup, target: Target): TargetAction {
-  const serving = _.defaultTo(_.get(totalServing, foodGroup), 0);
-  const targetServing = _.defaultTo(_.get(target.serving, foodGroup), 0);
+function getServingForFoodGroup(serving: Serving, foodGroup: FoodGroup) {
+  return _.defaultTo(_.get(serving, foodGroup), 0);
+}
+
+function getToTarget(serving: number, targetServing: number) {
 
   if (serving > targetServing) {
+    return "more"
+  }
+
+  if (serving < targetServing) {
+    return "less";
+  }
+
+  return "same";
+}
+
+type ToTarget = ReturnType<typeof getToTarget>;
+
+function getAction(toTarget: ToTarget, foodGroup: FoodGroup): TargetAction {
+
+  if (toTarget === "more") {
     if (isMinLimit(foodGroup)) {
       return "MeetTarget";
     } else {
@@ -18,7 +35,7 @@ function getAction(totalServing: Serving, foodGroup: FoodGroup, target: Target):
     }
   }
 
-  if (serving < targetServing) {
+  if (toTarget === "less") {
     if (foodGroup === "sweet") return "MeetTarget";
     return "DoMore";
   }
@@ -27,8 +44,15 @@ function getAction(totalServing: Serving, foodGroup: FoodGroup, target: Target):
   return "MeetTarget"
 }
 
-const mapStateToProps = (state: RootState, ownProps: { foodGroup: FoodGroup }) => ({
-  action: getAction(totalServingSelector(state), ownProps.foodGroup, targetSelector(state)),
-})
+const mapStateToProps = (state: RootState, ownProps: { foodGroup: FoodGroup }) => {
+  const { foodGroup } = ownProps;
+  const serving = getServingForFoodGroup(totalServingSelector(state), foodGroup);
+  const targetServing = getServingForFoodGroup(targetSelector(state).serving, foodGroup);
+  const toTarget = getToTarget(serving, targetServing);
+  const action = getAction(toTarget, foodGroup);
+  return {
+    action,
+  }
+}
 
 export default connect(mapStateToProps)(TargetAchievementIcon);
