5b08d10173a876ad4dad50e6f76a3868e9fb09e4
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-11-23 11:35:57 -0500

    Extract functions for readability

--- src/components/day-page/HeaderButtons.tsx
+++ src/components/day-page/HeaderButtons.tsx
@@ -1,15 +1,30 @@
+import { Fragment } from "react";
 import DoneButton from "../../features/day-page/DoneButton";
 import EditButton from "../../features/day-page/EditButton";
 import NewDayButton from "../../features/day-page/NewDayButton";
 import { DownloadButton } from "../../features/download/DownloadButton";
 
+const EditableViewButtons = () => (
+  <Fragment>
+    <NewDayButton />{' '}
+    <DoneButton />
+  </Fragment>
+)
+
+const UneditableViewButtons = (props: { showDownloadButton: boolean; }) => (
+  <Fragment>
+    {props.showDownloadButton ? <DownloadButton /> : ""}{' '}
+    <EditButton />
+  </Fragment>
+)
+
 export const HeaderButtons = (props: { editMode: boolean; showDownloadButton: boolean; }) => {
-  const {editMode, showDownloadButton} = props;
-  const downloadButton = showDownloadButton ? <DownloadButton /> : "";
+  const { editMode, showDownloadButton } = props;
   return (
     <span>
-      {editMode ? <NewDayButton /> : downloadButton}{' '}
-      {editMode ? <DoneButton /> : <EditButton />}
+      {editMode ?
+        <EditableViewButtons />
+        : <UneditableViewButtons showDownloadButton={showDownloadButton} />}
     </span>
-  );
+  )
 }
