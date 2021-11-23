3ea3b1cb6b7d13e5b1c633fc935ec080b0126492
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-11-23 14:47:31 -0500

    Move styling to component for cohesion

--- src/components/target/TargetPanel.tsx
+++ src/components/target/TargetPanel.tsx
@@ -1,6 +1,7 @@
 import ChangeTargetButton from "../../features/target/ChangeTargetButton";
 import NoTargetPanel from "../../features/target/NoTargetPanel";
 import TargetCaloriePanel from "../../features/target/TargetCaloriePanel";
+import { VariantSecondary } from "../buttons/ButtonVariant";
 
 interface Props {
   noTarget: boolean;
@@ -13,7 +14,8 @@ export const TargetPanel = (props: Props) => {
   return (
     <div className="d-flex align-items-center">
       {noTarget ? <NoTargetPanel /> : <TargetCaloriePanel />}&nbsp;
-      {showChangeTargetButton && <ChangeTargetButton>Change</ChangeTargetButton>}
+      {showChangeTargetButton
+        && <ChangeTargetButton variant={VariantSecondary}>Change</ChangeTargetButton>}
     </div>
   );
 };
--- src/features/target/ChangeTargetButton.ts
+++ src/features/target/ChangeTargetButton.ts
@@ -1,15 +1,11 @@
 import { Button } from "react-bootstrap";
 import { connect } from "react-redux";
 import { AppDispatch } from "../../app/store";
-import { VariantSecondary } from "../../components/buttons/ButtonVariant";
 import { enterEditTarget } from "./targetStateSlice";
-
-const mapStateToProps = () => ({
-  variant: VariantSecondary,
-})
 
 const mapDispatchToProps = (dispatch: AppDispatch) => ({
   onClick: () => dispatch(enterEditTarget()),
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(Button);
+export default connect(null, mapDispatchToProps)(Button);
+
