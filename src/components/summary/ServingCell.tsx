8627412740f27332d2736d0fbf3a6b684999ec88
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-11-22 20:24:41 -0500

    Extract component for injecting property

---
+++ src/components/summary/ServingCell.tsx
@@ -0,0 +1,34 @@
+import { Fragment } from "react";
+import TargetAchievementIcon from "../../features/summary/TargetAchievementIcon";
+import { FoodGroup } from "../../model/Food";
+import { displayServingValue } from "../../model/servingFunction";
+import { FoodGroupLabelBadge } from "../badge";
+import { FoodGroupLegend } from "../FoodGroupLegend";
+
+const CalorieText = (props: { amount?: number; }) => (
+  <Fragment>
+    <div className="d-block d-sm-none">{displayServingValue(props.amount)}</div>
+    <div className="d-none d-sm-block text-center" style={{ fontSize: '32px', minWidth: '110px' }}>{displayServingValue(props.amount)}</div>
+  </Fragment>
+);
+
+const FoodGroupLabel = (props: { foodGroup: FoodGroup; }) => (
+  <Fragment>
+    <div className="d-block d-sm-none">
+      <FoodGroupLabelBadge foodGroup={props.foodGroup} />
+    </div>
+    <div className="d-none d-sm-block">
+      <FoodGroupLegend foodGroup={props.foodGroup} />
+    </div>
+  </Fragment>
+);
+
+export const ServingCell = (props: { foodGroup: FoodGroup; amount?: number; }) => (
+  <div className="d-flex flex-column align-items-center m-1">
+    <CalorieText amount={props.amount} />
+    <div className="d-flex align-items-center">
+      <TargetAchievementIcon foodGroup={props.foodGroup} />
+      <FoodGroupLabel foodGroup={props.foodGroup} />
+    </div>
+  </div>
+);
--- src/components/summary/ServingSummary.tsx
+++ src/components/summary/ServingSummary.tsx
@@ -1,37 +1,5 @@
-import { Fragment } from "react";
-import TargetAchievementIcon from "../../features/summary/TargetAchievementIcon";
-import { FoodGroup, Serving } from "../../model/Food";
-import { displayServingValue } from "../../model/servingFunction";
-import { FoodGroupLabelBadge } from "../badge";
-import { FoodGroupLegend } from "../FoodGroupLegend";
-
-const CalorieText = (props: { amount?: number; }) => (
-  <Fragment>
-    <div className="d-block d-sm-none">{displayServingValue(props.amount)}</div>
-    <div className="d-none d-sm-block text-center" style={{ fontSize: '32px', minWidth: '110px' }}>{displayServingValue(props.amount)}</div>
-  </Fragment>
-)
-
-const FoodGroupLabel = (props: { foodGroup: FoodGroup; }) => (
-  <Fragment>
-    <div className="d-block d-sm-none">
-      <FoodGroupLabelBadge foodGroup={props.foodGroup} />
-    </div>
-    <div className="d-none d-sm-block">
-      <FoodGroupLegend foodGroup={props.foodGroup} />
-    </div>
-  </Fragment>
-)
-
-const ServingCell = (props: { foodGroup: FoodGroup; amount?: number; }) => (
-  <div className="d-flex flex-column align-items-center m-1">
-    <CalorieText amount={props.amount} />
-    <div className="d-flex align-items-center">
-      <TargetAchievementIcon foodGroup={props.foodGroup} />
-      <FoodGroupLabel foodGroup={props.foodGroup} />
-    </div>
-  </div>
-);
+import { Serving } from "../../model/Food";
+import { ServingCell } from "./ServingCell";
 
 export const ServingSummary = (props: { serving: Serving; }) => (
   <div className="d-flex justify-content-between flex-fill flex-wrap">
