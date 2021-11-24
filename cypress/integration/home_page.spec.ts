2de62592aa59b6d53d4e6b1ac30003f051b21a8a
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-11-24 14:04:19 -0500

    Home page must have a valid date

--- cypress/integration/home_page.spec.ts
+++ cypress/integration/home_page.spec.ts
@@ -1,5 +1,10 @@
 describe('The Home Page', () => {
+
+  const aValidDate = /^\d{1,2}[-\/]\d{1,2}[-\/]\d{4}$/;
+
   it('successfully loads', () => {
-    cy.visit('/')
+    cy.visit('/');
+
+    cy.get('#date').contains(aValidDate);
   })
 })
