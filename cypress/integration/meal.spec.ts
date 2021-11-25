9204a30a239074580cb7317a10b02e00c7498d36
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-11-24 20:36:02 -0500

    Edit a meal

--- cypress/integration/meal.spec.ts
+++ cypress/integration/meal.spec.ts
@@ -16,4 +16,26 @@ context("Meal operations", () => {
 
   })
 
+  it("edit a meal", () => {
+    // add some foods
+    cy.get("form").get("#inputFoodDescription").type("food 1");
+    cy.get("form").contains("Add").click();
+    cy.get("form").get("#inputFoodDescription").type("food 2");
+    cy.get("form").contains("Add").click();
+    cy.get("form").get("#inputFoodDescription").type("food 3");
+    cy.get("form").contains("Add").click();
+    cy.get("form").contains("Cancel").click();
+
+    // put it in edit mode
+    cy.get("#mealCards").first().contains("Edit").click();
+
+    cy.get("#mealCards").first().should("contain", "Delete");
+    cy.get("#mealCards").first().should("contain", "Done");
+    cy.get("#mealCards").first().contains("food 1").parentsUntil(".list-group-item").should("contain", "Edit");
+    cy.get("#mealCards").first().contains("food 2").parentsUntil(".list-group-item").should("contain", "Edit");
+    cy.get("#mealCards").first().contains("food 3").parentsUntil(".list-group-item").should("contain", "Edit");
+    cy.get("#mealCards").first().get("#buttonAddMeal").should("exist");
+
+  })
+
 })
