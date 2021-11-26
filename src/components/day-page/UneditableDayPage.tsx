4d5ab3ea3651446a3772f5e561d71505f24070e5
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-11-25 20:47:24 -0500

    Move meal card into page for cohesion

--- src/components/day-page/UneditableDayPage.tsx
+++ src/components/day-page/UneditableDayPage.tsx
@@ -1,9 +1,11 @@
 import _ from "lodash";
+import { Card, ListGroup } from "react-bootstrap";
 import Footer from "../../features/day-page/Footer";
 import Header from "../../features/day-page/Header";
 import Summary from "../../features/day-page/Summary";
+import MealCalorieServingPanel from "../../features/meal-card/MealCalorieServingPanel";
 import { Meal } from "../../model/Food";
-import { UneditableMealCard } from "../meal-card/UneditableMealCard";
+import { FoodItem } from "../FoodItem";
 
 export const UneditableDayPage = (props: { meals: Meal[]; }) => (
   <div>
@@ -12,7 +14,19 @@ export const UneditableDayPage = (props: { meals: Meal[]; }) => (
 
     {_.map(props.meals, (meal, index) => (
       <div key={index} data-cy={"meal-" + index}>
-        <UneditableMealCard meal={meal} />
+        <Card className="mt-1">
+          <Card.Header className="d-flex flex-wrap align-items-center">
+            <div className="flex-fill">{meal.mealTime}</div>
+            <MealCalorieServingPanel meal={meal} />
+          </Card.Header>
+
+          <ListGroup>
+            {meal.foods.map((food, index) => <ListGroup.Item key={index}>
+              <FoodItem food={food} />
+            </ListGroup.Item>
+            )}
+          </ListGroup>
+        </Card>
       </div>
     ))}
 
