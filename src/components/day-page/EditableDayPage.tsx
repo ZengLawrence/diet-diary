19f4e32bbe3017f8042622b3b6c38b19b57a1672
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-11-26 12:56:46 -0500

    Use index to reduce coupling

--- src/components/day-page/EditableDayPage.tsx
+++ src/components/day-page/EditableDayPage.tsx
@@ -5,19 +5,18 @@ import { Card, ListGroup } from "react-bootstrap";
 import AddMealButton from "../../features/day-page/AddMealButton";
 import Footer from "../../features/day-page/Footer";
 import Header from "../../features/day-page/Header";
-import { MealState } from "../../features/day-page/mealStatesSlice";
 import Summary from "../../features/day-page/Summary";
 import FoodListGroupItems from "../../features/meal-card/FoodListGroupItems";
 import MealCardHeader from "../../features/meal-card/MealCardHeader";
 import { VariantPrimary } from "../ButtonVariant";
 
-export const EditableDayPage = (props: { mealStates: MealState[]; }) => (
+export const EditableDayPage = (props: { numberOfMeals: number; }) => (
   <div>
     <Header />
     <Summary />
 
     <div data-cy="mealCards">
-      {_.map(props.mealStates, (_mealState, index) => (
+      {_.map(_.range(props.numberOfMeals), (index) => (
         <Card className="mt-1" key={index} data-cy={"meal-" + index}>
           <MealCardHeader mealIndex={index} />
 
--- src/features/day-page/EditableDayPage.ts
+++ src/features/day-page/EditableDayPage.ts
@@ -1,10 +1,11 @@
+import _ from "lodash";
 import { connect } from "react-redux";
 import { mealStatesSelector } from "../../app/selectors";
 import { RootState } from "../../app/store";
 import { EditableDayPage } from "../../components/day-page/EditableDayPage";
 
 const mapStateToProps = (state: RootState) => ({
-  mealStates: mealStatesSelector(state),
+  numberOfMeals: _.size(mealStatesSelector(state)),
 })
 
 export default connect(mapStateToProps)(EditableDayPage);
