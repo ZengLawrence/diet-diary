ae5b9522c782e5e1abd79435abb6716226a35563
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-11-25 20:49:20 -0500

    Remove extra div

--- src/components/day-page/UneditableDayPage.tsx
+++ src/components/day-page/UneditableDayPage.tsx
@@ -13,21 +13,19 @@ export const UneditableDayPage = (props: { meals: Meal[]; }) => (
     <Summary />
 
     {_.map(props.meals, (meal, index) => (
-      <div key={index} data-cy={"meal-" + index}>
-        <Card className="mt-1">
-          <Card.Header className="d-flex flex-wrap align-items-center">
-            <div className="flex-fill">{meal.mealTime}</div>
-            <MealCalorieServingPanel meal={meal} />
-          </Card.Header>
+      <Card className="mt-1" key={index} data-cy={"meal-" + index}>
+        <Card.Header className="d-flex flex-wrap align-items-center">
+          <div className="flex-fill">{meal.mealTime}</div>
+          <MealCalorieServingPanel meal={meal} />
+        </Card.Header>
 
-          <ListGroup>
-            {meal.foods.map((food, index) => <ListGroup.Item key={index}>
-              <FoodItem food={food} />
-            </ListGroup.Item>
-            )}
-          </ListGroup>
-        </Card>
-      </div>
+        <ListGroup>
+          {meal.foods.map((food, index) => <ListGroup.Item key={index}>
+            <FoodItem food={food} />
+          </ListGroup.Item>
+          )}
+        </ListGroup>
+      </Card>
     ))}
 
     <Footer />
