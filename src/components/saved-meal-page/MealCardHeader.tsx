5bc40e6795420f757c4a50226ca5cd3696e28cf2
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-06-29 16:56:08 -0400

    fix layout

--- src/components/saved-meal-page/MealCardHeader.tsx
+++ src/components/saved-meal-page/MealCardHeader.tsx
@@ -7,9 +7,7 @@ interface Props {
 }
 
 export const MealCardHeader = (props: Props) => (
-  <Card.Header className="d-flex flex-wrap align-items-center">
-    <div className="order-sm-1 flex-grow-1 flex-md-grow-0">
-      <MealCalorieServingPanel meal={props.meal} />
-    </div>
+  <Card.Header className="d-flex justify-content-end">
+    <MealCalorieServingPanel meal={props.meal} />
   </Card.Header>
 )
