2eae7639ccbdff66694cd30b775eb86ae5d094c3
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-06-30 12:10:29 -0400

    select to add meal

--- src/components/saved-meal-page/MealCardHeader.tsx
+++ src/components/saved-meal-page/MealCardHeader.tsx
@@ -1,6 +1,8 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import MealCalorieServingPanel from "../../features/meal-card/MealCalorieServingPanel";
 import { Food } from "../../model/Food";
+import { VariantSecondary } from "../ButtonVariant";
+import { SelectButton } from "./SelectButton";
 
 interface Props {
   meal: {foods: Food[];};
@@ -9,5 +11,6 @@ interface Props {
 export const MealCardHeader = (props: Props) => (
   <Card.Header className="d-flex justify-content-end">
     <MealCalorieServingPanel meal={props.meal} />
+    <SelectButton variant={VariantSecondary} meal={props.meal} >Select</SelectButton>
   </Card.Header>
 )
