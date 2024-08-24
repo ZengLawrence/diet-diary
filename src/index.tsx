e8bf3a35746ada6fad013ab028318534c43dd8a3
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-08-24 15:54:16 -0400

    not needed

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  createHashRouter,
-  RouterProvider,
-} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 import { Provider } from "react-redux";
 import store from "./app/store";
-import Root from './routes/Root';
-import SavedMeals from './routes/SavedMeals';
-import { Container } from 'react-bootstrap';
+import App from './App';
 
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <Root />, // redirect back to home page
-  },
-  {
-    path: "/saved-meals",
-    element: <SavedMeals />,
-  },
-]);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Container>
-        <RouterProvider router={router} />
-      </Container>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
