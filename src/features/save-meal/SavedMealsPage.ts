e2d6bd72ddbf34c3c2f195f0a417b53d0876677c
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-08-24 16:18:57 -0400

    clearer intent

--- src/features/save-meal/SavedMealsPage.ts
+++ src/features/save-meal/SavedMealsPage.ts
@@ -2,7 +2,7 @@ import _ from "lodash";
 import { connect } from "react-redux";
 import { savedMealsSelector } from "../../app/selectors";
 import { RootState } from "../../app/store";
-import { SavedMealsPage } from "../../components/saved-meal-page/SavedMealsPage";
+import { SavedMealsPage } from "../../components/saved-meal/SavedMealsPage";
 
 const mapStateToProps = (state: RootState) => ({
   numberOfMeals: _.size(savedMealsSelector(state)),
