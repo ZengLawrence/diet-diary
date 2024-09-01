8c9d2431f214b87fa54b9dce8f9424ccbad8c29d
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-01 09:53:49 -0400

    organize import

--- src/components/input-form/FoodInputForm.tsx
+++ src/components/input-form/FoodInputForm.tsx
@@ -1,13 +1,13 @@
 import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
 import { calcFoodCalories, displayCalorieValue } from "../../model/calorieFunction";
 import { Food } from "../../model/Food";
 import { VariantPrimary, VariantSecondary } from "../ButtonVariant";
 import { FoodDescriptionInputControl } from "./FoodDescriptionInputControl";
 import { ServingInputControl } from "./ServingInputControl";
 import { useFoodInputFormStateReducer } from "./useFoodInputFormStateReducer";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 
 export type ButtonLabel = "Add" | "Update";
 
