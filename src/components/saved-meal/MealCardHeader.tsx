c4f99adc975a81603bfceea7f305825471d1589a
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-09-01 11:07:19 -0400

    Delete saved meal (#103)

--- src/components/saved-meal/MealCardHeader.tsx
+++ src/components/saved-meal/MealCardHeader.tsx
@@ -4,17 +4,21 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import MealCalorieServingPanel from "../../features/meal-card/MealCalorieServingPanel";
 import { Food } from "../../model/Food";
-import { VariantSecondary } from "../ButtonVariant";
+import { VariantDanger, VariantSecondary } from "../ButtonVariant";
 
 interface Props {
   mealIndex: number;
   meal: { foods: Food[]; };
   selectMeal: (mealIndex: number, meal: { foods: Food[]; }) => void;
+  deleteMeal: (mealIndex: number) => void;
 }
 
 export const MealCardHeader = (props: Props) => (
   <Card.Header>
     <Row>
+      <Col xs="auto">
+        <Button variant={VariantDanger} onClick={() => props.deleteMeal(props.mealIndex)}>Delete</Button>
+      </Col>
       <Col />
       <Col xs="auto">
         <Button variant={VariantSecondary} onClick={() => props.selectMeal(props.mealIndex, props.meal)}>Select</Button>
