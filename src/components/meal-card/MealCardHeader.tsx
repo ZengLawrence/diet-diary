4ad8a0ba576c03287be3c734f2ddc58adaab5a1b
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-09-01 11:47:18 -0400

    Delete button on left (#104)
    * Conform with style guide to make dangerous operations harder to get to

--- src/components/meal-card/MealCardHeader.tsx
+++ src/components/meal-card/MealCardHeader.tsx
@@ -4,16 +4,24 @@ import Row from "react-bootstrap/Row";
 import MealButtons from "../../features/meal-card/MealButtons";
 import MealCalorieServingPanel from "../../features/meal-card/MealCalorieServingPanel";
 import { Meal } from "../../model/Food";
+import { VariantDanger } from "../ButtonVariant";
+import DeleteButton from "../../features/meal-card/DeleteButton";
 
 interface Props {
   meal: Meal;
   mealIndex: number;
   showButton: boolean;
+  showDeleteButton: boolean;
 }
 
 export const MealCardHeader = (props: Props) => (
   <Card.Header>
     <Row>
+      {props.showDeleteButton &&
+        <Col xs="auto">
+          <DeleteButton variant={VariantDanger} mealIndex={props.mealIndex} label="Delete" />
+        </Col>
+      }
       <Col>
         {props.meal.mealTime}
       </Col>
