ce7f0cb8c52d9a14e4c85d9965b4b28345db1f51
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-07 10:21:35 -0400

    align to what it looks like on ui

--- src/components/FoodGroupLegend.tsx
+++ src/components/FoodGroupLegend.tsx
@@ -5,8 +5,7 @@ export const FoodGroupLegend = (props: { foodGroup: FoodGroup; }) => {
   const { foodGroup } = props;
   return (
     <div className="d-flex justify-content-center border-0 rounded bg-light">
-      <FoodGroupLabelBadge foodGroup={foodGroup} />
-      <div>{displayName(foodGroup)}</div>
+      <FoodGroupLabelBadge foodGroup={foodGroup} />{displayName(foodGroup)}
     </div>
   );
 };
