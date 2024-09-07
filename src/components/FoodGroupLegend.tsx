03106960529a6e33ea89223148931749378e1b8d
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-07 10:22:55 -0400

    not needed

--- src/components/FoodGroupLegend.tsx
+++ src/components/FoodGroupLegend.tsx
@@ -4,7 +4,7 @@ import { FoodGroupLabelBadge } from "./badge";
 export const FoodGroupLegend = (props: { foodGroup: FoodGroup; }) => {
   const { foodGroup } = props;
   return (
-    <div className="d-flex justify-content-center border-0 rounded bg-light">
+    <div className="border-0 rounded bg-light">
       <FoodGroupLabelBadge foodGroup={foodGroup} />{displayName(foodGroup)}
     </div>
   );
