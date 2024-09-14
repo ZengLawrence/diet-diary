b9870f3664b94fee85cd6b6e3b498b0165f07d42
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-13 22:04:13 -0400

    organize imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import { Provider } from "react-redux";
-import store from "./app/store";
-import App from './App';
 import { createRoot } from 'react-dom/client';
+import { Provider } from "react-redux";
+import App from './App';
+import store from "./app/store";
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
