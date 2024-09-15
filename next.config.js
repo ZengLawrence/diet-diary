beccd6f4c7bb36b5267e235b829eb522d7646100
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-15 14:48:18 -0400

    only need one of two versions

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 // fix fs not found error for ANTLR4. See https://stackoverflow.com/questions/67478532/module-not-found-cant-resolve-fs-nextjs
 module.exports = {
+  output: 'export', // Outputs a Single-Page Application (SPA).
+  distDir: './build', // Changes the build output directory to `./dist`.
   webpack(config) {
     config.resolve.fallback = {
       ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
