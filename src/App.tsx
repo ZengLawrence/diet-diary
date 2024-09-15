9c452351f7b2eb50b87ecda4740f39f110583bde
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-15 09:03:14 -0400

    fix missing CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import Container from "react-bootstrap/Container";
 import { Provider } from "react-redux";
 import store from "./app/store";
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
