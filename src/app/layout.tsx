f0b8f8cee0c491d5bbe9e32ca67dc57f9cf00b0e
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-15 08:01:59 -0400

    link not needed when meta files in /app dir

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
         <meta
           name="description"
           content="Keeping records of food you eat for the day."
