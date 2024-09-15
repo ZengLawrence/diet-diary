ee9c26323bacc60cc059d59e9f0407e926adfeee
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-15 10:03:16 -0400

    fix unrecognized key error

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 // fix fs not found error for ANTLR4. See https://stackoverflow.com/questions/67478532/module-not-found-cant-resolve-fs-nextjs
 module.exports = {
-  future: {
-    webpack5: true, // by default, if you customize webpack config, they switch back to version 4. 
-      // Looks like backward compatibility approach.
-  },
   webpack(config) {
     config.resolve.fallback = {
       ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
