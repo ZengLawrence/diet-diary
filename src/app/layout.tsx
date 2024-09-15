71ca588c46dc289d7b3ee8ac6ab48f1af423b0a0
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-15 07:50:33 -0400

    remove meta not in migration guide

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,13 +9,10 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#000000" />
         <meta
           name="description"
           content="Keeping records of food you eat for the day."
         />
-        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo96.png" />
-        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
         <title>Diet Diary</title>
       </head>
       <body>
