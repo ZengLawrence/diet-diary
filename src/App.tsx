791894acdcc3525546f505224567e2d88fe40605
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-09-26 07:22:03 -0400

    Auto dark mode (#144)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import Script from 'next/script';
 import Container from "react-bootstrap/Container";
 import { Provider } from "react-redux";
 import store from "./app/store";
 import DayPage from "./features/day-page/DayPage";
 
+const AutoDarkModeScript = () => (
+  <Script
+    id="auto-dark-mode">
+    {`  
+    const setTheme = () => {
+      document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'));
+    };
+    setTheme();
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {    
+      setTheme();
+    });
+    `}
+  </Script>
+);
+
 export default function Root() {
   return (
     <Provider store={store}>
       <Container>
+        <AutoDarkModeScript />
         <DayPage />
       </Container>
     </Provider>
