4fdec1602a39e82746d0be9b845b22205b23391b
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-25 20:25:03 -0400

    set theme on load

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import Container from "react-bootstrap/Container";
 import { Provider } from "react-redux";
 import store from "./app/store";
 import DayPage from "./features/day-page/DayPage";
+import Script from 'next/script';
 
 export default function Root() {
   return (
     <Provider store={store}>
       <Container>
+        <Script
+          id="auto-dark-mode">
+            {`document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))`}
+          </Script>
         <DayPage />
       </Container>
     </Provider>
