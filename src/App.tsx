1502fd2b2877e33f5230ff460c3de1c21ccf9107
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-25 20:52:15 -0400

    extract function for readability

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,27 @@ import store from "./app/store";
 import DayPage from "./features/day-page/DayPage";
 import Script from 'next/script';
 
+const AutoDarkModeScript = () => (
+  <Script
+    id="auto-dark-mode">
+    {`  
+    const setTheme = () => {
+      document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'));
+    };
+    setTheme();
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {    
+      setTheme();
+    });
+    `}
+  </Script>
+);
+
 export default function Root() {
   return (
     <Provider store={store}>
       <Container>
-        <Script
-          id="auto-dark-mode">
-            {`  
-            const setTheme = () => {
-              document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'));
-            };
-            setTheme();
-            
-            window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {    
-              setTheme();
-            });
-            `}
-          </Script>
+        <AutoDarkModeScript />
         <DayPage />
       </Container>
     </Provider>
