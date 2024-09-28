aedb51478faaa598ceddb9e6794518e8259f243c
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-28 11:43:41 -0400

    not needed

--- src/components/day-page/Header.tsx
+++ src/components/day-page/Header.tsx
@@ -2,7 +2,7 @@ import DatePanel from "../../features/day-page/DatePanel";
 import HeaderButtons from "../../features/day-page/HeaderButtons";
 import TargetPanel from "../../features/target/TargetPanel";
 
-export const Header = (props: { date: string }) => (
+export const Header = () => (
   <div className="d-flex flex-column justify-content-between align-items-center">
     <DatePanel />
     <TargetPanel />
