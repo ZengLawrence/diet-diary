7bea01807e5c10c768b9af4f7757ffb04bdece89
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-28 12:23:25 -0400

    move to new file

--- src/components/day-page/Header.tsx
+++ src/components/day-page/Header.tsx
@@ -1,17 +1,6 @@
 import DatePanel from "../../features/day-page/DatePanel";
 import HeaderButtons from "../../features/day-page/HeaderButtons";
-import NewDayButton from "../../features/day-page/NewDayButton";
-import TargetPanel from "../../features/target/TargetPanel";
-import { VariantDanger } from "../ButtonVariant";
-
-const TargetPanelRow = () => (
-  <div className="d-flex flex-row">
-    <div className="d-flex align-items-center">
-      <NewDayButton variant={VariantDanger}>New Day</NewDayButton>
-    </div>
-    <TargetPanel />
-  </div>
-);
+import { TargetPanelRow } from "./TargetPanelRow";
 
 export const Header = () => (
   <div className="d-flex flex-column justify-content-between align-items-center">
