65174d4e20f70b4da068556158240b7fc14602b8
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-28 15:18:41 -0400

    clear separation between header and summary tabs

--- src/components/day-page/Summary.tsx
+++ src/components/day-page/Summary.tsx
@@ -2,5 +2,7 @@ import TabbedSummary from "../../features/summary/TabbedSummary";
 import { TotalSummary } from "../summary/TotalSummary";
 
 export const Summary = (props: { showTabs: boolean }) => (
-  props.showTabs ? <TabbedSummary /> : <TotalSummary />
+  <div className="mt-3">
+    {props.showTabs ? <TabbedSummary /> : <TotalSummary />}
+  </div>
 );
