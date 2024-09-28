e32cf4e0870ef41dbfd99c488e305c024bc5b8a0
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-27 20:05:56 -0400

    new colors for better contrast

--- src/components/backgroundColor.ts
+++ src/components/backgroundColor.ts
@@ -2,15 +2,15 @@ import _ from "lodash";
 import { FoodGroup } from "../model/Food";
 
 const BACKGROUND_COLORS = {
-  "vegetable": "green",
-  "fruit": "teal",
-  "carbohydrate": "cyan",
-  "proteinDiary": "blue",
+  "vegetable": "darkgreen",
+  "fruit": "yellowgreen",
+  "carbohydrate": "deepskyblue",
+  "proteinDiary": "royalblue",
   "fat": "orange",
   "sweet": "red",
 };
 
-export type BadgeBackgroundColor = "green" | "teal" | "cyan"| "blue" | "orange" | "red" ;
+export type BadgeBackgroundColor = "green" | "teal" | "cyan"| "blue" | "orange" | "red" | "darkgreen" | "yellowgreen" | "royalblue" | "deepskyblue";
 
 export function backgroundColor(foodGroup: FoodGroup)  {
   return _.get(BACKGROUND_COLORS, foodGroup, '') as BadgeBackgroundColor;
