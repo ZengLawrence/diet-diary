c8fd14f018fa6cdf9b2f9d6121339b414004f795
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-09-28 17:40:38 -0400

    Keep calorie label in one row on input control (#155)

--- src/components/panels/FoodGroupServingBadgePanel.tsx
+++ src/components/panels/FoodGroupServingBadgePanel.tsx
@@ -3,11 +3,11 @@ import { FoodGroupServingBadge } from "../badge";
 
 export const FoodGroupServingBadgePanel = (props: { serving: Serving; }) => (
   <span>
-    <FoodGroupServingBadge foodGroup="vegetable" serving={props.serving} />
-    <FoodGroupServingBadge foodGroup="fruit" serving={props.serving} />
-    <FoodGroupServingBadge foodGroup="carbohydrate" serving={props.serving} />
-    <FoodGroupServingBadge foodGroup="proteinDiary" serving={props.serving} />
-    <FoodGroupServingBadge foodGroup="fat" serving={props.serving} />
+    <FoodGroupServingBadge foodGroup="vegetable" serving={props.serving} />&nbsp;
+    <FoodGroupServingBadge foodGroup="fruit" serving={props.serving} />&nbsp;
+    <FoodGroupServingBadge foodGroup="carbohydrate" serving={props.serving} />&nbsp;
+    <FoodGroupServingBadge foodGroup="proteinDiary" serving={props.serving} />&nbsp;
+    <FoodGroupServingBadge foodGroup="fat" serving={props.serving} />&nbsp;
     <FoodGroupServingBadge foodGroup="sweet" serving={props.serving} />
   </span>
 );
