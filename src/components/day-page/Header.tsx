cf664f84f089aff91d76614a82c7c6a6c3e46de0
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-28 11:32:39 -0400

    same layout to avoid surprises

--- src/components/day-page/Header.tsx
+++ src/components/day-page/Header.tsx
@@ -2,13 +2,9 @@ import HeaderButtons from "../../features/day-page/HeaderButtons";
 import TargetPanel from "../../features/target/TargetPanel";
 
 export const Header = (props: { date: string }) => (
-  <div className="d-flex flex-column flex-md-row justify-content-between align-items-center">
-    <h1 className="order-md-1" data-cy="date">{props.date}</h1>
-    <div className="order-md-0">
-      <TargetPanel />
-    </div>
-    <div className="order-md-2">
-      <HeaderButtons />
-    </div>
+  <div className="d-flex flex-column justify-content-between align-items-center">
+    <h1 data-cy="date">{props.date}</h1>
+    <TargetPanel />
+    <HeaderButtons />
   </div>
 )
