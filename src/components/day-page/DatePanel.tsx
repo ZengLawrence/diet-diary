4671ce14a3995ddbd750e1db3e04017b60518166
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-28 15:43:41 -0400

    symbol to save space

--- src/components/day-page/DatePanel.tsx
+++ src/components/day-page/DatePanel.tsx
@@ -1,9 +1,17 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import NewDayButton from "../../features/day-page/NewDayButton";
 import { VariantDanger } from "../ButtonVariant";
+
+const NewDaySymbolButton = () => (
+  <NewDayButton variant={VariantDanger}>
+    <FontAwesomeIcon icon={faPlus} />
+  </NewDayButton>
+);
 
 export const DatePanel = (props: { date: string; showNewDayButton: boolean }) => (
   <div className="d-flex flex-row">
     <h1 data-cy="date">{props.date}</h1>
-    {props.showNewDayButton && <NewDayButton variant={VariantDanger}>New Day</NewDayButton>}
+    {props.showNewDayButton && <NewDaySymbolButton />}
   </div>
 );
