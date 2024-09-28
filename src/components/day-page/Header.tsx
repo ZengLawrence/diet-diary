a1020be3c1e259b59136c283f3fd2666cc761ca6
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-28 11:38:26 -0400

    extract component

--- src/components/day-page/Header.tsx
+++ src/components/day-page/Header.tsx
@@ -1,9 +1,13 @@
 import HeaderButtons from "../../features/day-page/HeaderButtons";
 import TargetPanel from "../../features/target/TargetPanel";
 
+const DatePanel = (props: { date: string }) => (
+  <h1 data-cy="date">{props.date}</h1>  
+);
+
 export const Header = (props: { date: string }) => (
   <div className="d-flex flex-column justify-content-between align-items-center">
-    <h1 data-cy="date">{props.date}</h1>
+    <DatePanel date={props.date} />
     <TargetPanel />
     <HeaderButtons />
   </div>
