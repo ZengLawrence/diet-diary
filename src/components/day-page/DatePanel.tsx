89f53dd78870ded8f1eb95833effef557960568f
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-09-28 16:15:47 -0400

    Change New Day button to Plus for better visual (#154)

--- src/components/day-page/DatePanel.tsx
+++ src/components/day-page/DatePanel.tsx
@@ -1,9 +1,19 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import NewDayButton from "../../features/day-page/NewDayButton";
 import { VariantDanger } from "../ButtonVariant";
 
+const NewDaySymbolButton = () => (
+  <div  className="align-self-center">
+    <NewDayButton variant={VariantDanger}>
+      <FontAwesomeIcon icon={faPlus} />
+    </NewDayButton>
+  </div>
+);
+
 export const DatePanel = (props: { date: string; showNewDayButton: boolean }) => (
-  <div className="d-flex flex-column align-items-center">
-    <h1 data-cy="date">{props.date}</h1>
-    {props.showNewDayButton && <NewDayButton variant={VariantDanger}>New Day</NewDayButton>}
+  <div className="d-flex flex-row">
+    <h1 data-cy="date">{props.date}</h1>&nbsp;
+    {props.showNewDayButton && <NewDaySymbolButton />}
   </div>
 );
