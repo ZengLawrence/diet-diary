8d9f24c0b7b0030ffcfe1b7039612b44c0ddb35b
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-28 07:57:09 -0400

    revert for better contrast

--- src/components/summary/CalorieSummary.tsx
+++ src/components/summary/CalorieSummary.tsx
@@ -1,7 +1,7 @@
 import { displayCalorieValue } from "../../model/calorieFunction";
 
 export const CalorieSummary = (props: { calories: number; }) => (
-  <div className="d-flex flex-column justify-content-center border rounded bg-info dd-text-yellow text-center p-1">
+  <div className="d-flex flex-column justify-content-center border rounded bg-info text-white text-center p-1">
     <div style={{ fontSize: '40px', width: '110px' }}>{displayCalorieValue(props.calories)}</div>
     <div>calories</div>
   </div>
