9a735c98c8bfbb470bda6d872b9f78b282e1d5e5
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-28 15:33:44 -0400

    on same row

--- src/components/day-page/DatePanel.tsx
+++ src/components/day-page/DatePanel.tsx
@@ -2,7 +2,7 @@ import NewDayButton from "../../features/day-page/NewDayButton";
 import { VariantDanger } from "../ButtonVariant";
 
 export const DatePanel = (props: { date: string; showNewDayButton: boolean }) => (
-  <div className="d-flex flex-column align-items-center">
+  <div className="d-flex flex-row">
     <h1 data-cy="date">{props.date}</h1>
     {props.showNewDayButton && <NewDayButton variant={VariantDanger}>New Day</NewDayButton>}
   </div>
