723332c3c2967df7752d23e873f74c2bdb70f2f2
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-28 12:22:58 -0400

    extract function

--- src/components/day-page/Header.tsx
+++ src/components/day-page/Header.tsx
@@ -4,15 +4,19 @@ import NewDayButton from "../../features/day-page/NewDayButton";
 import TargetPanel from "../../features/target/TargetPanel";
 import { VariantDanger } from "../ButtonVariant";
 
+const TargetPanelRow = () => (
+  <div className="d-flex flex-row">
+    <div className="d-flex align-items-center">
+      <NewDayButton variant={VariantDanger}>New Day</NewDayButton>
+    </div>
+    <TargetPanel />
+  </div>
+);
+
 export const Header = () => (
   <div className="d-flex flex-column justify-content-between align-items-center">
     <DatePanel />
-    <div className="d-flex flex-row">
-      <div className="d-flex align-items-center">
-        <NewDayButton variant={VariantDanger}>New Day</NewDayButton>
-      </div>
-      <TargetPanel />
-    </div>
+    <TargetPanelRow />
     <HeaderButtons />
   </div>
 )
