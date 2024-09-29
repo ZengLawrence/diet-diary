7d355ecbe62e505c809884436526747b6e00de1d
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-29 09:05:54 -0400

    not used

--- src/features/target/targetStateSlice.ts
+++ src/features/target/targetStateSlice.ts
@@ -1,15 +1,12 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { DEFAULT_TARGET, Gender, Target } from "../../model/Target";
-import { exitEditMode } from "../day-page/editModeSlice";
 
 interface TargetState {
-  editTarget: boolean,
   target: Target,
   gender: Gender,
 }
 
 const initialState = {
-  editTarget: false,
   target: DEFAULT_TARGET,
   gender: "man" as Gender,
 } as TargetState;
@@ -18,12 +15,6 @@ const targetStateSlice = createSlice({
   name: "targetState",
   initialState,
   reducers: {
-    enterEditTarget(state) {
-      state.editTarget = true;
-    },
-    exitEditTarget(state) {
-      state.editTarget = false;
-    },
     changeTarget(state, action: PayloadAction<Target>) {
       state.target = action.payload;
     },
@@ -31,15 +22,9 @@ const targetStateSlice = createSlice({
       state.gender = action.payload;
     },
   },
-  extraReducers: (builder) => {
-    builder.addCase(exitEditMode, (state) => {
-      state.editTarget = false
-    })
-  }
 })
 
 export const {
-  enterEditTarget, exitEditTarget,
   changeTarget,
   changeGender,
 } = targetStateSlice.actions;
