78c0639d556ff90b7c9a6b75eec0c93bddd0c669
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-29 11:32:18 -0400

    reduce file size

--- src/components/day-page/DayPage.tsx
+++ src/components/day-page/DayPage.tsx
@@ -2,35 +2,16 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import _ from "lodash";
 import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
-import ListGroup from "react-bootstrap/ListGroup";
 import Row from "react-bootstrap/Row";
 import AddMealButton from "../../features/day-page/AddMealButton";
 import Footer from "../../features/day-page/Footer";
 import SavedMealCardsOffcanvas from "../../features/day-page/SavedMealCardsOffcanvas";
 import Summary from "../../features/day-page/Summary";
-import FoodListGroupItems from "../../features/meal-card/FoodListGroupItems";
-import MealCardHeader from "../../features/meal-card/MealCardHeader";
 import { VariantPrimary, VariantSecondary } from "../ButtonVariant";
 import { Header } from "./Header";
+import { MealCards } from "../meal-card/MealCards";
 
-const MealCards = (props: { numberOfMeals: number; }) => (
-  _.map(_.range(props.numberOfMeals), (index) => (
-    <div>
-      <Card
-        id={_.toString(index)}
-        key={index}
-        data-cy="mealCard">
-        <MealCardHeader mealIndex={index} />
-
-        <ListGroup>
-          <FoodListGroupItems mealIndex={index} />
-        </ListGroup>
-      </Card>
-    </div>
-  ))
-);
 
 interface Props {
   numberOfMeals: number;
---
+++ src/components/meal-card/MealCards.tsx
@@ -0,0 +1,22 @@
+import _ from "lodash";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import FoodListGroupItems from "../../features/meal-card/FoodListGroupItems";
+import MealCardHeader from "../../features/meal-card/MealCardHeader";
+
+export const MealCards = (props: { numberOfMeals: number; }) => (
+  _.map(_.range(props.numberOfMeals), (index) => (
+    <div>
+      <Card
+        id={_.toString(index)}
+        key={index}
+        data-cy="mealCard">
+        <MealCardHeader mealIndex={index} />
+
+        <ListGroup>
+          <FoodListGroupItems mealIndex={index} />
+        </ListGroup>
+      </Card>
+    </div>
+  ))
+);
