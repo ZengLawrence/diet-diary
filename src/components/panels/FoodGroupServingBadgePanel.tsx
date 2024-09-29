1aa1549f910d1b240a1f3c334672d316f2f60a86
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-29 18:44:59 -0400

    refactor to allow more lines in function

--- src/components/panels/FoodGroupServingBadgePanel.tsx
+++ src/components/panels/FoodGroupServingBadgePanel.tsx
@@ -1,13 +1,15 @@
 import { Serving } from "../../model/Food";
 import { FoodGroupServingBadge } from "../badge";
 
-export const FoodGroupServingBadgePanel = (props: { serving: Serving; }) => (
-  <span>
-    <FoodGroupServingBadge foodGroup="vegetable" serving={props.serving} />&nbsp;
-    <FoodGroupServingBadge foodGroup="fruit" serving={props.serving} />&nbsp;
-    <FoodGroupServingBadge foodGroup="carbohydrate" serving={props.serving} />&nbsp;
-    <FoodGroupServingBadge foodGroup="proteinDiary" serving={props.serving} />&nbsp;
-    <FoodGroupServingBadge foodGroup="fat" serving={props.serving} />&nbsp;
-    <FoodGroupServingBadge foodGroup="sweet" serving={props.serving} />
-  </span>
-);
+export const FoodGroupServingBadgePanel = (props: { serving: Serving; }) => {
+  return (
+    <span>
+      <FoodGroupServingBadge foodGroup="vegetable" serving={props.serving} />
+      <FoodGroupServingBadge foodGroup="fruit" serving={props.serving} />
+      <FoodGroupServingBadge foodGroup="carbohydrate" serving={props.serving} />
+      <FoodGroupServingBadge foodGroup="proteinDiary" serving={props.serving} />
+      <FoodGroupServingBadge foodGroup="fat" serving={props.serving} />
+      <FoodGroupServingBadge foodGroup="sweet" serving={props.serving} />
+    </span>
+  );
+}
