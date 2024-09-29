ea728ade5f74a5ca8a9037c31bfea0d56ec488c5
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-29 16:47:14 -0400

    fix compile error

--- src/components/meal-card/MealCards.tsx
+++ src/components/meal-card/MealCards.tsx
@@ -1,22 +1,25 @@
 import _ from "lodash";
+import { Fragment } from "react";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import FoodListGroupItems from "../../features/meal-card/FoodListGroupItems";
 import MealCardHeader from "../../features/meal-card/MealCardHeader";
 
 export const MealCards = (props: { numberOfMeals: number; }) => (
-  _.map(_.range(props.numberOfMeals), (index) => (
-    <div className="mb-1">
-      <Card
-        id={_.toString(index)}
-        key={index}
-        data-cy="mealCard">
-        <MealCardHeader mealIndex={index} />
+  <Fragment>
+    {_.map(_.range(props.numberOfMeals), (index) => (
+      <div className="mb-1">
+        <Card
+          id={_.toString(index)}
+          key={index}
+          data-cy="mealCard">
+          <MealCardHeader mealIndex={index} />
 
-        <ListGroup>
-          <FoodListGroupItems mealIndex={index} />
-        </ListGroup>
-      </Card>
-    </div>
-  ))
+          <ListGroup>
+            <FoodListGroupItems mealIndex={index} />
+          </ListGroup>
+        </Card>
+      </div>
+    ))}
+  </Fragment>
 );
