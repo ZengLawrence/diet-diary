c6333ef06d78c02e3d093d153113d003e03b4692
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-29 18:46:31 -0400

    unpack for readability

--- src/components/panels/FoodGroupServingBadgePanel.tsx
+++ src/components/panels/FoodGroupServingBadgePanel.tsx
@@ -2,14 +2,16 @@ import { Serving } from "../../model/Food";
 import { FoodGroupServingBadge } from "../badge";
 
 export const FoodGroupServingBadgePanel = (props: { serving: Serving; }) => {
+  const { serving } = props;
+
   return (
     <span>
-      <FoodGroupServingBadge foodGroup="vegetable" serving={props.serving} />
-      <FoodGroupServingBadge foodGroup="fruit" serving={props.serving} />
-      <FoodGroupServingBadge foodGroup="carbohydrate" serving={props.serving} />
-      <FoodGroupServingBadge foodGroup="proteinDiary" serving={props.serving} />
-      <FoodGroupServingBadge foodGroup="fat" serving={props.serving} />
-      <FoodGroupServingBadge foodGroup="sweet" serving={props.serving} />
+      <FoodGroupServingBadge foodGroup="vegetable" serving={serving} />
+      <FoodGroupServingBadge foodGroup="fruit" serving={serving} />
+      <FoodGroupServingBadge foodGroup="carbohydrate" serving={serving} />
+      <FoodGroupServingBadge foodGroup="proteinDiary" serving={serving} />
+      <FoodGroupServingBadge foodGroup="fat" serving={serving} />
+      <FoodGroupServingBadge foodGroup="sweet" serving={serving} />
     </span>
   );
 }
