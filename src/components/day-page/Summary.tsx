31b9685586b3eb64ee4dec58d7e8ac46dfb71117
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-29 14:22:18 -0400

    not needed

--- src/components/day-page/Summary.tsx
+++ src/components/day-page/Summary.tsx
@@ -2,7 +2,5 @@ import TabbedSummary from "../../features/summary/TabbedSummary";
 import { TotalSummary } from "../summary/TotalSummary";
 
 export const Summary = (props: { showTabs: boolean }) => (
-  <div>
-    {props.showTabs ? <TabbedSummary /> : <TotalSummary />}
-  </div>
+  props.showTabs ? <TabbedSummary /> : <TotalSummary />
 );
