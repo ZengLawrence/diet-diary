3b53edfa934d02c77af590e6cd83aea85423c984
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-09-29 12:23:09 -0400

    simplify and unify how to spacing

--- src/components/day-page/Header.tsx
+++ src/components/day-page/Header.tsx
@@ -1,11 +1,18 @@
+import Row from "react-bootstrap/Row";
 import DatePanel from "../../features/day-page/DatePanel";
 import HeaderButtons from "../../features/day-page/HeaderButtons";
 import TargetCaloriePanel from "../../features/target/TargetCaloriePanel";
 
 export const Header = () => (
-  <div className="d-flex flex-column justify-content-between align-items-center">
-    <DatePanel />
-    <TargetCaloriePanel />
-    <HeaderButtons />
-  </div>
+  <Row className="gy-1">
+    <div className="d-flex justify-content-center">
+      <DatePanel />
+    </div>
+    <div className="d-flex justify-content-center">
+      <TargetCaloriePanel />
+    </div>
+    <div className="d-flex flex-column justify-content-between align-items-center">
+      <HeaderButtons />
+    </div>
+  </Row>
 )
