647fd34ab932c7a0843883b0d4cbd8d0c7fe2592
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-10-01 18:17:33 -0400

    keep inside border

--- src/components/input-form/ServingInputControl.tsx
+++ src/components/input-form/ServingInputControl.tsx
@@ -41,7 +41,7 @@ export const ServingInputControl = (props: Props) => {
     <Form.Group controlId={controlId} className="border rounded">
       <div className="d-flex flex-row flex-nowrap">
         <FoodGroupLabelBadge foodGroup={foodGroup} />
-        <span className="dd-calorie-label">{calories}<span className="dd-x-small-font">&nbsp;Cal.</span></span>
+        <span className="dd-calorie-label">{calories}<span className="dd-x-small-font">Cal.</span></span>
       </div>
       <Form.Control
         type="number"
