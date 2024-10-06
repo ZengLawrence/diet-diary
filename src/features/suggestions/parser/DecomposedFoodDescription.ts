496118e86aa3a7f7649156d5d89984f8095b6ff3
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-10-06 11:59:47 -0400

    replace deprecated method

--- src/features/suggestions/parser/DecomposedFoodDescription.ts
+++ src/features/suggestions/parser/DecomposedFoodDescription.ts
@@ -5,8 +5,8 @@ export default function decompose(foodDescription: string) {
   const { foodName, amount, measurement } = parse(foodDescription);
   const unitText = measurement?.unitText;
   // put a space after a word
-  const foodNameCompleted = foodDescription.substr(_.size(foodName), 1) === " ";
-  const unitCompleted = (amount && unitText) ? foodDescription.substr(_.size(foodDescription) - 1, 1) === " " : false;
+  const foodNameCompleted = foodDescription.substring(_.size(foodName), _.size(foodName) + 1) === " ";
+  const unitCompleted = (amount && unitText) ? foodDescription.substring(_.size(foodDescription) - 1, _.size(foodDescription)) === " " : false;
   return {
     foodName,
     amount,
