e3af9475f3452249cc4baae0e6d7685deabceff8
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-10-13 16:44:00 -0400

    update to use gradle generated JS target

--- gen-parser.sh
+++ gen-parser.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-mvn clean package -f ./antlr4/food-description/pom.xml
-mvn clean package -f ./antlr4/amount/pom.xml
+./antlr4/gradlew -p ./antlr4 clean generateJsTarget
 
 if [ ! -d "./src/generated/parser" ] 
 then
     mkdir -p ./src/generated/parser
 fi
-cp ./antlr4/food-description/target/js/*.js ./src/generated/parser/
-cp ./antlr4/amount/target/js/*.js ./src/generated/parser/
+cp ./antlr4/lib/build/js/*.js ./src/generated/parser/
