023e60d249389625a6df6169d15ef89ae9876c6a
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-10-15 19:08:13 -0400

    parameterize test

--- antlr4/lib/src/test/java/grammar/FoodDescriptionIT.java
+++ antlr4/lib/src/test/java/grammar/FoodDescriptionIT.java
@@ -5,19 +5,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.Trees;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class FoodDescriptionIT {
 
-  @Test
-  public void test() {
-    final var input = "broccoli 1 cup";
+  @ParameterizedTest
+  @CsvFileSource(resources = "/food-description-data.csv", numLinesToSkip = 1)
+  public void testGrammar(final String input, final String expected) {
     final var charStream = CharStreams.fromString(input);
     final var lexer = new FoodDescriptionLexer(charStream);
     final var tokens = new CommonTokenStream(lexer);
     final var parser = new FoodDescriptionParser(tokens);
     final var parserRuleContext = parser.foodDescription();
-    final String lispTree = Trees.toStringTree(parserRuleContext, parser);
-    assertEquals("(foodDescription (foodName broccoli) (measurement (quantity 1) (unit cup)))", lispTree);
+    final var lispTree = Trees.toStringTree(parserRuleContext, parser);
+    assertEquals(expected, lispTree);
   }
 }
