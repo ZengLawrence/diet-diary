c7ef2c27791450ab7b512aefdf68359a304c8f80
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-10-18 18:25:28 -0400

    more clear

--- src/__test__/FoodDescriptionGrammar.test.ts
+++ src/__test__/FoodDescriptionGrammar.test.ts
@@ -3,19 +3,19 @@ import FoodDescriptionLexer from "../generated/parser/FoodDescriptionLexer";
 import FoodDescriptionParser from "../generated/parser/FoodDescriptionParser";
 import Trees from "./Trees";
 
-function testGrammar(input: string, expectedParsedTree: string) {
+function parseTree(input: string) {
   const charStream = CharStreams.fromString(input);
   const lexer = new FoodDescriptionLexer(charStream);
   const tokens = new CommonTokenStream(lexer);
   const parser = new FoodDescriptionParser(tokens);
   const parserRuleContext = parser.foodDescription();
-  const lispTree = Trees.toStringTree(parserRuleContext, parser.ruleNames, parser);
-  expect(lispTree).toBe(expectedParsedTree);
+  return Trees.toStringTree(parserRuleContext, parser.ruleNames, parser);
 }
 
 describe("Full description", () => {
 
   test("food name + measurement", () => {
-    testGrammar("broccoli 1 cup", "(foodDescription (foodName broccoli) (measurement (quantity 1) (unit cup)))");
+    expect(parseTree("broccoli 1 cup"))
+      .toEqual("(foodDescription (foodName broccoli) (measurement (quantity 1) (unit cup)))");
   });
 })
