bbbe51ffd28f3955b16e9210f758e2536a717265
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-17 20:20:45 -0400

    Raise Saved Meal limit to allow more food for suggestions (#190)

--- src/features/saved-meal/savedMealsSlice.ts
+++ src/features/saved-meal/savedMealsSlice.ts
@@ -1,7 +1,7 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { Food, Meal } from "../../model/Food";
 
-const MAX_SAVED_COUNT = 50;
+const MAX_SAVED_COUNT = 100;
 
 interface SavedMeal {
   foods: Food[];
