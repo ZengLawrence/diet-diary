b7174dae880441b6c146b4ca5af3fe50b8c970d0
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-10-18 18:16:35 -0400

    reuse

--- src/__test__/FoodDescriptionGrammar.test.ts
+++ src/__test__/FoodDescriptionGrammar.test.ts
@@ -3,16 +3,19 @@ import FoodDescriptionLexer from "../generated/parser/FoodDescriptionLexer";
 import FoodDescriptionParser from "../generated/parser/FoodDescriptionParser";
 import Trees from "./Trees";
 
+function testGrammar(input: string, expectedParsedTree: string) {
+  const charStream = CharStreams.fromString(input);
+  const lexer = new FoodDescriptionLexer(charStream);
+  const tokens = new CommonTokenStream(lexer);
+  const parser = new FoodDescriptionParser(tokens);
+  const parserRuleContext = parser.foodDescription();
+  const lispTree = Trees.toStringTree(parserRuleContext, parser.ruleNames, parser);
+  expect(lispTree).toBe(expectedParsedTree);
+}
+
 describe("Food description grammar", () => {
 
   test("Full description", () => {
-    const input = "broccoli 1 cup";
-    const charStream = CharStreams.fromString(input);
-    const lexer = new FoodDescriptionLexer(charStream);
-    const tokens = new CommonTokenStream(lexer);
-    const parser = new FoodDescriptionParser(tokens);
-    const parserRuleContext = parser.foodDescription();
-    const lispTree = Trees.toStringTree(parserRuleContext, parser.ruleNames, parser);
-    expect(lispTree).toBe("(foodDescription (foodName broccoli) (measurement (quantity 1) (unit cup)))");
+    testGrammar("broccoli 1 cup", "(foodDescription (foodName broccoli) (measurement (quantity 1) (unit cup)))");
   });
 })
