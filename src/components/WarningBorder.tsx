f304a4c6e62e5b386136bacf9270b8036faea079
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-20 15:02:37 -0400

    functional style for readability

--- src/components/WarningBorder.tsx
+++ src/components/WarningBorder.tsx
@@ -3,16 +3,14 @@ interface Props extends React.PropsWithChildren {
   critical?: boolean;
 }
 
-function className(options: {flexFill?: boolean; critical?: boolean}) {
+function className(options: { flexFill?: boolean; critical?: boolean }) {
   const borderColor = options.critical ? "border-danger" : "border-warning";
   const border = "border rounded border-2 " + borderColor;
   return (options.flexFill ? "d-flex flex-fill " : "") + border
 }
 
-export const WarningBorder = (props: Props) => {
-  return (
-    <div className={className(props)}>
-      {props.children}
-    </div>
-  );
-}
+export const WarningBorder = (props: Props) => (
+  <div className={className(props)}>
+    {props.children}
+  </div>
+)
