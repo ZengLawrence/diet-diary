a6e248a4eeff2b179704c5548a953650b24e108c
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-20 19:25:36 -0400

    initial version

---
+++ src/components/warning/WarningAlert.tsx
@@ -0,0 +1,19 @@
+import { Alert } from "react-bootstrap";
+
+interface Props {
+  show?: boolean;
+  critical?: boolean;
+}
+
+function variant() {
+  return "warning";
+}
+
+function alertText() {
+  return "Total calories exceed target by 5%.";
+}
+export const WarningAlert = (props: Props) => (
+  <Alert variant={variant()}>
+    {alertText()}
+  </Alert>
+)
