ece34696b7f0152caab73d0199a060b85e1d11d7
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-20 15:01:29 -0400

    add critical warning

--- src/components/WarningBorder.tsx
+++ src/components/WarningBorder.tsx
@@ -1,12 +1,17 @@
 interface Props extends React.PropsWithChildren {
   flexFill?: boolean;
+  critical?: boolean;
+}
+
+function className(options: {flexFill?: boolean; critical?: boolean}) {
+  const borderColor = options.critical ? "border-danger" : "border-warning";
+  const border = "border rounded border-2 " + borderColor;
+  return (options.flexFill ? "d-flex flex-fill " : "") + border
 }
 
 export const WarningBorder = (props: Props) => {
-  const className = (props.flexFill ? "d-flex flex-fill " : "") + "border rounded border-2 border-warning";
-
   return (
-    <div className={className}>
+    <div className={className(props)}>
       {props.children}
     </div>
   );
