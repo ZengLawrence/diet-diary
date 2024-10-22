e5fdb4e17246a4eb30a6bdc681542df807706498
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-21 20:40:26 -0400

    always show alert if critical

--- src/features/warning/WarningAlert.ts
+++ src/features/warning/WarningAlert.ts
@@ -5,8 +5,22 @@ import { isCritical, shouldShow } from "./warnings";
 import { dismissWarning } from "./warningSlice";
 import { warningSelector } from "../../app/selectors";
 
+function show(state: RootState) {
+  if (isCritical(state)) {
+    return true;
+  }
+
+  if (shouldShow(state)) {
+    if (warningSelector(state).dismissWarning) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
 const mapStateToProps = (state: RootState) => ({
-  show: shouldShow(state) && !warningSelector(state).dismissWarning,
+  show: show(state),
   critical: isCritical(state),
 })
 
