5d6e7b897d20e8c72b4198a004b61baa5fd7a938
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-26 17:27:33 -0400

    fix width

--- src/components/summary/Percentage.tsx
+++ src/components/summary/Percentage.tsx
@@ -1,7 +1,7 @@
 import { toIntString } from "../../model/calorieFunction";
 
 export const Percentage = (props: { value: number; }) => (
-  <div className="border rounded bg-info text-white text-center p-1">
-    <span style={{ fontSize: '40px', width: '110px' }}>{toIntString(props.value)}</span>%
+  <div className="border rounded bg-info text-white text-center p-1" style={{ width: '110px' }}>
+    <span style={{ fontSize: '40px' }}>{toIntString(props.value)}</span>%
   </div>
 );
