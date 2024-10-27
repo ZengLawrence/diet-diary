96ecd45f70ca76c3d5c6d0ee1920156037f3da82
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-27 08:05:33 -0400

    Do not show target icons on Best Choice tab (#213)
    Target icons are not relevant on this tab and they are confusing.

--- src/features/summary/ServingCell.ts
+++ src/features/summary/ServingCell.ts
@@ -1,11 +1,11 @@
 import { connect } from "react-redux";
-import { targetSelector } from "../../app/selectors";
+import { summaryTypeSelector, targetSelector } from "../../app/selectors";
 import { RootState } from "../../app/store";
 import { ServingCell } from "../../components/summary/ServingCell";
 import { hasATarget } from "../../model/Target";
 
 const mapStateToProps = (state: RootState) => ({
-  showTargetActionIcon: hasATarget(targetSelector(state)),
+  showTargetActionIcon: hasATarget(targetSelector(state)) && summaryTypeSelector(state) != 'best-choice-percentage',
 })
 
 export default connect(mapStateToProps)(ServingCell);
