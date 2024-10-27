8e04ac0314a10e1eeb9bb56069fee43f32c6250c
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-27 08:30:09 -0400

    Center percent text on Best Choice tab (#214)

--- src/components/summary/Percentage.tsx
+++ src/components/summary/Percentage.tsx
@@ -1,7 +1,13 @@
 import { toIntString } from "../../model/calorieFunction";
 
+const ValueSpan = (props: { value: number; }) => (
+  <span style={{ fontSize: '40px' }}>
+    {toIntString(props.value)}
+  </span>
+)
+
 export const Percentage = (props: { value: number; }) => (
-  <div className="border rounded bg-info text-white text-center p-1" style={{ width: '110px' }}>
-    <span style={{ fontSize: '40px' }}>{toIntString(props.value)}</span>%
+  <div className="d-flex flex-column justify-content-center border rounded bg-info text-white text-center p-1" style={{ width: '110px' }}>
+    <span><ValueSpan value={props.value}/>%</span>
   </div>
 );
