8ff8d34289568a1c0911c7e6192c761faece8aa5
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-30 19:26:21 -0400

    extract to a new component for reuse

--- src/components/TwoColumnChart.tsx
+++ src/components/TwoColumnChart.tsx
@@ -1,6 +1,16 @@
-export const TwoColumnChart = () => (
+interface Props {
+  col1: {
+    height: number;
+  };
+}
+
+const Column = (props: {height: number;}) => (
+  <rect width="25" height={props.height} x="0" y={100 - props.height} style={{ fill: "#0d6efd" }} />
+);
+
+export const TwoColumnChart = (props: Props) => (
   <svg width="51" height="100" xmlns="http://www.w3.org/2000/svg">
-    <rect width="25" height="60" x="0" y="40" style={ {fill: "#0d6efd"}}/>
-    <rect width="25" height="40" x="26" y="60" style={ {fill: "#ced4da"}} />
+    <Column {...props.col1} />
+    <rect width="25" height="40" x="26" y="60" style={{ fill: "#ced4da" }} />
   </svg>
 )
