7f08449c6c12ee06055fea5dee5f32787a9df6a2
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-30 19:56:03 -0400

    rename for better intent

--- src/components/BestChoiceComparisonChart.tsx
+++ src/components/BestChoiceComparisonChart.tsx
@@ -8,7 +8,7 @@ const Column = (props: { height: number; className?: string }) => (
   <rect width="25" height={props.height} x="0" y={100 - props.height} className={props.className} />
 );
 
-export const TwoColumnChart = (props: Props) => (
+export const BestChoiceComparisonChart = (props: Props) => (
   <svg width="51" height="100" xmlns="http://www.w3.org/2000/svg">
     <Column {...props.col1} className="dd-chart-best-choice-column" />
     <rect width="25" height="40" x="26" y="60" style={{ fill: "#ced4da" }} />
