78037b63b688c0093be0bf7f24c900e6bf3dcb6f
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-30 19:17:38 -0400

    initial version

---
+++ src/components/TwoColumnChart.tsx
@@ -0,0 +1,6 @@
+export const TwoColumnChart = () => (
+  <svg width="51" height="100" xmlns="http://www.w3.org/2000/svg">
+    <rect width="25" height="60" x="0" y="40" style={ {fill: "#0d6efd"}}/>
+    <rect width="25" height="40" x="26" y="60" style={ {fill: "#ced4da"}} />
+  </svg>
+)
