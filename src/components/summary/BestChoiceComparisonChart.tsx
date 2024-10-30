8657b982be8a1aaa33dd034b945cf4b26d5ef4c5
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-30 19:59:09 -0400

    clear intent

--- src/components/summary/BestChoiceComparisonChart.tsx
+++ src/components/summary/BestChoiceComparisonChart.tsx
@@ -1,6 +1,6 @@
 interface Props {
-  col1: {
-    height: number;
+  bestChoice: {
+    value: number;
   };
 }
 
@@ -10,7 +10,7 @@ const Column = (props: { height: number; className?: string }) => (
 
 export const BestChoiceComparisonChart = (props: Props) => (
   <svg width="51" height="100" xmlns="http://www.w3.org/2000/svg">
-    <Column {...props.col1} className="dd-chart-best-choice-column" />
+    <Column height={props.bestChoice.value} className="dd-chart-best-choice-column" />
     <rect width="25" height="40" x="26" y="60" style={{ fill: "#ced4da" }} />
   </svg>
 )
