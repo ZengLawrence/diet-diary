fccbb37d519b91d3db731fd5c9ce670321f7ce84
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-30 19:31:05 -0400

    move fill color to css

--- src/components/TwoColumnChart.tsx
+++ src/components/TwoColumnChart.tsx
@@ -4,13 +4,13 @@ interface Props {
   };
 }
 
-const Column = (props: {height: number;}) => (
-  <rect width="25" height={props.height} x="0" y={100 - props.height} style={{ fill: "#0d6efd" }} />
+const Column = (props: { height: number; className?: string }) => (
+  <rect width="25" height={props.height} x="0" y={100 - props.height} className={props.className} />
 );
 
 export const TwoColumnChart = (props: Props) => (
   <svg width="51" height="100" xmlns="http://www.w3.org/2000/svg">
-    <Column {...props.col1} />
+    <Column {...props.col1} className="dd-chart-best-choice-column" />
     <rect width="25" height="40" x="26" y="60" style={{ fill: "#ced4da" }} />
   </svg>
 )
