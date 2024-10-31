771643ab2287f30c872d5a2d42e365bcfa34a7ad
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-31 10:37:50 -0400

    Center single digit value on comparison chart (#222)

--- src/components/summary/BestChoiceComparisonChart.tsx
+++ src/components/summary/BestChoiceComparisonChart.tsx
@@ -6,10 +6,6 @@ interface Props {
   others: number;
 }
 
-const Column = (props: { height: number; x: number; y: number; className?: string }) => (
-  <rect width="25" {...props} />
-);
-
 function height(value: number, max: number) {
   return value / max * 100;
 }
@@ -18,24 +14,32 @@ function format(val: number) {
   return val > 10 ? numeral(val).format('0') : numeral(val).format('0[.][0]');
 }
 
+function offset(s: string) {
+  return _.size(s) == 1 ? 5 : 0;
+}
+
 const CANVAS_HEIGHT = 120;
 const TEXT_HEIGHT = 5;
 
 export const BestChoiceComparisonChart = (props: Props) => {
   const { bestChoice, others } = props;
   const maxHeight = _.defaultTo(_.max([bestChoice, others, 10]), 10);
+
   const bcHeight = height(bestChoice, maxHeight);
   const bcY = CANVAS_HEIGHT - bcHeight;
+  const bcVal = format(bestChoice);
+
   const othersHight = height(others, maxHeight);
   const othersY = CANVAS_HEIGHT - othersHight;
+  const othersVal = format(others);
 
   return (
     <svg width="51" height={CANVAS_HEIGHT} xmlns="http://www.w3.org/2000/svg">
-      <Column x={0} y={bcY} height={bcHeight} className="dd-chart-best-choice-column" />
-      <text x="2" y={bcY - TEXT_HEIGHT} fill="currentColor" >{format(bestChoice)}</text>
+      <rect x={0} y={bcY} width="25" height={bcHeight} className="dd-chart-best-choice-column" />
+      <text x={2 + offset(bcVal)} y={bcY - TEXT_HEIGHT} fill="currentColor" >{bcVal}</text>
 
-      <Column x={26} y={othersY} height={othersHight} className="dd-chart-others-column" />
-      <text x="26" y={othersY - TEXT_HEIGHT} fill="currentColor" >{format(others)}</text>
+      <rect x={26} y={othersY} width="25" height={othersHight} className="dd-chart-others-column" />
+      <text x={26 + offset(othersVal)} y={othersY - TEXT_HEIGHT} fill="currentColor" >{othersVal}</text>
   </svg>
   );
 }
