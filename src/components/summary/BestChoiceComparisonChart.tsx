45bc6ee3d8cac0bbbe19c67325c70e2fb909e310
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-31 10:00:53 -0400

    format value to avoid chop off

--- src/components/summary/BestChoiceComparisonChart.tsx
+++ src/components/summary/BestChoiceComparisonChart.tsx
@@ -1,4 +1,5 @@
 import _ from "lodash";
+import numeral from "numeral";
 
 interface Props {
   bestChoice: number;
@@ -11,6 +12,10 @@ const Column = (props: { height: number; x: number; y: number; className?: strin
 
 function height(value: number, max: number) {
   return value / max * 100;
+}
+
+function format(val: number) {
+  return val > 10 ? numeral(val).format('0') : numeral(val).format('0[.][0]');
 }
 
 const CANVAS_HEIGHT = 120;
@@ -27,10 +32,10 @@ export const BestChoiceComparisonChart = (props: Props) => {
   return (
     <svg width="51" height={CANVAS_HEIGHT} xmlns="http://www.w3.org/2000/svg">
       <Column x={0} y={bcY} height={bcHeight} className="dd-chart-best-choice-column" />
-      <text x="2" y={bcY - TEXT_HEIGHT} fill="currentColor" >{bestChoice}</text>
+      <text x="2" y={bcY - TEXT_HEIGHT} fill="currentColor" >{format(bestChoice)}</text>
 
       <Column x={26} y={othersY} height={othersHight} className="dd-chart-others-column" />
-      <text x="26" y={othersY - TEXT_HEIGHT} fill="currentColor" >{others}</text>
+      <text x="26" y={othersY - TEXT_HEIGHT} fill="currentColor" >{format(others)}</text>
   </svg>
   );
 }
