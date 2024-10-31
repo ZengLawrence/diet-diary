9ffc3ab654a0499fad78d378114b1c8b7c980a58
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-31 09:28:15 -0400

    extract variable for readability

--- src/components/summary/BestChoiceComparisonChart.tsx
+++ src/components/summary/BestChoiceComparisonChart.tsx
@@ -13,14 +13,20 @@ function height(value: number, max: number) {
   return value / max * 100;
 }
 
+const CANVAS_HEIGHT = 100;
+
 export const BestChoiceComparisonChart = (props: Props) => {
   const { bestChoice, others } = props;
   const maxHeight = _.defaultTo(_.max([bestChoice, others, 10]), 10);
+  const bcHeight = height(bestChoice, maxHeight);
+  const bcY = CANVAS_HEIGHT - bcHeight;
+  const othersHight = height(others, maxHeight);
+  const othersY = CANVAS_HEIGHT - othersHight;
 
   return (
-    <svg width="51" height="100" xmlns="http://www.w3.org/2000/svg">
-      <Column x={0} y={100 - height(bestChoice, maxHeight)} height={height(bestChoice, maxHeight)} className="dd-chart-best-choice-column" />
-      <Column x={26} y={100 - height(others, maxHeight)} height={height(others, maxHeight)} className="dd-chart-others-column" />
+    <svg width="51" height={CANVAS_HEIGHT} xmlns="http://www.w3.org/2000/svg">
+      <Column x={0} y={bcY} height={bcHeight} className="dd-chart-best-choice-column" />
+      <Column x={26} y={othersY} height={othersHight} className="dd-chart-others-column" />
     </svg>
   );
 }
