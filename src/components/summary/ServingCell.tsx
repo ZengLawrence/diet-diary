42bdb28543db2c8d8e41dd9df52b5baa73fc74a0
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-30 20:45:01 -0400

    extract to new file for reuse

--- src/components/summary/ServingCell.tsx
+++ src/components/summary/ServingCell.tsx
@@ -2,25 +2,13 @@ import { Fragment } from "react";
 import TargetActionIcon from "../../features/summary/TargetActionIcon";
 import { FoodGroup } from "../../model/Food";
 import { displayServingValue } from "../../model/servingFunction";
-import { FoodGroupLabelBadge } from "../badge";
-import { FoodGroupLegend } from "../FoodGroupLegend";
+import { FoodGroupLabel } from "./FoodGroupLabel";
 
 const CalorieText = (props: { amount?: number; percentAmount?: boolean; }) => (
   <Fragment>
     <div className="d-block d-sm-none">{displayServingValue(props.amount, props.percentAmount)}{props.percentAmount && '%'}</div>
     <div className="d-none d-sm-block text-center">
       <span className="fs-2" >{displayServingValue(props.amount, props.percentAmount)}</span>{props.percentAmount && '%'}
-    </div>
-  </Fragment>
-);
-
-const FoodGroupLabel = (props: { foodGroup: FoodGroup; }) => (
-  <Fragment>
-    <div className="d-block d-sm-none">
-      <FoodGroupLabelBadge foodGroup={props.foodGroup} />
-    </div>
-    <div className="d-none d-sm-block">
-      <FoodGroupLegend foodGroup={props.foodGroup} />
     </div>
   </Fragment>
 );
