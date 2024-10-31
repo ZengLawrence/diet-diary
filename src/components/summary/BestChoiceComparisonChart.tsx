b17c02454d5b3d40f56245ae054f7adf70089396
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-31 10:07:42 -0400

    Show value on Best Choice comparison chart (#220)

--- src/components/summary/BestChoiceComparisonChart.tsx
+++ src/components/summary/BestChoiceComparisonChart.tsx
@@ -1,26 +1,41 @@
 import _ from "lodash";
+import numeral from "numeral";
 
 interface Props {
   bestChoice: number;
   others: number;
 }
 
-const Column = (props: { height: number; x: number; className?: string }) => (
-  <rect width="25" height={props.height} x={props.x} y={100 - props.height} className={props.className} />
+const Column = (props: { height: number; x: number; y: number; className?: string }) => (
+  <rect width="25" {...props} />
 );
 
 function height(value: number, max: number) {
   return value / max * 100;
 }
 
+function format(val: number) {
+  return val > 10 ? numeral(val).format('0') : numeral(val).format('0[.][0]');
+}
+
+const CANVAS_HEIGHT = 120;
+const TEXT_HEIGHT = 5;
+
 export const BestChoiceComparisonChart = (props: Props) => {
   const { bestChoice, others } = props;
   const maxHeight = _.defaultTo(_.max([bestChoice, others, 10]), 10);
+  const bcHeight = height(bestChoice, maxHeight);
+  const bcY = CANVAS_HEIGHT - bcHeight;
+  const othersHight = height(others, maxHeight);
+  const othersY = CANVAS_HEIGHT - othersHight;
 
   return (
-    <svg width="51" height="100" xmlns="http://www.w3.org/2000/svg">
-      <Column x={0} height={height(bestChoice, maxHeight)} className="dd-chart-best-choice-column" />
-      <Column x={26} height={height(others, maxHeight)} className="dd-chart-others-column" />
-    </svg>
+    <svg width="51" height={CANVAS_HEIGHT} xmlns="http://www.w3.org/2000/svg">
+      <Column x={0} y={bcY} height={bcHeight} className="dd-chart-best-choice-column" />
+      <text x="2" y={bcY - TEXT_HEIGHT} fill="currentColor" >{format(bestChoice)}</text>
+
+      <Column x={26} y={othersY} height={othersHight} className="dd-chart-others-column" />
+      <text x="26" y={othersY - TEXT_HEIGHT} fill="currentColor" >{format(others)}</text>
+  </svg>
   );
 }
