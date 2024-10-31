a3fe0ca05067219a62d0f814939923f0482fcec1
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-31 10:31:12 -0400

    center text for single digit value

--- src/components/summary/BestChoiceComparisonChart.tsx
+++ src/components/summary/BestChoiceComparisonChart.tsx
@@ -14,6 +14,10 @@ function format(val: number) {
   return val > 10 ? numeral(val).format('0') : numeral(val).format('0[.][0]');
 }
 
+function offset(s: string) {
+  return _.size(s) == 1 ? 5 : 0;
+}
+
 const CANVAS_HEIGHT = 120;
 const TEXT_HEIGHT = 5;
 
@@ -32,10 +36,10 @@ export const BestChoiceComparisonChart = (props: Props) => {
   return (
     <svg width="51" height={CANVAS_HEIGHT} xmlns="http://www.w3.org/2000/svg">
       <rect x={0} y={bcY} width="25" height={bcHeight} className="dd-chart-best-choice-column" />
-      <text x="2" y={bcY - TEXT_HEIGHT} fill="currentColor" >{bcVal}</text>
+      <text x={2 + offset(bcVal)} y={bcY - TEXT_HEIGHT} fill="currentColor" >{bcVal}</text>
 
       <rect x={26} y={othersY} width="25" height={othersHight} className="dd-chart-others-column" />
-      <text x="26" y={othersY - TEXT_HEIGHT} fill="currentColor" >{othersVal}</text>
+      <text x={26 + offset(othersVal)} y={othersY - TEXT_HEIGHT} fill="currentColor" >{othersVal}</text>
   </svg>
   );
 }
