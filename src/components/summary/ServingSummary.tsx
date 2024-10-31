ca1c9200dad087b2679fa65aa92d9b362860f608
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-10-31 10:15:46 -0400

    remove percent references

--- src/components/summary/ServingSummary.tsx
+++ src/components/summary/ServingSummary.tsx
@@ -1,13 +1,13 @@
 import ServingCell from "../../features/summary/ServingCell";
 import { Serving } from "../../model/Food";
 
-export const ServingSummary = (props: { serving: Serving; isPercent?: boolean }) => (
+export const ServingSummary = (props: { serving: Serving; }) => (
   <div className="d-flex justify-content-between flex-fill flex-wrap">
-    <ServingCell foodGroup="vegetable" amount={props.serving.vegetable} percentAmount={props.isPercent} />
-    <ServingCell foodGroup="fruit" amount={props.serving.fruit} percentAmount={props.isPercent} />
-    <ServingCell foodGroup="carbohydrate" amount={props.serving.carbohydrate} percentAmount={props.isPercent} />
-    <ServingCell foodGroup="proteinDiary" amount={props.serving.proteinDiary} percentAmount={props.isPercent} />
-    <ServingCell foodGroup="fat" amount={props.serving.fat} percentAmount={props.isPercent} />
-    <ServingCell foodGroup="sweet" amount={props.serving.sweet} percentAmount={props.isPercent} />
+    <ServingCell foodGroup="vegetable" amount={props.serving.vegetable} />
+    <ServingCell foodGroup="fruit" amount={props.serving.fruit} />
+    <ServingCell foodGroup="carbohydrate" amount={props.serving.carbohydrate} />
+    <ServingCell foodGroup="proteinDiary" amount={props.serving.proteinDiary} />
+    <ServingCell foodGroup="fat" amount={props.serving.fat} />
+    <ServingCell foodGroup="sweet" amount={props.serving.sweet} />
   </div>
 )
