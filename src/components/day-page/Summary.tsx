afaeca7d21787a80ef8109c85ce03d451b4a7562
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-11-02 08:54:17 -0400

    always show tabbed summary

--- src/components/day-page/Summary.tsx
+++
@@ -1,6 +0,0 @@
-import TabbedSummary from "../../features/summary/TabbedSummary";
-import { TotalSummary } from "../summary/TotalSummary";
-
-export const Summary = (props: { showTabs: boolean }) => (
-  props.showTabs ? <TabbedSummary /> : <TotalSummary />
-);
