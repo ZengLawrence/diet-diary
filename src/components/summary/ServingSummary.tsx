db50e2f91da0b732c0f4eba87f3343ef8a893cdf
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-11-02 10:49:16 -0400

    display serving summary in rows of 3

--- src/components/summary/ServingSummary.tsx
+++ src/components/summary/ServingSummary.tsx
@@ -1,13 +1,33 @@
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import { Serving } from "../../model/Food";
 import { ServingCell } from "./ServingCell";
 
 export const ServingSummary = (props: { serving: Serving; }) => (
-  <div className="d-flex justify-content-between flex-fill flex-wrap">
-    <ServingCell foodGroup="vegetable" amount={props.serving.vegetable} />
-    <ServingCell foodGroup="fruit" amount={props.serving.fruit} />
-    <ServingCell foodGroup="carbohydrate" amount={props.serving.carbohydrate} />
-    <ServingCell foodGroup="proteinDiary" amount={props.serving.proteinDiary} />
-    <ServingCell foodGroup="fat" amount={props.serving.fat} />
-    <ServingCell foodGroup="sweet" amount={props.serving.sweet} />
-  </div>
+  <Row>
+    <Col>
+      <Row>
+        <ServingCell foodGroup="vegetable" amount={props.serving.vegetable} />
+      </Row>
+      <Row>
+        <ServingCell foodGroup="proteinDiary" amount={props.serving.proteinDiary} />
+      </Row>
+    </Col>
+    <Col>
+      <Row>
+        <ServingCell foodGroup="fruit" amount={props.serving.fruit} />
+      </Row>
+      <Row>
+        <ServingCell foodGroup="fat" amount={props.serving.fat} />
+      </Row>
+    </Col>
+    <Col>
+      <Row>
+        <ServingCell foodGroup="carbohydrate" amount={props.serving.carbohydrate} />
+      </Row>
+      <Row>
+        <ServingCell foodGroup="sweet" amount={props.serving.sweet} />
+      </Row>
+    </Col>
+  </Row>
 )
