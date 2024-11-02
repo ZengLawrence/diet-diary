5ad7fef2ec0fb4c7a53f605afcf43d684d5bca8a
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-11-02 08:59:39 -0400

    target is always > 0

--- src/features/warning/warnings.ts
+++ src/features/warning/warnings.ts
@@ -1,14 +1,13 @@
 import { totalCaloriesSelector, targetSelector } from "../../app/selectors";
 import { RootState } from "../../app/store";
-import { hasATarget } from "../../model/Target";
 
 export function shouldShow(state: RootState): boolean {
   const total = totalCaloriesSelector(state);
   const target = targetSelector(state);
-  return hasATarget(target) && (total / target.calorie) > 1.05;
+  return (total / target.calorie) > 1.05;
 }
 export function isCritical(state: RootState): boolean {
   const total = totalCaloriesSelector(state);
   const target = targetSelector(state);
-  return hasATarget(target) && (total / target.calorie) > 1.10;
+  return (total / target.calorie) > 1.10;
 }
