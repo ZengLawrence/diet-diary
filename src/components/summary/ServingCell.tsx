efad026202ed711627ea8eac92618b70df564d04
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-11-03 13:33:50 -0500

    better cohesion

--- src/components/summary/ServingCell.tsx
+++ src/components/summary/ServingCell.tsx
@@ -3,10 +3,6 @@ import { FoodGroup } from "../../model/Food";
 import { displayServingValue } from "../../model/servingFunction";
 import { FoodGroupLabel } from "./FoodGroupLabel";
 
-const CalorieText = (props: { amount?: number; }) => (
-    <div className="dd-serving-cell-calorie">{displayServingValue(props.amount)}</div>
-);
-
 interface Props {
   foodGroup: FoodGroup;
   amount?: number;
@@ -14,7 +10,7 @@ interface Props {
 
 export const ServingCell = (props: Props) => (
   <div className="d-flex flex-column align-items-center">
-    <CalorieText amount={props.amount} />
+    <div className="dd-serving-cell-calorie">{displayServingValue(props.amount)}</div>
     <div className="d-flex align-items-center">
       {<TargetActionIcon foodGroup={props.foodGroup} />}
       <FoodGroupLabel foodGroup={props.foodGroup} />
