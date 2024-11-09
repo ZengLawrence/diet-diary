a94fcec838ebf4a1a2a27d0b13ce2246411e500e
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-11-09 15:15:01 -0500

    makes ui too busy

--- src/components/summary/DifferenceSummary.tsx
+++ src/components/summary/DifferenceSummary.tsx
@@ -1,13 +1,10 @@
 import CalorieDifference from "../../features/summary/CalorieDifference";
 import ServingDifference from "../../features/summary/ServingDifference";
-import WarningBorder from "../../features/warning/WarningBorder";
 
 export const DifferenceSummary = () => {
   return (
     <div className="d-flex">
-      <WarningBorder flex>
-        <CalorieDifference />
-      </WarningBorder>
+      <CalorieDifference />
       <div className="flex-fill">
         <ServingDifference />
       </div>
