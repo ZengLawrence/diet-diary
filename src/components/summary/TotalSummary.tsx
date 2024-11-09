70af7eba8345b11f607d25b64e3eb9841d29b13c
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-11-09 15:25:25 -0500

    Remove warning border (#237)
    Makes UI too busy. Not useful.

--- src/components/summary/TotalSummary.tsx
+++ src/components/summary/TotalSummary.tsx
@@ -1,12 +1,9 @@
 import CalorieSummary from "../../features/summary/CalorieSummary";
 import ServingSummary from "../../features/summary/ServingSummary";
-import WarningBorder from "../../features/warning/WarningBorder";
 
 export const TotalSummary = () => (
   <div className="d-flex">
-    <WarningBorder flex>
-      <CalorieSummary />
-    </WarningBorder>
+    <CalorieSummary />
     <div className="flex-fill">
       <ServingSummary />
     </div>
