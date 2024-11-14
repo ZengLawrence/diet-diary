3b9448f5a3ff10890e6edce0cf1d130a05241739
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-11-13 20:24:12 -0500

    move index inside meal prop

--- src/components/saved-meal/SavedMealCards.tsx
+++ src/components/saved-meal/SavedMealCards.tsx
@@ -2,19 +2,20 @@ import _ from "lodash";
 import { Card, ListGroup } from "react-bootstrap";
 import SavedMealFoodListGroupItems from "../../features/saved-meal/FoodListGroupItems";
 import MealCardHeader from "../../features/saved-meal/MealCardHeader";
+import { Food } from "../../model/Food";
 
 interface Props {
-  numberOfMeals: number;
+  meals: { index: number; foods: Food[]; }[]
 }
 
 export const SavedMealCards = (props: Props) => (
   <div>
-    {_.map(_.range(props.numberOfMeals), (index) => (
-      <Card className="mt-1" key={index} data-cy="mealCard">
-        <MealCardHeader mealIndex={index}/>
-  
+    {_.map(props.meals, m => (
+      <Card className="mt-1" key={m.index} data-cy="mealCard">
+        <MealCardHeader mealIndex={m.index} />
+
         <ListGroup>
-          <SavedMealFoodListGroupItems mealIndex={index} />
+          <SavedMealFoodListGroupItems mealIndex={m.index} />
         </ListGroup>
       </Card>
     ))}
--- src/features/saved-meal/SavedMealCards.ts
+++ src/features/saved-meal/SavedMealCards.ts
@@ -3,9 +3,14 @@ import { connect } from "react-redux";
 import { savedMealsSelector } from "../../app/selectors";
 import { RootState } from "../../app/store";
 import { SavedMealCards } from "../../components/saved-meal/SavedMealCards";
+import { Food } from "../../model/Food";
+
+function indexedMeals(meals: { foods: Food[] }[]) {
+  return _.map(meals, (m, index) => { return { index: index, foods: m.foods }; });
+}
 
 const mapStateToProps = (state: RootState) => ({
-  numberOfMeals: _.size(savedMealsSelector(state)),
+  meals: indexedMeals(savedMealsSelector(state)),
 })
 
 export default connect(mapStateToProps)(SavedMealCards);
