5e1c4c3948026dc8302be84a28c8c7efe9fd57a4
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-11-15 19:40:45 -0500

    search by single term

--- src/features/saved-meal/SavedMealCardsOffcanvas.ts
+++ src/features/saved-meal/SavedMealCardsOffcanvas.ts
@@ -1,6 +1,6 @@
 import _ from "lodash";
 import { connect } from "react-redux";
-import { savedMealsSelector, showSavedMealsSelector } from "../../app/selectors";
+import { savedMealsSelector, savedMealStateSelector, showSavedMealsSelector } from "../../app/selectors";
 import { AppDispatch, RootState } from "../../app/store";
 import SavedMealCardsOffcanvas from "../../components/saved-meal/SavedMealCardsOffcanvas";
 import { Food } from "../../model/Food";
@@ -10,9 +10,19 @@ function indexedMeals(meals: { foods: Food[] }[]) {
   return _.map(meals, (m, index) => ({ index: index, foods: m.foods }));
 }
 
+function hasTerm(meal: { foods: Food[] }, term: string): boolean {
+  const found = _.find(meal.foods, food => food.description.includes(term));
+  return found ? true : false;
+}
+
+function filterMeals(state: RootState) {
+  const searchTerm = savedMealStateSelector(state).searchTerm;
+  return _.filter(indexedMeals(savedMealsSelector(state)), m => hasTerm(m, searchTerm));
+}
+
 const mapStateToProps = (state: RootState) => ({
   show: showSavedMealsSelector(state),
-  meals: indexedMeals(savedMealsSelector(state)),
+  meals: filterMeals(state),
 })
 
 const mapDispatchToProps = (dispatch: AppDispatch) => ({
