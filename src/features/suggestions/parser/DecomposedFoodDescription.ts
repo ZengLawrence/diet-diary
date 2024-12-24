faa5b7e50ff6ed04dbc3f435bfc287dd9f9d173b
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-12-24 09:26:47 -0500

    initial version

--- src/features/suggestions/parser/DecomposedFoodDescription.ts
+++ src/features/suggestions/parser/DecomposedFoodDescription.ts
@@ -9,11 +9,15 @@ function isEndedWithSpace(s: string) {
   return isSpaceAfter(s, _.size(s) - 1);
 }
 
+function isEndedWithRightParenthesis(s: string) {
+  return s.substring(_.size(s) - 1, _.size(s)) === ")";
+}
+
 export default function decompose(foodDescription: string) {
   const { foodName, amount, measurement } = parse(foodDescription);
   const unitText = measurement?.unitText;
   const foodNameCompleted = isSpaceAfter(foodDescription, _.size(foodName));
-  const unitCompleted = (amount && unitText) ? isEndedWithSpace(foodDescription) : false;
+  const unitCompleted = (amount && unitText) ? isEndedWithSpace(foodDescription) || isEndedWithRightParenthesis(foodDescription) : false;
   return {
     foodName,
     amount,
