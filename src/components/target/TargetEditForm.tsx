ea8cd742171738414c0a468071ecde3bc1b51240
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-04-05 11:14:58 -0400

    as 2 rows

--- src/components/target/TargetEditForm.tsx
+++ src/components/target/TargetEditForm.tsx
@@ -15,29 +15,28 @@ function updateFoodGroupServing(foodGroup: string, serving: number) {
 
 const TargetEditForm = (props: Props) => (
     <Form>
-        <Form.Group as={Row} controlId="formServings">
-            <Row className="justify-content-between">
-                <Col xs={4} sm={3} lg={2}>
-                    <ServingInputControl foodGroup="vegetable" serving={props.target.serving} isInvalid={false} onChange={updateFoodGroupServing} />
-                </Col>
-                <Col xs={4} sm={3} lg={2}>
-                    <ServingInputControl foodGroup="fruit" serving={props.target.serving} isInvalid={false} onChange={updateFoodGroupServing} />
-                </Col>
-                <Col xs={4} sm={3} lg={2}>
-                    <ServingInputControl foodGroup="carbohydrate" serving={props.target.serving} isInvalid={false} onChange={updateFoodGroupServing} />
-                </Col>
-                <Col xs={4} sm={3} lg={2}>
-                    <ServingInputControl foodGroup="proteinDiary" serving={props.target.serving} isInvalid={false} onChange={updateFoodGroupServing} />
-                </Col>
-                <Col xs={4} sm={3} lg={2}>
-                    <ServingInputControl foodGroup="fat" serving={props.target.serving} isInvalid={false} onChange={updateFoodGroupServing} />
-                </Col>
-                <Col xs={4} sm={3} lg={2}>
-                    <ServingInputControl foodGroup="sweet" serving={props.target.serving} isInvalid={false} onChange={updateFoodGroupServing} />
-                </Col>
-            </Row>
-        </Form.Group>
-
+        <Row className="justify-content-between">
+            <Col>
+                <ServingInputControl foodGroup="vegetable" serving={props.target.serving} isInvalid={false} onChange={updateFoodGroupServing} />
+            </Col>
+            <Col>
+                <ServingInputControl foodGroup="fruit" serving={props.target.serving} isInvalid={false} onChange={updateFoodGroupServing} />
+            </Col>
+            <Col>
+                <ServingInputControl foodGroup="carbohydrate" serving={props.target.serving} isInvalid={false} onChange={updateFoodGroupServing} />
+            </Col>
+        </Row>
+        <Row className="justify-content-between">
+            <Col>
+                <ServingInputControl foodGroup="proteinDiary" serving={props.target.serving} isInvalid={false} onChange={updateFoodGroupServing} />
+            </Col>
+            <Col>
+                <ServingInputControl foodGroup="fat" serving={props.target.serving} isInvalid={false} onChange={updateFoodGroupServing} />
+            </Col>
+            <Col>
+                <ServingInputControl foodGroup="sweet" serving={props.target.serving} isInvalid={false} onChange={updateFoodGroupServing} />
+            </Col>
+        </Row>
     </Form>
 )
 
