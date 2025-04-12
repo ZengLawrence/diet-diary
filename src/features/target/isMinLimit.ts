6e05c690ef13e24057cb4073495af281aa790dc7
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-04-12 10:26:48 -0400

    default not needed

--- src/features/target/isMinLimit.ts
+++ src/features/target/isMinLimit.ts
@@ -1,6 +1,6 @@
 import { FoodGroup } from "../../model/Food";
 
-export function isMinLimit(foodGroup: FoodGroup, unlimitedFruit: boolean = true) {
+export function isMinLimit(foodGroup: FoodGroup, unlimitedFruit: boolean) {
   return (foodGroup === "vegetable"
     || (foodGroup == "fruit" && unlimitedFruit)
   );
