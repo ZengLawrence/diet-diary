e24ddfc838964eaa9012ac1c10c16de6b8dd29e8
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-04-13 11:23:45 -0400

    replace with hooks

--- src/components/day-page/NewDaySymbolButton.tsx
+++ src/components/day-page/NewDaySymbolButton.tsx
@@ -1,14 +1,22 @@
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import NewDayButton from "../../features/day-page/NewDayButton";
+import Button from "react-bootstrap/Button";
+import { useDispatch } from "react-redux";
+import { newDay } from "../../features/day-page/dateSlice";
 import { VariantDanger } from "../ButtonVariant";
 
-const NewDaySymbolButton = () => (
-  <div className="align-self-center">
-    <NewDayButton variant={VariantDanger}>
-      <FontAwesomeIcon icon={faPlus} />
-    </NewDayButton>
-  </div>
-);
+const NewDaySymbolButton = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="align-self-center">
+      <Button 
+        variant={VariantDanger}
+        onClick={() => dispatch(newDay())} >
+        <FontAwesomeIcon icon={faPlus} />
+      </Button>
+    </div>
+  );
+}
 
 export default NewDaySymbolButton;
