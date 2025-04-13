70b3f41990f18ca1dd39be68d0547c89ac749d9d
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-04-13 11:16:50 -0400

    extract component

--- src/components/day-page/DatePanel.tsx
+++ src/components/day-page/DatePanel.tsx
@@ -1,15 +1,4 @@
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import NewDayButton from "../../features/day-page/NewDayButton";
-import { VariantDanger } from "../ButtonVariant";
-
-const NewDaySymbolButton = () => (
-  <div  className="align-self-center">
-    <NewDayButton variant={VariantDanger}>
-      <FontAwesomeIcon icon={faPlus} />
-    </NewDayButton>
-  </div>
-);
+import { NewDaySymbolButton } from "./NewDaySymbolButton";
 
 export const DatePanel = (props: { date: string; showNewDayButton: boolean }) => (
   <div className="d-flex flex-row">
---
+++ src/components/day-page/NewDaySymbolButton.tsx
@@ -0,0 +1,12 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import NewDayButton from "../../features/day-page/NewDayButton";
+import { VariantDanger } from "../ButtonVariant";
+
+export const NewDaySymbolButton = () => (
+  <div className="align-self-center">
+    <NewDayButton variant={VariantDanger}>
+      <FontAwesomeIcon icon={faPlus} />
+    </NewDayButton>
+  </div>
+);
