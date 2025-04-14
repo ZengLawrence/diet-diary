b3121357e7dc3b9f54a5d99cf53230d22494ce1b
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-04-14 10:09:18 -0400

    to start position

--- src/components/button/StartPositionButton.tsx
+++ src/components/button/StartPositionButton.tsx
@@ -1,12 +1,20 @@
 import { faBackwardStep } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Button from "react-bootstrap/Button";
+import { useDispatch } from "react-redux";
+import { atStart } from "../../features/history/historySlice";
 import { VariantSecondary } from "../ButtonVariant";
 
 const StartPositionButton = () => {
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(atStart())
+  }
+
   return (
     <Button
-      variant={VariantSecondary}>
+      variant={VariantSecondary}
+      onClick={handleClick}>
       <FontAwesomeIcon icon={faBackwardStep} />
     </Button>
   );
