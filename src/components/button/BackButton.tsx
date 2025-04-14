b9fde04e85d4e993816a2e86ab64ba2bac92d637
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-04-14 09:27:44 -0400

    format

--- src/components/button/BackButton.tsx
+++ src/components/button/BackButton.tsx
@@ -3,8 +3,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Button from "react-bootstrap/Button";
 import { VariantSecondary } from "../ButtonVariant";
 
-const BackButton = () => (
-  <Button variant={VariantSecondary}><FontAwesomeIcon icon={faCaretLeft} /></Button>
-);
+const BackButton = () => {
+  return (
+    <Button
+      variant={VariantSecondary}>
+      <FontAwesomeIcon icon={faCaretLeft} />
+    </Button>
+  );
+}
 
 export default BackButton;
