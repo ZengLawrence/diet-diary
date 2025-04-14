813310f3475f9582bd910159af558eb399d2cb60
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-04-14 18:06:54 -0400

    fix lint error

--- src/features/day-page/HeaderButtons.ts
+++ src/features/day-page/HeaderButtons.ts
@@ -1,14 +1,8 @@
 import _ from "lodash";
 import { connect } from "react-redux";
-import { dayPageSelector, mealsSelector } from "../../app/selectors";
+import { dayPageSelector } from "../../app/selectors";
 import { RootState } from "../../app/store";
 import { HeaderButtons } from "../../components/day-page/HeaderButtons";
-import { Meal } from "../../model/Food";
-
-function hasAtLeastOneFood(meals: Meal[]) {
-  return _.size(meals) > 0
-    && _.size(meals[0].foods) > 0;
-}
 
 const mapStateToProps = (state: RootState) => ({
   editMode: dayPageSelector(state).editMode,
