b898193625f4a9dcc9e293e135574ebefa0da46f
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-04-14 08:01:38 -0400

    initial add

--- src/components/day-page/DatePanel.tsx
+++ src/components/day-page/DatePanel.tsx
@@ -1,7 +1,11 @@
+import BackButton from "../button/BackButton";
 import NewDayButton from "./NewDayButton";
 
 export const DatePanel = (props: { date: string; showNewDayButton: boolean }) => (
   <div className="d-flex flex-row">
+    <div className="align-self-center">
+      <BackButton />
+    </div>
     <div data-cy="date" className="fs-1">{props.date}</div>&nbsp;
     {props.showNewDayButton && <NewDayButton />}
   </div>
