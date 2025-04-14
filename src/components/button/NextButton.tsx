e8231309953699e08ca4518711cce7ac472ad375
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-04-14 09:54:57 -0400

    format

--- src/components/button/NextButton.tsx
+++ src/components/button/NextButton.tsx
@@ -3,8 +3,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Button from "react-bootstrap/Button";
 import { VariantSecondary } from "../ButtonVariant";
 
-const NextButton = () => (
-  <Button variant={VariantSecondary}><FontAwesomeIcon icon={faCaretRight} /></Button>
-);
-
+const NextButton = () => {
+  return (
+    <Button variant={VariantSecondary}>
+      <FontAwesomeIcon icon={faCaretRight} />
+    </Button>
+  );
+}
 export default NextButton;
