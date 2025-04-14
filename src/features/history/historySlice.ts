c9b1d55b5476f4821a0a3fde1ab6d744bd179a3c
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-04-14 08:33:48 -0400

    keep max 7 days of history

--- src/features/history/historySlice.ts
+++ src/features/history/historySlice.ts
@@ -12,6 +12,7 @@ interface History {
 }
 
 const TODAY = -1;
+const MAX_DAYS = 7;
 
 const initialState: History = {
   days: [],
@@ -24,6 +25,9 @@ const historySlice = createSlice({
   reducers: {
     add(state, action: PayloadAction<DayHistory>) {
       state.days.unshift(action.payload);
+      if (state.days.length > MAX_DAYS) {
+        state.days = state.days.slice(0, MAX_DAYS);
+      }
     },
     back(state) {
       state.dateIndex += 1;
