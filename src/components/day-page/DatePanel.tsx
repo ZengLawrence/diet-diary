2fa103c329c6dc2ffd7043eec26871486b173d38
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-04-14 07:34:02 -0400

    center vertically

--- src/components/day-page/DatePanel.tsx
+++ src/components/day-page/DatePanel.tsx
@@ -2,7 +2,7 @@ import NewDayButton from "./NewDayButton";
 
 export const DatePanel = (props: { date: string; showNewDayButton: boolean }) => (
   <div className="d-flex flex-row">
-    <h1 data-cy="date">{props.date}</h1>&nbsp;
+    <div data-cy="date" className="fs-1">{props.date}</div>&nbsp;
     {props.showNewDayButton && <NewDayButton />}
   </div>
 );
