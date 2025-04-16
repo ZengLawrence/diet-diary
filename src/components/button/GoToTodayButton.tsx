0bf30c074f63269ec6ed90d94ec26130f59e4b2b
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-04-16 19:18:55 -0400

    consistent naming

--- src/components/button/GoToTodayButton.tsx
+++ src/components/button/GoToTodayButton.tsx
@@ -1,12 +1,12 @@
 import Button from "react-bootstrap/Button";
 import { useDispatch } from "react-redux";
-import { atEnd } from "../../features/history/historySlice";
+import { goToToday } from "../../features/history/historySlice";
 import { VariantSecondary } from "../ButtonVariant";
 
 const GoToTodayButton = () => {
   const dispatch = useDispatch();
   const handleClick = () => {
-    dispatch(atEnd())
+    dispatch(goToToday())
   }
 
   return (
