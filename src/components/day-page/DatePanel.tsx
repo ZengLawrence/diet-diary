eb8d72ad2f008588097b18aac2931a574746d9e9
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-04-17 20:23:57 -0400

    extra columns for spacing

--- src/components/day-page/DatePanel.tsx
+++ src/components/day-page/DatePanel.tsx
@@ -13,22 +13,28 @@ interface Props {
 export const DatePanel = (props: Props) => (
   <Row className="flex-fill">
     <Col />
+    {props.showHistoryStepButtons && <Col />}
+    <Col xs="auto" className="align-content-center">
+      <BackButton />
+    </Col>
     <Col xs="auto" className="align-content-center">
       <div className="d-flex flex-row">
-        <div className="align-content-center">
-          <BackButton />
-        </div>&nbsp;
         <div data-cy="date" className="fs-1">{props.date}</div>&nbsp;
-        {props.showHistoryStepButtons &&
-          <div className="align-content-center">
-            <NextButton />
-          </div>}
-        {props.showNewDayButton && <NewDayButton />}
       </div>
     </Col>
-    <Col className="align-content-center">
+    <Col xs="auto" className="align-content-center">
       {props.showHistoryStepButtons &&
-        <GoToTodayButton />}
+        <div className="align-content-center">
+          <NextButton />
+        </div>}
+      {props.showNewDayButton && <NewDayButton />}
     </Col>
+    <Col xs="auto" className="align-content-center">
+      <Row>
+        {props.showHistoryStepButtons &&
+          <GoToTodayButton />}
+      </Row>
+    </Col>
+    <Col />
   </Row>
 );
