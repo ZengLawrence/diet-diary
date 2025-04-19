649765314b9fddafebc97315c35215914b982315
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-04-19 08:30:40 -0400

    readability and maintainability

--- src/components/day-page/DatePanel.tsx
+++ src/components/day-page/DatePanel.tsx
@@ -1,34 +1,9 @@
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import BackButton from "../button/BackButton";
-import GoToTodayButton from "../button/GoToTodayButton";
-import NextButton from "../button/NextButton";
-import NewDayButton from "./NewDayButton";
 import { DateSpan } from "./DateSpan";
-
-const HistoryDatePanel = (props: React.PropsWithChildren) => (
-  <Row className="flex-fill">
-    <Col />
-    <Col sm="1" className="d-block d-none d-sm-block" />
-    <Col xs="auto" className="align-content-center">
-      <BackButton />
-    </Col>
-    <Col xs="auto" className="align-content-center">
-      {props.children}
-    </Col>
-    <Col xs="auto" className="align-content-center">
-      <div className="align-content-center">
-        <NextButton />
-      </div>
-    </Col>
-    <Col xs="auto" className="align-content-center">
-      <Row>
-        <GoToTodayButton />
-      </Row>
-    </Col>
-    <Col className="d-block d-none d-sm-block" />
-  </Row>
-);
+import HistoryDatePanel from "./HistoryDatePanel";
+import NewDayButton from "./NewDayButton";
 
 type ShowNewDayButtonProps = Pick<Props, 'showNewDayButton'>;
 
