79444dafac43d1f9dc76a7c26cc7db207acaa329
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-04-19 16:53:28 -0400

    extract date panel row

--- src/components/date-panel/HistoryDatePanel.tsx
+++ src/components/date-panel/HistoryDatePanel.tsx
@@ -13,31 +13,36 @@ const ViewProgressBarRow = () => {
   );
 }
 
+const DatePanelRow = (props: React.PropsWithChildren) => (
+  <Row className="flex-fill">
+    <Col />
+    <Col sm="1" className="d-block d-none d-sm-block" />
+    <Col xs="auto" className="align-content-center">
+      <BackButton />
+    </Col>
+    <Col xs="auto" className="align-content-center">
+      {props.children}
+    </Col>
+    <Col xs="auto" className="align-content-center">
+      <div className="align-content-center">
+        <NextButton />
+      </div>
+    </Col>
+    <Col xs="auto" className="align-content-center">
+      <Row>
+        <GoToTodayButton />
+      </Row>
+    </Col>
+    <Col className="d-block d-none d-sm-block" />
+  </Row>
+);
+
 const HistoryDatePanel = (props: React.PropsWithChildren) => (
   <div>
     <ViewProgressBarRow />
-    <Row className="flex-fill">
-      <Col />
-      <Col sm="1" className="d-block d-none d-sm-block" />
-      <Col xs="auto" className="align-content-center">
-        <BackButton />
-      </Col>
-      <Col xs="auto" className="align-content-center">
-        {props.children}
-      </Col>
-      <Col xs="auto" className="align-content-center">
-        <div className="align-content-center">
-          <NextButton />
-        </div>
-      </Col>
-      <Col xs="auto" className="align-content-center">
-        <Row>
-          <GoToTodayButton />
-        </Row>
-      </Col>
-      <Col className="d-block d-none d-sm-block" />
-    </Row>
-
+    <DatePanelRow>
+      {props.children}
+    </DatePanelRow>
   </div>
 );
 
