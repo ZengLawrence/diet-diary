205f84c8bad3c6bac55a96b69647a1eeff5ee019
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-04-19 16:49:44 -0400

    initial setup

--- src/components/date-panel/HistoryDatePanel.tsx
+++ src/components/date-panel/HistoryDatePanel.tsx
@@ -1,31 +1,44 @@
 import Col from "react-bootstrap/Col";
+import ProgressBar from "react-bootstrap/ProgressBar";
 import Row from "react-bootstrap/Row";
 import BackButton from "../button/BackButton";
 import GoToTodayButton from "../button/GoToTodayButton";
 import NextButton from "../button/NextButton";
 
+const ViewProgressBarRow = () => {
+  return (
+    <Row className="flex-fill">
+      <ProgressBar now={80} />
+    </Row>
+  );
+}
+
 const HistoryDatePanel = (props: React.PropsWithChildren) => (
-  <Row className="flex-fill">
-    <Col />
-    <Col sm="1" className="d-block d-none d-sm-block" />
-    <Col xs="auto" className="align-content-center">
-      <BackButton />
-    </Col>
-    <Col xs="auto" className="align-content-center">
-      {props.children}
-    </Col>
-    <Col xs="auto" className="align-content-center">
-      <div className="align-content-center">
-        <NextButton />
-      </div>
-    </Col>
-    <Col xs="auto" className="align-content-center">
-      <Row>
-        <GoToTodayButton />
-      </Row>
-    </Col>
-    <Col className="d-block d-none d-sm-block" />
-  </Row>
+  <div>
+    <ViewProgressBarRow />
+    <Row className="flex-fill">
+      <Col />
+      <Col sm="1" className="d-block d-none d-sm-block" />
+      <Col xs="auto" className="align-content-center">
+        <BackButton />
+      </Col>
+      <Col xs="auto" className="align-content-center">
+        {props.children}
+      </Col>
+      <Col xs="auto" className="align-content-center">
+        <div className="align-content-center">
+          <NextButton />
+        </div>
+      </Col>
+      <Col xs="auto" className="align-content-center">
+        <Row>
+          <GoToTodayButton />
+        </Row>
+      </Col>
+      <Col className="d-block d-none d-sm-block" />
+    </Row>
+
+  </div>
 );
 
 export default HistoryDatePanel;
