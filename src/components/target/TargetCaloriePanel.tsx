c8b9c6fae007a445012daf35945ae8795e165141
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-04-20 09:08:20 -0400

    initial setup

--- src/components/target/TargetCaloriePanel.tsx
+++ src/components/target/TargetCaloriePanel.tsx
@@ -1,16 +1,30 @@
+import Button from "react-bootstrap/Button";
 import TargetDropDown from "../../features/target/TargetDropDown";
 import TargetFoodGroupServingGoalBadgePanel from "../../features/target/TargetFoodGroupServingGoalBadgePanel";
 import TargetLabel from "../../features/target/TargetLabel";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
 
 export const TargetCaloriePanel = (props: { editTarget: boolean; }) => {
   const { editTarget } = props;
   const caloriePanel = editTarget ? <TargetDropDown /> : <TargetLabel />;
+  const customizeButton = editTarget ? (<Button>Custom</Button>) : "";
   return (
-    <div>
-      <div className="d-flex flex-nowrap  align-items-end">
-        Target:&nbsp; {caloriePanel} &nbsp;Cal.
-      </div>
-      <TargetFoodGroupServingGoalBadgePanel />
-    </div>
+    <Row className="flex-fill">
+      <Col />
+      <Col xs="auto" >
+        <div>
+          <div className="d-flex flex-nowrap  align-items-end">
+            Target:&nbsp; {caloriePanel} &nbsp;Cal.
+          </div>
+          <div className="d-flex flex-nowrap  align-items-end">
+            <TargetFoodGroupServingGoalBadgePanel />
+          </div>
+        </div>
+      </Col>
+      <Col >
+        {customizeButton}
+      </Col>
+    </Row>
   );
 };
