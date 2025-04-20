dc94b30222b7394729274f23cb7c2bba8859bdc8
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-04-20 08:26:48 -0400

    Show Back button only if there is history (#282)

--- src/components/date-panel/TodayDatePanel.tsx
+++ src/components/date-panel/TodayDatePanel.tsx
@@ -3,16 +3,17 @@ import Row from "react-bootstrap/Row";
 import BackButton from "../button/BackButton";
 import NewDayButton from "../day-page/NewDayButton";
 import { useSelector } from "react-redux";
-import { viewOptionsSelector } from "../../app/selectors";
+import { historyDaysProgressSelector, viewOptionsSelector } from "../../app/selectors";
 
 const TodayDatePanel = (props: React.PropsWithChildren) => {
   const showNewDayButton = useSelector(viewOptionsSelector).canAddNewDay;
+  const showBackButton = useSelector(historyDaysProgressSelector).totalDays > 0;
   
   return (
     <Row className="flex-fill">
       <Col />
       <Col xs="auto" className="align-content-center">
-        <BackButton />
+        {showBackButton && <BackButton />}
       </Col>
       <Col xs="auto" className="align-content-center">
         {props.children}
