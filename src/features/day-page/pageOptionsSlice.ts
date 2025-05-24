c968d736983db9318efa8c379bd18fb10d7f0104
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-05-24 09:33:53 -0400

    Refactor - restructure pageOptionsSlice for improved clarity and organization

--- src/features/day-page/pageOptionsSlice.ts
+++ src/features/day-page/pageOptionsSlice.ts
@@ -1,1 +1,57 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
 export type MealEditState = "add" | "edit" | undefined;
+
+export interface PageOptions {
+  mealState: MealState;
+}
+
+interface MealState {
+  editState: MealEditState;
+  mealIndex: number;
+  foodIndex: number;
+  showMealSavedAlertIndex: number;
+}
+
+function newMealState(): MealState {
+  return {
+    editState: undefined,
+    mealIndex: -1,
+    foodIndex: -1,
+    showMealSavedAlertIndex: -1,
+  };
+}
+
+function initialState(): PageOptions {
+  return {
+    mealState: newMealState(),
+  };
+}
+
+const pageOptionsSlice = createSlice({
+  name: "pageOptions",
+  initialState: initialState(),
+  reducers: {
+    enterMealEditMode(state, { payload: { mealIndex } }: PayloadAction<{ mealIndex: number; }>) {
+      state.mealState = {
+        ...newMealState(),
+        editState: "edit",
+        mealIndex,
+      }
+    },
+    enterMealAddMode(state, { payload: { mealIndex } }: PayloadAction<{ mealIndex: number; }>) {
+      state.mealState = {
+        ...newMealState(),
+        editState: "add",
+        mealIndex,
+      }
+    },
+    exitMealEditMode(state) {
+      state.mealState = newMealState();
+    },
+  },
+});
+
+export const { enterMealEditMode, enterMealAddMode, exitMealEditMode } = pageOptionsSlice.actions;
+
+export default pageOptionsSlice.reducer;
