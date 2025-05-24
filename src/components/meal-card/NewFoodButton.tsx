e80f542ceffcf990f44cef7e7c0a94d103f42ec4
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-05-24 09:07:06 -0400

    refactor: rename interface from OwnProps to Props for consistency

--- src/components/meal-card/NewFoodButton.tsx
+++ src/components/meal-card/NewFoodButton.tsx
@@ -3,12 +3,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from "react-bootstrap";
 import { VariantPrimary } from "../ButtonVariant";
 
-interface OwnProps { 
+interface Props { 
   mealIndex: number;
   onClick: () => void;
 }
 
-export const NewFoodButton = (props: OwnProps) => (
+export const NewFoodButton = (props: Props) => (
   <Button variant={VariantPrimary}  data-cy="buttonNewFood" onClick={props.onClick}>
     <FontAwesomeIcon icon={faPlus} />
   </Button>
