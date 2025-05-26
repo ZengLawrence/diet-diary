732e68c7d28f714f21ec0f590f249ec97ebe9d9a
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-05-26 09:06:23 -0400

    Remove dateSlice and mealStatesSlice for code simplification

--- src/app/reducers.ts
+++ src/app/reducers.ts
@@ -1,7 +1,5 @@
 import { combineReducers } from "redux";
-import date from "../features/day-page/dateSlice";
 import editMode from "../features/day-page/editModeSlice";
-import mealStates from "../features/day-page/mealStatesSlice";
 import summaryType from "../features/day-page/summaryTypeSlice";
 import targetState from "../features/target/targetStateSlice";
 import savedMeals from "../features/saved-meal/savedMealsSlice";
@@ -15,9 +13,7 @@ import today from "../features/day-page/todaySlice";
 
 export default combineReducers(
   {
-    date,
     summaryType,
-    mealStates,
     editMode,
     targetState,
     savedMeals,
