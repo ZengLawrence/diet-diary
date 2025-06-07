478ed7aa26cbc79e9ac3a8438914286794656654
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-06-07 15:53:16 -0400

    Refactor localStorage handling to support deprecated state conversion

--- src/app/localStorage.ts
+++ src/app/localStorage.ts
@@ -1,4 +1,23 @@
+import { PageOptions } from "../features/day-page/pageOptionsSlice";
 import { RootState } from "./store";
+
+type DeprecatedState = Omit<RootState, 'pageOptions' > & {
+  pageOptions: Omit<PageOptions, 'currentDate'>;
+};
+
+function isDeprecatedState(state: any): state is DeprecatedState {
+  return 'pageOptions' in state && 'currentDate' in state.pageOptions;
+}
+
+function convert(state: DeprecatedState): RootState {
+  return {
+    ...state,
+    pageOptions: {
+      ...state.pageOptions,
+      currentDate: "today", // Default to "today" for deprecated states
+    },
+  };
+}
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const loadState = (): any => {
@@ -8,6 +27,9 @@ export const loadState = (): any => {
       return undefined;
     }
     const state = JSON.parse(serializedState);
+    if (isDeprecatedState(state)) {
+      return convert(state);
+    }
     return state;
   } catch (e) {
     console.error("Error loading state from localStorage", e);
