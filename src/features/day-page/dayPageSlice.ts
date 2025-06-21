fe5a95d27f2c36983d1665b7e17f5b4b19816e58
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-06-21 10:59:13 -0400

    Fix import path for addMeal in AddMealButton to use dayPageSlice

--- src/features/day-page/dayPageSlice.ts
+++ src/features/day-page/dayPageSlice.ts
@@ -19,6 +19,13 @@ export function newDay() {
   };
 }
 
+export function addMeal() {
+  return (dispatch: Dispatch) => {
+    const newDay = today.addMeal();
+    dispatch(dayPageSlice.actions.setDayPage(newDay));
+  }
+}
+
 const dayPageSlice = createSlice({
   name: 'dayPage',
   initialState: today.newDay(),
