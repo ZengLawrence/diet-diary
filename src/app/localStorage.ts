ee65a0636eb26ef422af591d62288f2947c0eca4
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-06-28 11:08:56 -0400

    Refactor local storage handling to separate saved meals logic and improve state loading

--- src/app/localStorage.ts
+++ src/app/localStorage.ts
@@ -1,6 +1,14 @@
+import { SavedMeal } from "../model/SavedMeal";
 import { RootState } from "./store";
 
-function loadReduxState(): RootState | null {
+type ReduxState = Omit<RootState, 'savedMeals'>;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function isReduxState(state: any): state is ReduxState {
+  return !state.savedMeals || !Array.isArray(state.savedMeals) || state.savedMeals.length === 0;
+} 
+
+function loadReduxState(): RootState | ReduxState | null {
   try {
     const serializedState = localStorage.getItem('state');
     if (serializedState === null) {
@@ -13,16 +21,47 @@ function loadReduxState(): RootState | null {
   }
 }
 
+function loadSavedMeals(): {meals:SavedMeal[]} {
+  try {
+    const serializedMeals = localStorage.getItem('savedMeals');
+    if (serializedMeals === null) {
+      return { meals: [] };
+    }
+    return JSON.parse(serializedMeals);
+  } catch (e) {
+    console.error("Error loading saved meals from localStorage", e);
+    return { meals: [] };
+  }
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const loadState = (): any => {
   const state = loadReduxState();
   if (state === null) {
     return undefined;
   }
+  if (isReduxState(state)) {
+    const savedMeals = loadSavedMeals();
+    return { ...state, savedMeals: savedMeals.meals };
+  }
   return state;
 };
 
-function saveReduxState(state: RootState): void {
+function removeSavedMeals(state: RootState): ReduxState {
+  const { savedMeals: _ignored, ...rest } = state;
+  return rest;
+}
+
+function saveSavedMeals(savedMeals: {meals: SavedMeal[]}) {
+  try {
+    const serializedMeals = JSON.stringify(savedMeals);
+    localStorage.setItem('savedMeals', serializedMeals);
+  } catch {
+    // ignore write errors
+  }
+}
+
+function saveReduxState(state: ReduxState): void {
   try {
     const serializedState = JSON.stringify(state);
     localStorage.setItem('state', serializedState);
@@ -32,5 +71,8 @@ function saveReduxState(state: RootState): void {
 }
 
 export const saveState = (state: RootState) => {
-  saveReduxState(state);
+  saveReduxState(removeSavedMeals(state));
+  saveSavedMeals({
+    meals: state.savedMeals,
+  });
 };
