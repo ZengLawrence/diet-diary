4e3822854d8281d92062c51d14e6dec2b25da23a
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-06-29 10:47:06 -0400

    Refactor - convert SavedMealCards to use explicit return statement

--- src/components/saved-meal/SavedMealCards.tsx
+++ src/components/saved-meal/SavedMealCards.tsx
@@ -8,16 +8,18 @@ interface Props {
   meals: { index: number; foods: Food[]; }[]
 }
 
-export const SavedMealCards = (props: Props) => (
-  <div>
-    {_.map(props.meals, m => (
-      <Card className="mt-1" key={m.index} data-cy="mealCard">
-        <MealCardHeader meal={m} />
+export const SavedMealCards = (props: Props) => {
+  return (
+    <div>
+      {_.map(props.meals, m => (
+        <Card className="mt-1" key={m.index} data-cy="mealCard">
+          <MealCardHeader meal={m} />
 
-        <ListGroup>
-          <FoodListGroupItems mealIndex={m.index} foods={m.foods} />
-        </ListGroup>
-      </Card>
-    ))}
-  </div>
-);
+          <ListGroup>
+            <FoodListGroupItems mealIndex={m.index} foods={m.foods} />
+          </ListGroup>
+        </Card>
+      ))}
+    </div>
+  );
+}
