c4ecbb7ffd0b72dedf13cf84a92d2f5200a328c8
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-07-04 13:53:03 -0400

    Add suggestions parameter to SavedMeals instantiation for improved meal suggestion handling

--- src/features/saved-meal/index.ts
+++ src/features/saved-meal/index.ts
@@ -1,6 +1,7 @@
 import { SavedMealsLocalStorage } from "../../app/savedMealLocalStorage";
 import { SavedMeals } from "../../model/savedMeals";
 import { today } from "../day-page/api";
+import { suggestions } from "../suggestions/SavedMealSuggestion";
 
 const savedMealLocalStorage = new SavedMealsLocalStorage();
-export const savedMeals = new SavedMeals(savedMealLocalStorage, savedMealLocalStorage, today);
+export const savedMeals = new SavedMeals(savedMealLocalStorage, savedMealLocalStorage, today, suggestions);
