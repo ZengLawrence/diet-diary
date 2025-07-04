4ded7bc2a76dbe314c11859f09411614f03f22c6
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-07-04 08:51:32 -0400

    Refactor - include 'today' in SavedMeals constructor for improved functionality

--- src/features/saved-meal/index.ts
+++ src/features/saved-meal/index.ts
@@ -1,5 +1,6 @@
 import { SavedMealsLocalStorage } from "../../app/savedMealLocalStorage";
 import { SavedMeals } from "../../model/savedMeals";
+import { today } from "../day-page/api";
 
 const savedMealLocalStorage = new SavedMealsLocalStorage();
-export const savedMeals = new SavedMeals(savedMealLocalStorage, savedMealLocalStorage);
+export const savedMeals = new SavedMeals(savedMealLocalStorage, savedMealLocalStorage, today);
