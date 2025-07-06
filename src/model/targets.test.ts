f53e38c8b4fcf84d3006b0879ebd99ee0eead44c
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-07-06 08:24:52 -0400

    test for woman targets

---
+++ src/model/targets.test.ts
@@ -0,0 +1,39 @@
+import { ReadOnlyCustomTargets } from "./customTarget";
+import { Target } from "./Target";
+import { Targets } from "./targets";
+
+const ZERO_SERVING = {
+  vegetable: 0,
+  fruit: 0,
+  carbohydrate: 0,
+  proteinDiary: 0,
+  fat: 0,
+  sweet: 0
+};
+
+describe('Targets class', () => {
+  describe('getByGender', () => {
+    it('should return targets for woman', () => {
+      const mockTargets: Target[] = [
+        { calorie: 1200, serving: ZERO_SERVING },
+        { calorie: 1400, serving: ZERO_SERVING },
+        { calorie: 1600, serving: ZERO_SERVING },
+        { calorie: 1800, serving: ZERO_SERVING },
+        { calorie: 2000, serving: ZERO_SERVING },
+      ];
+      const mockReadOnlyCustomTargets = {
+        getAll: jest.fn(),
+      } as unknown as jest.Mocked<ReadOnlyCustomTargets>;
+      mockReadOnlyCustomTargets.getAll.mockReturnValueOnce(mockTargets);
+
+      const targets = new Targets(mockReadOnlyCustomTargets);
+      expect(targets.getByGender('woman')).toStrictEqual([
+        { calorie: 1200, serving: ZERO_SERVING },
+        { calorie: 1400, serving: ZERO_SERVING },
+        { calorie: 1600, serving: ZERO_SERVING },
+        { calorie: 1800, serving: ZERO_SERVING },
+      ]);
+    });
+  });
+
+});
