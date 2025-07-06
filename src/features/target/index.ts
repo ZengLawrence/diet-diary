16f54e6096cd0a7cfd6e4de5205bfa555690a319
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-07-06 09:36:43 -0400

    replace redux with targetsApi

--- src/features/target/index.ts
+++ src/features/target/index.ts
@@ -1,4 +1,6 @@
 import { customTargetsLocalStorage } from "../../app/customTargetsLocalStorage";
 import { CustomTargets } from "../../model/customTarget";
+import { Targets } from "../../model/targets";
 
 export const customTargets = new CustomTargets(customTargetsLocalStorage, customTargetsLocalStorage);
+export const targetsApi = new Targets(customTargets);
