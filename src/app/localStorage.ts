451f98080d106906c620f03cbfda71eefd77e623
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-07-13 06:46:52 -0400

    Remove custom target conversion in localStorage module (#349)

--- src/app/localStorage.ts
+++ src/app/localStorage.ts
@@ -1,15 +1,6 @@
-import { Target } from "../model/Target";
-import { saveCustomTargets } from "./customTargetsLocalStorage";
 import { RootState } from "./store";
 
-type SerializedReduxState = RootState | RootState & {customTargets: {targets: Target[]}};
-
-//eslint-disable-next-line @typescript-eslint/no-explicit-any
-function hasCustomTargets(state: any): state is RootState & {customTargets: {targets: Target[]}} {
-  return ('customTargets' in state) 
-    && Array.isArray(state.customTargets.targets) 
-    && state.customTargets.targets.length > 0;
-}
+type SerializedReduxState = RootState;
 
 function loadReduxState(): SerializedReduxState | null {
   try {
@@ -29,11 +20,6 @@ export const loadState = (): any => {
   const state = loadReduxState();
   if (state === null) {
     return undefined;
-  }
-  if (hasCustomTargets(state)) {
-    const {customTargets, ...rest} = state;
-    saveCustomTargets(customTargets);
-    return rest;
   }
   return state;
 }
