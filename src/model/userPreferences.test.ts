466b1620504a4c8eadb68615b128ae2e92bb8ec0
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-07-13 15:08:54 -0400

    extract test setup

--- src/model/userPreferences.test.ts
+++ src/model/userPreferences.test.ts
@@ -3,11 +3,12 @@ import { Preferences } from "./preferences";
 import { UserPreferences } from "./userPreferences";
 
 describe("UserPreferences class", () => {
+  const mockPreferences = Object.create(Preferences.prototype);
+  const mockTargets = Object.create(CustomTargets.prototype);
+  const userPreferences = new UserPreferences(mockPreferences, mockTargets);
+
   describe("getStartDayTarget", () => {
     it("should return undefined if there is no preference", () => {
-      const mockPreferences = Object.create(Preferences.prototype);
-      const mockTargets = Object.create(CustomTargets.prototype);
-      const userPreferences = new UserPreferences(mockPreferences, mockTargets);
       expect(userPreferences.getStartDayTarget()).toBeUndefined();
     })
   })
