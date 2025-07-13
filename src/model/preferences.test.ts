197f6fc70d05f96fd6b61b82c1ae132f281e4634
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-07-13 08:40:36 -0400

    extract setup

--- src/model/preferences.test.ts
+++ src/model/preferences.test.ts
@@ -1,19 +1,20 @@
 import { PreferenceLoader, Preferences, PreferenceSaver } from "./preferences";
 
 describe("Preferences class", () => {
+  const loader = {
+    load: jest.fn(),
+  };
+  const saver = {
+    save: jest.fn(),
+  }
+  const prefs = new Preferences(loader, saver);
 
   describe("get", () => {
     it("should get from loader", () => {
-      const loader = {
-        load: jest.fn(),
-      };
-      const saver = {
-        save: jest.fn(),
-      }
       loader.load.mockReturnValue({ startDayWithCalorieLevel: 1600 });
-      const prefs = new Preferences(loader, saver);
+
       expect(prefs.get()).toStrictEqual({ startDayWithCalorieLevel: 1600 });
     });
   });
-  
+
 });
