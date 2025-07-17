8c4d3c2502994594660047af08a13eb53698cc49
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-07-18 05:17:25 +0800

    convert menu button  to drop down

--- src/components/date-panel/HamburgerMenu.tsx
+++ src/components/date-panel/HamburgerMenu.tsx
@@ -1,12 +1,19 @@
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Button } from "react-bootstrap";
+import { Button, Dropdown } from "react-bootstrap";
 import { VariantSecondary } from "../ButtonVariant";
 
 const HamburgerMenu = () => (
-  <Button variant={VariantSecondary}>
-    <FontAwesomeIcon icon={faBars} />
-  </Button>
+  <Dropdown>
+    <Dropdown.Toggle variant={VariantSecondary}>
+      <FontAwesomeIcon icon={faBars} />
+    </Dropdown.Toggle>
+
+    <Dropdown.Menu>
+      <Dropdown.Item>Preferences</Dropdown.Item>
+      <Dropdown.Item>Custom targets</Dropdown.Item>
+    </Dropdown.Menu>
+  </Dropdown>
 );
 
 export default HamburgerMenu;
