97d5c06e0bdc107a56030fc5a64b2f61a243a538
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-07-18 05:35:09 +0800

    Replace toggle with regular button

--- src/components/date-panel/HamburgerMenu.tsx
+++ src/components/date-panel/HamburgerMenu.tsx
@@ -1,19 +1,24 @@
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 import { Button, Dropdown } from "react-bootstrap";
 import { VariantSecondary } from "../ButtonVariant";
 
-const HamburgerMenu = () => (
-  <Dropdown>
-    <Dropdown.Toggle variant={VariantSecondary}>
-      <FontAwesomeIcon icon={faBars} />
-    </Dropdown.Toggle>
+const HamburgerMenu = () => {
+  const [show, setShow] = useState(false);
 
-    <Dropdown.Menu>
-      <Dropdown.Item>Preferences</Dropdown.Item>
-      <Dropdown.Item>Custom targets</Dropdown.Item>
-    </Dropdown.Menu>
-  </Dropdown>
-);
+  return (
+    <Dropdown show={show}>
+      <Button variant={VariantSecondary} onClick={() => setShow(!show)}>
+        <FontAwesomeIcon icon={faBars} />
+      </Button>
+
+      <Dropdown.Menu>
+        <Dropdown.Item>Preferences</Dropdown.Item>
+        <Dropdown.Item>Custom targets</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
 
 export default HamburgerMenu;
