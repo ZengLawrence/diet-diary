a356ef6d99479b91bbb9931e9bb37281061982d2
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-07-19 06:33:50 +0800

    format for adding state management

--- src/components/preference/PreferenceForm.tsx
+++ src/components/preference/PreferenceForm.tsx
@@ -2,29 +2,31 @@ import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 
-const PreferenceForm = () => (
-  <Form>
-    <Row>
-      <Col xs="auto">
-        <Form.Check
-          id="checkBoxStartDayCalorieLevel"
-          label="Start day with"
-        />
-      </Col>
-      <Col>
-        <Form.Select>
-          <option>1200</option>
-          <option>1400</option>
-          <option>1600</option>
-          <option>1800</option>
-          <option>1000</option>
-        </Form.Select>
-      </Col>
-      <Col>
-        calories
-      </Col>
-    </Row>
-  </Form>
-);
+const PreferenceForm = () => {
+  return (
+    <Form>
+      <Row>
+        <Col xs="auto">
+          <Form.Check
+            id="checkBoxStartDayCalorieLevel"
+            label="Start day with"
+          />
+        </Col>
+        <Col>
+          <Form.Select>
+            <option>1200</option>
+            <option>1400</option>
+            <option>1600</option>
+            <option>1800</option>
+            <option>1000</option>
+          </Form.Select>
+        </Col>
+        <Col>
+          calories
+        </Col>
+      </Row>
+    </Form>
+  );
+}
 
 export default PreferenceForm;
