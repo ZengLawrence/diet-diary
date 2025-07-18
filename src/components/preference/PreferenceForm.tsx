da596d5db67b059679cc33c3580cef5067d17d19
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-07-19 06:45:28 +0800

    add interaction properties

--- src/components/preference/PreferenceForm.tsx
+++ src/components/preference/PreferenceForm.tsx
@@ -2,7 +2,14 @@ import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 
-const PreferenceForm = () => {
+interface Props {
+  checkedStartDayCalorieLevel: boolean;
+  toggleCheckedStartDayCalorieLevel: () => void;
+  startDayCalorieLevel: number | undefined;
+  setStartDayCalorieLevel: (calorieLevel: number) => void;
+}
+
+const PreferenceForm = (props: Props) => {
   return (
     <Form>
       <Row>
@@ -10,10 +17,15 @@ const PreferenceForm = () => {
           <Form.Check
             id="checkBoxStartDayCalorieLevel"
             label="Start day with"
+            checked={props.checkedStartDayCalorieLevel}
+            onChange={props.toggleCheckedStartDayCalorieLevel}
           />
         </Col>
         <Col>
-          <Form.Select>
+          <Form.Select 
+            value={props.startDayCalorieLevel}
+            onChange={e => props.setStartDayCalorieLevel(parseInt(e.target.value))}
+            >
             <option>1200</option>
             <option>1400</option>
             <option>1600</option>
