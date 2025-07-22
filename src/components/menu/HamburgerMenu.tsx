a836eaee94588f894278c2a7ab9befa0173c1572
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-07-22 09:43:26 +0800

    connect to menu

--- src/components/menu/HamburgerMenu.tsx
+++ src/components/menu/HamburgerMenu.tsx
@@ -2,10 +2,18 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import { Button, Dropdown } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { openPreferences } from "../../features/overlays/overlaysSlice";
 import { VariantSecondary } from "../ButtonVariant";
 
 const HamburgerMenu = () => {
   const [show, setShow] = useState(false);
+
+  const dispatch = useDispatch();
+  const handlePreferencesItemClick = () => {
+    setShow(false);
+    dispatch(openPreferences());
+  };
 
   return (
     <Dropdown show={show}>
@@ -14,7 +22,7 @@ const HamburgerMenu = () => {
       </Button>
 
       <Dropdown.Menu>
-        <Dropdown.Item onClick={() => setShow(false)}>Preferences</Dropdown.Item>
+        <Dropdown.Item onClick={handlePreferencesItemClick}>Preferences</Dropdown.Item>
         <Dropdown.Item onClick={() => setShow(false)}>Custom targets</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
