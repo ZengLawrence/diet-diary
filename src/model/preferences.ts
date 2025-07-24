dd3eff844af01b1b79a82850c33d721547d9671c
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-07-24 15:33:12 +0800

    unneeded functions

--- src/model/preferences.ts
+++ src/model/preferences.ts
@@ -49,15 +49,6 @@ function savePreference(saver: PreferenceSaver, preference: Preference): void {
 export class ReadonlyPreferences {
   constructor(protected readonly loader: PreferenceLoader) { }
 
-  get(): Preference {
-    const pref = this.loader.load() || DEFAULT_PREFERENCE;
-    if (isValid(pref)) {
-      return pref;
-    } else {
-      return DEFAULT_PREFERENCE;
-    }
-  }
-
   getStartDayCalorieTarget(): Preference["startDayCalorieTarget"] {
     const pref = loadPreference(this.loader);
     return pref.startDayCalorieTarget;
@@ -70,12 +61,6 @@ export class Preferences extends ReadonlyPreferences {
     private readonly saver: PreferenceSaver
   ) {
     super(loader);
-  }
-
-  set(preference: Preference): void {
-    if (isValid(preference)) {
-      this.saver.save(preference);
-    }
   }
 
   toggleStartDayCalorieTarget(): boolean {
