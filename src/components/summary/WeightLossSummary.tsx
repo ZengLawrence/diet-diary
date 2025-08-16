cfc841899f4207a1e9c8065c492c97c78dd559aa
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-08-16 10:45:03 -0400

    Update WeightLossSummary component to use weight prop instead of calories for accurate representation

--- src/components/summary/WeightLossSummary.tsx
+++ src/components/summary/WeightLossSummary.tsx
@@ -1,12 +1,12 @@
 import { toString } from "../stringUtil";
 
 interface Props {
-  calories: number;
+  weight: number;
 }
 
 export const WeightLossSummary = (props: Props) => (
   <div className="d-flex flex-column justify-content-center border rounded bg-info text-white text-center p-1">
-    <div className="fs-1 dd-calorie-summary" >{toString(props.calories)}</div>
+    <div className="fs-1 dd-calorie-summary" >{toString(props.weight)}</div>
     <div>pound</div>
   </div>
 );
