60f8fd5bf2aec796933925582c95f6e3c48f3aaa
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-08-16 10:44:18 -0400

    Refactor WeightLossSummary component to use a Props interface for better type clarity

--- src/components/summary/WeightLossSummary.tsx
+++ src/components/summary/WeightLossSummary.tsx
@@ -1,6 +1,10 @@
 import { toString } from "../stringUtil";
 
-export const WeightLossSummary = (props: { calories: number; }) => (
+interface Props {
+  calories: number;
+}
+
+export const WeightLossSummary = (props: Props) => (
   <div className="d-flex flex-column justify-content-center border rounded bg-info text-white text-center p-1">
     <div className="fs-1 dd-calorie-summary" >{toString(props.calories)}</div>
     <div>pound</div>
