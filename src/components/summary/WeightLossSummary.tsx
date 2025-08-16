381d241c254ce99274a7d848a5f112550bd69645
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-08-16 09:59:26 -0400

    Add WeightLossSummary component

---
+++ src/components/summary/WeightLossSummary.tsx
@@ -0,0 +1,8 @@
+import { toString } from "../toIntString";
+
+export const WeightLossSummary = (props: { calories: number; }) => (
+  <div className="d-flex flex-column justify-content-center border rounded bg-info text-white text-center p-1">
+    <div className="fs-1 dd-calorie-summary" >{toString(props.calories)}</div>
+    <div>pound</div>
+  </div>
+);
