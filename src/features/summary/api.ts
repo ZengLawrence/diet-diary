47f4efe9efe70ecdc4a5d4e1cc1c9c620f55e45d
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-08-24 08:37:03 -0400

    export of summary variable not needed

--- src/features/summary/api.ts
+++ src/features/summary/api.ts
@@ -2,5 +2,5 @@ import { Summary } from "../../model/summary";
 import { diaryHistory } from "../day-page/api";
 import { preferencesApi } from "../preference/api";
 
-export const summary = new Summary(diaryHistory, preferencesApi);
+const summary = new Summary(diaryHistory, preferencesApi);
 export default summary;
