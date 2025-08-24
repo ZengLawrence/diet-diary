a4d8ed5f9da03c9a43ea8c627df8b0c855a3f7f3
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-08-24 09:50:42 -0400

    Add listener management to Summary class for weight loss updates

--- src/model/summary.test.ts
+++ src/model/summary.test.ts
@@ -1,9 +1,10 @@
-import { DiaryHistory } from "./diaryHistory";
+import { DiaryHistory, DiaryHistoryListener } from "./diaryHistory";
 import { ReadonlyPreferences } from "./preferences";
 import { Summary } from "./summary";
 
 describe("Summary class", () => {
   const mockDiaryHistory = Object.create(DiaryHistory.prototype);
+  mockDiaryHistory.registerListener = jest.fn();
   const mockPreferences = Object.create(ReadonlyPreferences.prototype);
   const summary = new Summary(mockDiaryHistory, mockPreferences);
 
@@ -14,5 +15,26 @@ describe("Summary class", () => {
     const result = summary.totalWeightLoss();
 
     expect(result).toBe(5);
+  });
+
+  it("should notify listeners when total weight loss is updated", () => {
+    const listener = {
+      onTotalWeightLossUpdated: jest.fn(),
+    };
+    let historyListener: DiaryHistoryListener | undefined;
+    mockDiaryHistory.registerListener = jest.fn((listener) => {
+      historyListener = listener;
+    });
+    // registering history listener in constructor
+    const summary = new Summary(mockDiaryHistory, mockPreferences);
+
+    summary.registerListener(listener);
+    if (historyListener) {
+      historyListener.dayAdded();
+    } else {
+      throw new Error("DiaryHistoryListener was not registered");
+    }
+
+    expect(listener.onTotalWeightLossUpdated).toHaveBeenCalled();
   });
 });
