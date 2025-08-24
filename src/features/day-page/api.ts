4e80439ad7d9b3f116e2a2c826820a229092b0ec
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-08-24 07:37:25 -0400

    Refactor - move target preferences to day-page module for cohesion and encapsulation

--- src/features/day-page/api.ts
+++ src/features/day-page/api.ts
@@ -1,9 +1,10 @@
 import { HistoryLocalStorage } from "../../app/historyLocalStorage";
 import { TodayLocalStorage } from "../../app/todayLocalStorage";
-import { Today } from "../../model/today";
 import { createDiary } from "../../model/diary";
 import { DiaryHistory } from "../../model/diaryHistory";
-import { targetPreferences } from "../preference/api";
+import { TargetPreferences } from "../../model/TargetPreferences";
+import { Today } from "../../model/today";
+import { preferencesApi } from "../preference/api";
 import { customTargets } from "../target";
 
 const historyLocalStorage = new HistoryLocalStorage();
@@ -12,6 +13,7 @@ export const diaryHistory = new DiaryHistory(historyLocalStorage, historyLocalSt
 const todayLocalStorage = new TodayLocalStorage();
 export const today = new Today(todayLocalStorage, todayLocalStorage);
 
+const targetPreferences = new TargetPreferences(preferencesApi, customTargets);
 export const diary = createDiary(
   today,
   diaryHistory,
--- src/features/preference/api.ts
+++ src/features/preference/api.ts
@@ -1,8 +1,5 @@
 import { PreferencesLocalStorage } from "../../app/preferencesLocalStorage";
 import { Preferences } from "../../model/preferences";
-import { TargetPreferences } from "../../model/TargetPreferences";
-import { customTargets } from "../target";
 
 const preferencesLocalStorage = new PreferencesLocalStorage();
 export const preferencesApi = new Preferences(preferencesLocalStorage, preferencesLocalStorage);
-export const targetPreferences = new TargetPreferences(preferencesApi, customTargets);
