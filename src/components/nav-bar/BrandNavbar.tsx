15784ced7ec086817c87d0b55d15bc6fbfdbd74e
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-09-06 17:12:47 -0400

    Branded Navbar component (#385)

---
+++ src/components/nav-bar/BrandNavbar.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from '../../app/apple-icon.png';
+
+function BrandNavbar() {
+  return (
+    <Navbar expand="lg" className="dd-brand-background-color">
+      <Container>
+        <Navbar.Brand>
+          <Image
+            src={logo}
+            alt="Logo"
+            width="30"
+            height="30"
+            priority={true}
+          />{' '}
+          Diet Diary
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link>Preferences</Nav.Link>
+            <Nav.Link>Custom Targets</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BrandNavbar;
