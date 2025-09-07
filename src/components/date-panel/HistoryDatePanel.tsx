6b856161d3d0a9d3bd3ce1274cd2bc002f81c1b6
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-09-07 10:44:05 -0400

    combine for better cohesion

--- src/components/date-panel/HistoryDatePanel.tsx
+++ src/components/date-panel/HistoryDatePanel.tsx
@@ -5,35 +5,25 @@ import GoToTodayButton from "../button/GoToTodayButton";
 import NextButton from "../button/NextButton";
 import HistoryDaysProgressBar from "./HistoryDaysProgressBar";
 
-const ViewProgressBarRow = () => (
-  <Row>
-    <HistoryDaysProgressBar />
-  </Row>
-);
-
-const DatePanelRow = (props: React.PropsWithChildren) => (
-  <Row className="g-2">
-    <Col>
-      <BackButton />
-    </Col>
-    <Col>
-      {props.children}
-    </Col>
-    <Col>
-      <NextButton />
-    </Col>
-    <Col>
-      <GoToTodayButton />
-    </Col>
-  </Row>
-);
-
 const HistoryDatePanel = (props: React.PropsWithChildren) => (
   <div>
-    <ViewProgressBarRow />
-    <DatePanelRow>
-      {props.children}
-    </DatePanelRow>
+    <Row>
+      <HistoryDaysProgressBar />
+    </Row>
+    <Row className="g-2">
+      <Col>
+        <BackButton />
+      </Col>
+      <Col>
+        {props.children}
+      </Col>
+      <Col>
+        <NextButton />
+      </Col>
+      <Col>
+        <GoToTodayButton />
+      </Col>
+    </Row>
   </div>
 );
 
