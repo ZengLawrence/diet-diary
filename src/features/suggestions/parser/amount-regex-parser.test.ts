85453b7ad38ac6bea8c3bfa38ad84f564280078e
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-11-16 11:54:21 -0500

    Parse amount using regex (#400)

---
+++ src/features/suggestions/parser/amount-regex-parser.test.ts
@@ -0,0 +1,178 @@
+import { expect, test } from "@jest/globals";
+import _ from "lodash";
+import { StandardUnit } from "../convert/standard-unit";
+import parseAmount from "./amount-regex-parser";
+
+test("unit pound(s) -> lb", () => {
+  testCases("pound", "lb").run();
+})
+
+test("unit ounce(s) -> oz", () => {
+  testCases("ounce", "oz").run();
+})
+
+test("unit fluid ounce(s) -> fl-oz", () => {
+  testCases("fluid ounce", "fl-oz", ["fl oz"]).run();
+})
+
+test("unit cup(s) -> cup", () => {
+  testCases("cup", "cup").run();
+})
+
+test("unit pint(s) -> pnt", () => {
+  testCases("pint", "pnt", ["pt"]).run();
+})
+
+test("unit quart(s) -> qt", () => {
+  testCases("quart", "qt").run();
+})
+
+test("unit gallon(s) -> gal", () => {
+  testCases("gallon", "gal").run();
+})
+
+test("unit teaspoon(s) -> tsp", () => {
+  testCases("teaspoon", "tsp").run();
+})
+
+test("unit tablespoon(s) -> Tbs", () => {
+  testCases("tablespoon", "Tbs", ["tbsp"]).run();
+})
+
+test("unit gram(s) -> g", () => {
+  testCases("gram", "g").run();
+})
+
+test("unit kilogram(s) -> g", () => {
+  testCases("kilogram", "kg").run();
+})
+
+test("unit milliliter(s) -> ml", () => {
+  testCases("milliliter", "ml").run();
+})
+
+test("unit liter(s) -> l", () => {
+  testCases("liter", "l").run();
+})
+
+test("unit small -> small", () => {
+  givenPluralHasSameSpelling().testCases("small", "small").run();
+})
+
+test("unit medium -> medium", () => {
+  givenPluralHasSameSpelling().testCases("medium", "medium").run();
+})
+
+test("unit large -> large", () => {
+  givenPluralHasSameSpelling().testCases("large", "large").run();
+})
+
+test("alternate measurement in amount", () => {
+  expect(parseAmount("3/4 cup or 1 medium")).toMatchObject({
+    measurement: {
+      quantity: 0.75,
+      unit: "cup",
+      unitText: "cup",
+    },
+    alternateMeasurement: {
+      quantity: 1,
+      unit: "medium",
+      unitText: "medium",
+    }
+  });
+})
+
+test("variable unit e.g. diameter unit '12-inch'", () => {
+  expect(parseAmount("1/8 of 14-inch")).toMatchObject({
+    measurement: {
+      quantity: 0.125,
+      unit: {
+        diameter: 14,
+      },
+      unitText: "of 14-inch",
+    }
+  });
+})
+
+test("parenthesis in unit e.g. 1/2 large (4 ounces)", () => {
+  expect(parseAmount("1/2 large (4 ounces)")).toMatchObject({
+    measurement: {
+      quantity: 0.5,
+      unit: "large",
+      unitText: "large (4 ounces)",
+    }
+  });
+})
+
+test("parenthesis in unit includes /", () => {
+  expect(parseAmount("1 pack (3/4 ounce)")).toMatchObject({
+    measurement: {
+      quantity: 1,
+      unit: "unknown",
+      unitText: "pack (3/4 ounce)",
+    }
+  });
+})
+
+test("ounce unit with no quantity, default quantity to 1", () => {
+  expect(parseAmount("2-ounce roll")).toMatchObject({
+    measurement: {
+      quantity: 1,
+      unit: "unknown",
+      unitText: "2-ounce roll",
+    }
+  });
+})
+
+function givenPluralHasSameSpelling() {
+  return {
+    testCases: _.partialRight(testCases, [], { pluralSameSpelling: true })
+  }
+}
+
+function testCases(unit: string, abbr: StandardUnit, commonAbbreviations: string[] = [], options: { pluralSameSpelling: boolean } = { pluralSameSpelling: false }) {
+  const singular = {
+    input: "1 " + unit,
+    output: {
+      measurement: {
+        unit: abbr
+      }
+    }
+  };
+  const plural = {
+    input: "2 " + unit + "s",
+    output: {
+      measurement: {
+        unit: abbr
+      }
+    }
+  };
+  const abbreviation = {
+    input: "3 " + abbr,
+    output: {
+      measurement: {
+        unit: abbr
+      }
+    }
+  };
+  return {
+    run: () => {
+      expect(parseAmount(singular.input)).toMatchObject(singular.output);
+      if (!options.pluralSameSpelling) {
+        expect(parseAmount(plural.input)).toMatchObject(plural.output);
+      }
+      if (commonAbbreviations) {
+        _.forEach(commonAbbreviations, function (_abbr) {
+          const input = "4 " + _abbr;
+          expect(parseAmount(input)).toMatchObject({ 
+            measurement: {
+              unit: abbr,
+            }
+          });
+        })
+      } else {
+        expect(parseAmount(abbreviation.input)).toMatchObject(abbreviation.output);
+      }
+    }
+  }
+}
---
+++ src/features/suggestions/parser/amount-regex-parser.ts
@@ -0,0 +1,89 @@
+import Fraction from "fraction.js";
+import { parseUnit, Unit } from "../convert";
+import _ from "lodash";
+
+interface Measurement {
+  quantity?: number,
+  quantityText?: string,
+  unitText?: string,
+}
+
+export type DecomposedAmount = {
+  measurement: {
+    quantity: number;
+    unit: Unit;
+    unitText: string | undefined;
+    amountWithUnitText: (unitText: string) => string;
+  };
+  alternateMeasurement?: undefined;
+} | {
+  measurement: {
+    quantity: number;
+    unit: Unit;
+    unitText: string | undefined;
+    amountWithUnitText: (unitText: string) => string;
+  };
+  alternateMeasurement: {
+    quantity: number;
+    unit: Unit;
+    unitText: string | undefined;
+    amountWithUnitText: (unitText: string) => string;
+  };
+}
+
+function toNumber(str: string): number {
+  return str.includes('/') ? new Fraction(str).valueOf() : _.toNumber(str);
+}
+
+function parseMeasurement(measurementText: string) {
+  const amountRegex = /^(\d*\.?\d+|\d+\s+\d+\/\d+|\d+\/\d+)((?=\s).+|)$/;
+  const match = measurementText.match(amountRegex);
+  if (match) {
+    const quantityText = match[1];
+    const quantity = toNumber(quantityText);
+    const unitText = match[2].trim() || undefined;
+    return { quantity, quantityText, unitText };
+  } else {
+    return { quantity: 1, unitText: measurementText };
+  }
+}
+
+function compose(quantityText: string | undefined, unitText: string) {
+  return quantityText + " " + unitText;
+}
+
+function createMeasurement(measurement?: Measurement) {
+  return {
+    quantity: measurement?.quantity || 1,
+    unit: parseUnit(measurement?.unitText),
+    unitText: measurement?.unitText,
+    amountWithUnitText: _.partial(compose, measurement?.quantityText),
+  }
+}
+
+// parse amount string into two measurements when joined by "or"
+function parseAmount(input: string) : {
+  measurement? : Measurement,
+  alternateMeasurement?: Measurement,
+} {
+  const orSeparatorRegex = /\s+or\s+/i;
+  const parts = input.split(orSeparatorRegex).map(part => part.trim());
+  if (parts.length === 2) {
+    return {
+      measurement: parseMeasurement(parts[0]),
+      alternateMeasurement: parseMeasurement(parts[1]),
+    };
+  } else {
+    return {
+      measurement: parseMeasurement(input),
+    };
+  }
+}
+
+export default function decompose(amount: string): DecomposedAmount {
+  const { measurement, alternateMeasurement } = parseAmount(amount);
+  return {
+    measurement: createMeasurement(measurement),
+    alternateMeasurement: alternateMeasurement ? createMeasurement(alternateMeasurement) : undefined, 
+  }
+}
