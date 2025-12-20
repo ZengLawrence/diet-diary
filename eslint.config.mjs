0097e39354cdb79beac9297ae7512c50375f2a8c
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-12-20 11:10:07 -0500

    formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
 
-export default [...nextCoreWebVitals, ...nextTypescript, {
-  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-}];
+export default [
+    ...nextCoreWebVitals,
+    ...nextTypescript,
+    {
+        ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+    },
+];
