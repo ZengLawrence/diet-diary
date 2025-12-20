915a0b0e8eb5823fd54667c677e9a2bd64cd5cb4
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-12-20 17:23:30 -0500

    use eslint config directly

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,45 @@
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextTypescript from "eslint-config-next/typescript";
+import js from '@eslint/js'
+import reactDom from 'eslint-plugin-react-dom'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import reactX from 'eslint-plugin-react-x'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import nextPlugin from '@next/eslint-plugin-next'
 
-const eslintConfig = [
-    ...nextCoreWebVitals,
-    ...nextTypescript,
-    {
-        ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      // tseslint.configs.strictTypeChecked,
+      // tseslint.configs.stylisticTypeChecked,
+      // Enable lint rules for React
+      // reactX.configs['recommended-typescript'],
+      // Enable lint rules for React DOM
+      reactDom.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      // reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
-];
-export default eslintConfig;
+  },
+  {
+    files: ['src/**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+    },
+  },
+])
