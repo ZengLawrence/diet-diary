713d72db1f31df1513425d5eefd917527cd772b6
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-12-20 09:31:55 -0500

    configure npm dev

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
