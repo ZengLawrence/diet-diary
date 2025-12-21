a4541d71b855949dea42d4ace63a9e35bcb48de4
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-12-21 16:00:19 -0500

    remove unused React plugins from ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,6 @@
 import js from '@eslint/js'
 import reactDom from 'eslint-plugin-react-dom'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import reactX from 'eslint-plugin-react-x'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
