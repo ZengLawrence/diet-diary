9c14781eb7afb1cc78bc4c0297dce36e1adb43ac
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-12-23 11:55:23 -0500

    Fix ESLint configuration by enabling React lint rules and correcting project array formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import js from '@eslint/js'
+import reactX from 'eslint-plugin-react-x'
 import reactDom from 'eslint-plugin-react-dom'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -16,7 +17,7 @@ export default defineConfig([
       // tseslint.configs.strictTypeChecked,
       // tseslint.configs.stylisticTypeChecked,
       // Enable lint rules for React
-      // reactX.configs['recommended-typescript'],
+      reactX.configs['recommended-typescript'],
       // Enable lint rules for React DOM
       reactDom.configs.recommended,
       reactHooks.configs.flat.recommended,
@@ -31,7 +32,7 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        project:['./tsconfig.app.json', './tsconfig.node.json'],
+        project: ['./tsconfig.app.json', './tsconfig.node.json'],
         tsconfigRootDir: import.meta.dirname,
       },
     },
