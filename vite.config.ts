ef42da937b82fb450d2eb59442b21c0cfe13a6b1
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-12-25 10:08:15 -0500

    chunk minisearch

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
           'lodash': ['lodash'],
           'bootstrap': ['bootstrap'],
           'react-bootstrap': ['react-bootstrap'],
+          'minisearch': ['minisearch'],
         },
       },
     },
