6ca142f73b17b2a7f57b8d1c2df261f8dfb7070c
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-12-27 14:35:07 -0500

    code split react dom client

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import App from './App'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import('react-dom/client').then(({ createRoot }) => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+});
