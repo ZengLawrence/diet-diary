8210468d95aa121f86798485bbe2ca7c1190e985
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-12-27 15:42:54 -0500

    Code split react-dom client (#425)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import App from './App'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import('react-dom/client').then(({ createRoot }) => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}).catch((error) => {
+  console.error('Failed to load ReactDOM:', error);
+  const fallbackContainer = document.getElementById('root');
+    if (fallbackContainer) {
+        fallbackContainer.textContent = 'Sorry, the application could not load. Please refresh your browser.';
+    }
+});
