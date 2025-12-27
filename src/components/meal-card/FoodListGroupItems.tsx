5a95d5ae45774efda8d86b19350cccce51e67ba4
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-12-27 10:30:44 -0500

    Code split food input form (#424)

--- src/components/meal-card/FoodListGroupItems.tsx
+++ src/components/meal-card/FoodListGroupItems.tsx
@@ -1,16 +1,17 @@
 import _ from "lodash";
-import { Fragment } from "react";
+import { Fragment, lazy, Suspense } from "react";
 import Col from "react-bootstrap/Col";
 import ListGroup from "react-bootstrap/ListGroup";
 import Row from "react-bootstrap/Row";
 import type { MealEditState } from "../../features/day-page/pageOptionsSlice";
-import AddFoodInputForm from "../../features/input-form/AddFoodInputForm";
-import UpdateFoodInputForm from "../../features/input-form/UpdateFoodInputForm";
 import EditFoodButton from "../../features/meal-card/EditFoodButton";
 import NewFoodButton from "../../features/meal-card/NewFoodButton";
 import type { Food } from "../../model/Food";
 import { VariantPrimary } from "../ButtonVariant";
 import { FoodItem } from "../FoodItem";
+
+const AddFoodInputForm = lazy(() => import("../../features/input-form/AddFoodInputForm"));
+const UpdateFoodInputForm = lazy(() => import("../../features/input-form/UpdateFoodInputForm"));
 
 const EditableFoodItem = (props: {
   food: Food;
@@ -46,7 +47,9 @@ export const FoodListGroupItems = (props: Props) => {
             </ListGroup.Item>
           ))}
           <ListGroup.Item key={_.size(props.foods)}>
-            <AddFoodInputForm mealIndex={props.mealIndex} />
+            <Suspense fallback={<div>Loading...</div>}>
+              <AddFoodInputForm mealIndex={props.mealIndex} />
+            </Suspense>
           </ListGroup.Item>
         </Fragment>
       );
@@ -60,7 +63,9 @@ export const FoodListGroupItems = (props: Props) => {
             props.foods.map((food, index) => (
               <ListGroup.Item key={index} data-cy="foodItem">
                 {index === props.foodEditIndex
-                  ? <UpdateFoodInputForm food={food} mealIndex={props.mealIndex} foodIndex={index} />
+                  ? <Suspense fallback={<div>Loading...</div>}>
+                    <UpdateFoodInputForm food={food} mealIndex={props.mealIndex} foodIndex={index} />
+                  </Suspense>
                   : <EditableFoodItem food={food} mealIndex={props.mealIndex} foodIndex={index} />
                 }
               </ListGroup.Item>
@@ -84,6 +89,6 @@ export const FoodListGroupItems = (props: Props) => {
           ))}
         </Fragment>
       );
-      /* eslint-enable react-x/no-array-index-key */
+    /* eslint-enable react-x/no-array-index-key */
   }
 }
