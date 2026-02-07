4f05bc68d8c3944349f66cb9b4041dfb76a50992
Lawrence Zeng <lawrence.zeng@gmail.com>
2026-02-07 08:45:24 -0500

    Add onClick handler to SavedFoodIcon and define secondary fill color

--- src/components/meal-card/SaveFoodIcon.tsx
+++ src/components/meal-card/SaveFoodIcon.tsx
@@ -4,11 +4,12 @@ function SavedFoodIcon(props: {
   mealIndex: number;
   foodIndex: number;
   isSaved?: boolean;
+  onClick?: () => void;
 }) {
   if (props.isSaved) {
-    return (<Save2Fill size={24} className="dd-fill-success" />);
+    return (<Save2Fill size={24} className="dd-fill-success" onClick={props.onClick} />);
   } else {
-    return (<Save2 size={24} className="dd-fill-gray-300" />);
+    return (<Save2 size={24} className="dd-fill-secondary" onClick={props.onClick} />);
   };
 }
 
