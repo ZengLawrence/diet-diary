e5702682b6148b3964ef72377e831933574cfe44
Lawrence Zeng <lawrence.zeng@gmail.com>
2026-02-07 13:59:45 -0500

    Add select mode state

--- src/components/saved-food/SavedFoodsOffcanvas.tsx
+++ src/components/saved-food/SavedFoodsOffcanvas.tsx
@@ -5,17 +5,26 @@ import Offcanvas from "react-bootstrap/Offcanvas";
 import { savedFoods } from "../../features/day-page/api";
 import type { Food } from "../../model/Food";
 import { FoodItem } from "../FoodItem";
+import { VariantSecondary } from "../ButtonVariant";
 
 interface State {
   foods: Food[];
+  inSelectMode: boolean;
 }
 
 type SetFoodsAction = { type: 'set-foods', foods: Food[] };
+type EnterSelectModeAction = { type: 'enter-select-mode' };
+type ExitSelectModeAction = { type: 'exit-select-mode' };
 
-function reducer(state: State, action: SetFoodsAction) {
+type Action = SetFoodsAction | EnterSelectModeAction | ExitSelectModeAction;  
+function reducer(state: State, action: Action) {
   switch (action.type) {
     case 'set-foods':
-      return { foods: action.foods };
+      return { ...state, foods: action.foods };
+    case 'enter-select-mode':
+      return { ...state, inSelectMode: true };
+    case 'exit-select-mode':
+      return { ...state, inSelectMode: false };
     default:
       return state;
   }
@@ -26,8 +35,13 @@ interface Props {
   onHide: () => void,
 }
 
+const initialState: State = {
+  foods: [],
+  inSelectMode: false,
+};
+
 function SavedFoodsOffcanvas(props: Props) {
-  const [{ foods }, dispatch] = useReducer(reducer, { foods: [] });
+  const [{ foods, inSelectMode }, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
     const loadedFoods = new Promise<Food[]>((resolve) => resolve(savedFoods.getAll()));
@@ -46,7 +60,12 @@ function SavedFoodsOffcanvas(props: Props) {
       </Offcanvas.Header>
       <Offcanvas.Body>
         <div className="mb-2 d-flex flex-row-reverse">
-          <Button>Select</Button>
+          <Button 
+            onClick={() => dispatch({ type: inSelectMode ? 'exit-select-mode' : 'enter-select-mode' })}
+            variant={VariantSecondary}
+            >
+            {inSelectMode ? 'Cancel' : 'Select'}
+          </Button>
         </div>
         <ListGroup>
           {foods.map((food, index) => (
