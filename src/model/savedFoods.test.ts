6e4336e92bd5f12ecbb6d542153cefcdfebde839
Lawrence Zeng <lawrence.zeng@gmail.com>
2026-02-06 19:32:51 -0500

    add test for init()

--- src/model/savedFoods.test.ts
+++ src/model/savedFoods.test.ts
@@ -19,14 +19,16 @@ class InMemoryPersistence implements SavedFoodsLoader, SavedFoodsSaver {
 }
 
 describe("SavedFoods Class", () => {
+  const mockSuggestions = {
+    addSuggestion: jest.fn(),
+    addSuggestions: jest.fn(),
+    removeSuggestion: jest.fn()
+  };
+
   describe("add a food to saved foods", () => {
     it("should add a food to saved foods with latest in the beginning, and add to suggestions", () => {
       const existingFoods = [{ description: "existing food", serving: {} }];
       const persistence = new InMemoryPersistence(existingFoods);
-      const mockSuggestions = {
-        addSuggestion: jest.fn(),
-        removeSuggestion: jest.fn()
-      };
       const savedFoods = new SavedFoods(persistence, persistence, mockSuggestions);
       const newFood = { description: "new food", serving: {} };
 
@@ -36,4 +38,20 @@ describe("SavedFoods Class", () => {
       expect(mockSuggestions.addSuggestion).toHaveBeenCalledWith(newFood);
     });
   });
+
+  describe("init", () => {
+    it("should add all saved foods to suggestions", () => {
+      const existingFoods = [
+        { description: "existing food 1", serving: {} },
+        { description: "existing food 2", serving: {} }
+      ];
+      const persistence = new InMemoryPersistence(existingFoods);
+      const savedFoods = new SavedFoods(persistence, persistence, mockSuggestions);
+
+      savedFoods.init();
+
+      expect(mockSuggestions.addSuggestions).toHaveBeenCalledWith(existingFoods);
+    });
+  });
+
 });
