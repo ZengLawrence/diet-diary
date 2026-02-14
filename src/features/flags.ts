56e2726f1349afcdb78886d18da8f2aa9f6a98b6
Lawrence Zeng <lawrence.zeng@gmail.com>
2026-02-14 08:52:08 -0500

    fix ts build error on unused variable

--- src/features/flags.ts
+++ src/features/flags.ts
@@ -1,5 +1,6 @@
+// @ts-expect-error: individual feature flag should be exported as function for consistency
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function isFeatureFlagEnabled(name: string): boolean {
+function _isFeatureFlagEnabled(name: string): boolean {
   const query = new URLSearchParams(window.location.search);
   return query.has(name);
 }
