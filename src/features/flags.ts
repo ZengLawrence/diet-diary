45edb14ea43cd9196c6800e93358249043f359ec
Lawrence Zeng <lawrence.zeng@gmail.com>
2026-02-14 08:33:50 -0500

    no longer needed

--- src/features/flags.ts
+++ src/features/flags.ts
@@ -1,8 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function isFeatureFlagEnabled(name: string): boolean {
   const query = new URLSearchParams(window.location.search);
   return query.has(name);
-}
-
-export function isSavedFoodEnabled() {
-  return isFeatureFlagEnabled("saved-food-enabled");
 }
